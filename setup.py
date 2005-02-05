78234f559ba389535c923bb49f11fa648550de87
logg <devnull@localhost>
2005-02-05 03:31:05 +0000

    Tailorized "2005-02-04 21:31:05 by logg" Updates for version 0.1.4

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ from os import chdir
 chdir("src")
 
 setup(name="FFC",
-      version="0.1.3",
+      version="0.1.4",
       description="The FEniCS Form Compiler",
       author="Anders Logg",
       author_email="logg@tti-c.org",
       url="http://www.fenics.org/ffc/",
       packages=["ffc/",
-                "ffc/parser",
+                "ffc/common",
                 "ffc/compiler",
-                "ffc/format"],
+                "ffc/format",
+                "ffc/parser"],
       scripts=["bin/ffc"])
