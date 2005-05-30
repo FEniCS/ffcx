3904300e21c3c6a12c232954150eed81653d1b8f
logg <devnull@localhost>
2005-05-30 22:37:16 +0000

    Tailorized "2005-05-30 17:37:16 by logg" Added missing file constants.py.

---
+++ src/ffc/common/constants.py
@@ -0,0 +1,8 @@
+__author__ = "Anders Logg (logg@tti-c.org)"
+__date__ = "2005-05-20"
+__copyright__ = "Copyright (c) 2005 Anders Logg"
+__license__  = "GNU GPL Version 2"
+
+FFC_VERSION = "0.1.8"
+FFC_EPSILON = 1e-14
+FFC_LICENSE = "GNU GPL Version 2"
