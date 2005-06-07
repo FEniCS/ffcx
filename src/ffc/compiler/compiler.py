4d10e4299c58e4532af1877a135f3a478402465a
logg <devnull@localhost>
2005-06-07 02:11:06 +0000

    Tailorized "2005-06-06 21:11:06 by logg" Fix for ASE output format:
    - Import of ase format temporarily moved under if-case in compiler.py
    - All forms in src/demo/ verified agains src/reference/ ok
    - Matt added to AUTHORS

--- src/ffc/compiler/compiler.py
+++ src/ffc/compiler/compiler.py
@@ -20,7 +20,6 @@ sys.path.append("../../")
 from ffc.format import dolfin
 from ffc.format import latex
 from ffc.format import raw
-from ffc.format import ase
 
 # FFC compiler modules
 from form import *
@@ -54,6 +53,7 @@ def compile(sums, name = "Form", language = "C++", license = FFC_LICENSE):
     elif language == "raw":
         format = raw
     elif language == "ase":
+        from ffc.format import ase
         format = ase
     else:
         raise "RuntimeError", "Unknown language " + str(language)
