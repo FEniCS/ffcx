550e6a7a3aa055bbbd613685ba257d8db034ff06
logg <devnull@localhost>
2005-06-28 05:21:42 +0000

    Tailorized "2005-06-28 00:21:42 by logg" Move computation of reordering of dofs on faces based on alignment to new module alignment. Looks like it's working now, but not tested with DOLFIN yet.
    Example output for degree 6 Lagrange on tets:
    
    void dofmap(int dofs[], const Cell& cell, const Mesh& mesh) const
    {
      static unsigned int face_reordering[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 4, 7, 9, 1, 5, 8, 2, 6, 3, 3, 6, 8, 9, 2, 5, 7, 1, 4, 0, 3, 2, 1, 0, 6, 5, 4, 8, 7, 9, 9,
     7, 4, 0, 8, 5, 1, 6, 2, 3, 9, 8, 6, 3, 7, 5, 2, 4, 1, 0};
    
      dofs[0] = cell.nodeID(0);
      dofs[1] = cell.nodeID(1);
      dofs[2] = cell.nodeID(2);
      dofs[3] = cell.nodeID(3);
      int alignment = cell.edgeAlignment(0);
      int offset = mesh.noNodes();
      dofs[4] = offset + 5*cell.edgeID(0) + ( alignment == 0 ? 0 : 4 );
      dofs[5] = offset + 5*cell.edgeID(0) + ( alignment == 0 ? 1 : 3 );
      ...
      alignment = cell.faceAlignment(0);
      offset = offset + 5*mesh.noEdges();
      dofs[34] = offset + 10*cell.faceID(0) + face_reordering[alignment*10 + 0];
      dofs[35] = offset + 10*cell.faceID(0) + face_reordering[alignment*10 + 1];
      dofs[36] = offset + 10*cell.faceID(0) + face_reordering[alignment*10 + 2];
    }

---
+++ src/ffc/compiler/alignment.py
@@ -0,0 +1,85 @@
+__author__ = "Anders Logg (logg@tti-c.org)"
+__date__ = "2005-06-27"
+__copyright__ = "Copyright (c) 2005 Anders Logg"
+__license__  = "GNU GPL Version 2"
+
+# Python modules
+from math import sqrt
+
+def face_reordering(num_nodes):
+    """Compute and return map for reordering of degrees of freedom on
+    faces of tetrahedra. The return value is a two-dimensional list,
+    indexed by (alignment, node number), which maps degrees of freedom
+    from the numbering local to the face of a tetrahedron to a common
+    local numbering for two tetrahedra sharing a common face. The
+    reordering is determined by the alignment of the common face with
+    the tetrahedron."""
+
+    # Compute number of nodes in each direction
+    n = int((sqrt(8*num_nodes+1)-3)/2 + 1)
+    if not n*(n+1)/2 == num_nodes:
+        raise RuntimeError, "Inconsistent number of nodes on face."
+    
+    # Offsets for rows (n nodes in first row, n-1 in second and so on)
+    offsets = [0]
+    for i in range(1, n):
+        offsets += [offsets[-1] + n + 1 - i]
+    print "offsets = " + str(offsets)
+
+    # Now do a nested loop over rows (i) and columns (j) in different
+    # directions depending on the alignment
+    nodes0 = [0 for i in range(num_nodes)]
+    nodes1 = [0 for i in range(num_nodes)]
+    nodes2 = [0 for i in range(num_nodes)]
+    nodes3 = [0 for i in range(num_nodes)]
+    nodes4 = [0 for i in range(num_nodes)]
+    nodes5 = [0 for i in range(num_nodes)]
+
+    # alignment == 0
+    node = 0
+    for i in range(n):
+        for j in range(n-i):
+            nodes0[offsets[i] + j] = node
+            node += 1
+    # alignment == 1
+    node = 0
+    for j in range(n):
+        for i in range(n-j):
+            nodes1[offsets[i] + j] = node
+            node += 1
+    # alignment == 2
+    node = 0
+    for j in range(n):
+        for i in range(n-1-j,-1,-1):
+            nodes2[offsets[i] + j] = node
+            node += 1
+    # alignment == 3
+    node = 0
+    for i in range(n):
+        for j in range(n-1-i,-1,-1):
+            nodes3[offsets[i] + j] = node
+            node += 1
+    # alignment == 4
+    node = 0
+    for j in range(n-1,-1,-1):
+        for i in range(j + 1):
+            nodes4[offsets[i] + j-i] = node
+            node += 1
+    # alignment == 5
+    node = 0
+    for j in range(n-1,-1,-1):
+        for i in range(j,-1,-1):
+            nodes5[offsets[i] + j-i] = node
+            node += 1
+            
+    print nodes0
+    print nodes1
+    print nodes2
+    print nodes3
+    print nodes4
+    print nodes5
+        
+    #for alignment in range(6):
+    #    print alignment
+
+    return [nodes0, nodes1, nodes2, nodes3, nodes4, nodes5]
