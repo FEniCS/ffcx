c3546a0e79e0f65a07d2e9002a3e6af184c1ccf0
logg <devnull@localhost>
2005-09-07 22:19:49 +0000

    Tailorized "2005-09-07 17:19:49 by logg" Compare terms and check for common hard and soft signatures. Implement util.permutations() to loop over possible reorderings of secondary indices for terms with common soft signatures (not actually reordering the indices yet).

--- src/ffc/common/util.py
+++ src/ffc/common/util.py
@@ -1,12 +1,28 @@
 __author__ = "Anders Logg (logg@tti-c.org)"
-__date__ = "2005-02-04"
+__date__ = "2005-02-04 -- 2005-09-07"
 __copyright__ = "Copyright (c) 2005 Anders Logg"
 __license__  = "GNU GPL Version 2"
 
-def listcopy(list):
+def listcopy(l):
     """Create a copy of the list, calling the copy constructor on each
     object in the list (problems when using copy.deepcopy)."""
-    if not list:
+    if not l:
         return []
     else:
-        return [object.__class__(object) for object in list]
+        return [object.__class__(object) for object in l]
+
+def permutations(l):
+    "Return a list of all permutations of the given list."
+    if len(l) < 1:
+        yield l
+    for i in range(len(l)):
+        pivot = l[i]
+        other = l[:i] + l[i+1:]
+        for p in permutations(other):
+            yield [pivot] + p
+    return
+
+if __name__ == "__main__":
+
+    for p in permutations([0, 1, 2, 3]):
+        print p
--- src/ffc/compiler/reorder.py
+++ src/ffc/compiler/reorder.py
@@ -8,7 +8,11 @@ __date__ = "2005-09-06"
 __copyright__ = "Copyright (c) 2004 Anders Logg"
 __license__  = "GNU GPL Version 2"
 
-# FFC modules
+# FFC common modules
+from ffc.common.util import permutations
+
+# FFC compiler modules
+from reassign import *
 from signature import *
 
 def reorder_indices(sum):
@@ -16,11 +20,44 @@ def reorder_indices(sum):
     if not isinstance(sum, Sum):
         raise RuntimeError, "Indices can only be reordered for Sums."
 
-    for p in sum.products:
-        soft = compute_soft_signature(p)
-        hard = compute_hard_signature(p)
+    # Compare signatures for pairs of terms
+    for i in range(len(sum.products) - 1):
 
-        print "Soft signature: " + soft
-        print "Hard signature: " + hard
+        p = sum.products[i]
+        p_soft = compute_soft_signature(p)
+        p_hard = compute_hard_signature(p)
+
+        # Compare term i against term j for j > i
+        for j in range(i + 1, len(sum.products)):
+
+            q = sum.products[j]
+            q_soft = compute_soft_signature(q)
+            q_hard = compute_hard_signature(q)
+
+            # Group terms if hard signature matches
+            if p_hard == q_hard:
+                print "Hard signatures match for terms %d and %d" % (i, j)
+
+            # Reorder terms if soft signature matches, then group
+            if p_soft == q_soft:
+                __reorder_indices(p, q)
+                print "Soft signatures match for terms %d and %d" % (i, j)
 
     return
+
+def __reorder_indices(p, q):
+    """Reorder secondary indices of Product q to match the secondary
+    indices of Product p."""
+
+    # Get the number of secondary indices (assuming indices have been
+    # previously reassigned to be in the range 0,...,n-1)
+    p_max = max_index(p, "secondary")
+    q_max = max_index(q, "secondary")
+    if not p_max == q_max:
+        raise RuntimeError, \
+              "Terms have different index ranges but common soft signature"
+    n = p_max + 1
+
+    # Generate all permutations of indices in the range 0,...,n-1
+    for reordering in permutations(range(n)):
+        print "Reordering indices according to " + str(reordering)
