6079b49901c7847477c7c866a7be1f429c19c5cd
logg <devnull@localhost>
2005-09-24 02:32:23 +0000

    Tailorized "2005-09-23 21:32:23 by logg" Add reference for Stokes demo.

--- src/ffc/common/constants.py
+++ src/ffc/common/constants.py
@@ -1,8 +1,8 @@
 __author__ = "Anders Logg (logg@tti-c.org)"
-__date__ = "2005-05-20 -- 2005-07-05"
+__date__ = "2005-05-20 -- 2005-09-23"
 __copyright__ = "Copyright (c) 2005 Anders Logg"
 __license__  = "GNU GPL Version 2"
 
-FFC_VERSION = "0.1.9"
+FFC_VERSION = "0.2.0"
 FFC_EPSILON = 1e-14
 FFC_LICENSE = "GNU GPL Version 2"
