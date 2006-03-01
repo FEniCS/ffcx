21120e3c4b2619c26a9c784abe9fed4870d76349
Anders Logg <logg@tti-c.org>
2006-03-01 11:35:53 -0500

    Minor fixes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os import chdir
 chdir("src")
 
 setup(name="FFC",
-      version="0.2.5",
+      version="0.3.0",
       description="The FEniCS Form Compiler",
       author="Anders Logg",
       author_email="logg@tti-c.org",
--- src/ffc/common/constants.py
+++ src/ffc/common/constants.py
@@ -1,9 +1,9 @@
 __author__ = "Anders Logg (logg@tti-c.org)"
-__date__ = "2005-05-20 -- 2005-12-28"
-__copyright__ = "Copyright (c) 2005 Anders Logg"
+__date__ = "2005-05-20 -- 2006-03-01"
+__copyright__ = "Copyright (c) 2005-2006 Anders Logg"
 __license__  = "GNU GPL Version 2"
 
-FFC_VERSION  = "0.2.5"
+FFC_VERSION  = "0.3.0"
 FFC_EPSILON  = 1e-14
 FFC_LANGUAGE = "dolfin"
 FFC_LICENSE  = "GNU GPL Version 2"
