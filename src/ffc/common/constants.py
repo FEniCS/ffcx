f8fd9a024c85533e0a1d3133730ffb64279d6b3f
Anders Logg <logg@tti-c.org>
2006-03-28 17:22:05 -0500

    Updates for version 0.3.1

--- src/ffc/common/constants.py
+++ src/ffc/common/constants.py
@@ -1,9 +1,9 @@
 __author__ = "Anders Logg (logg@tti-c.org)"
-__date__ = "2005-05-20 -- 2006-03-22"
+__date__ = "2005-05-20 -- 2006-03-28"
 __copyright__ = "Copyright (C) 2005-2006 Anders Logg"
 __license__  = "GNU GPL Version 2"
 
-FFC_VERSION  = "0.3.0"
+FFC_VERSION  = "0.3.1"
 FFC_EPSILON  = 1e-14
 FFC_LANGUAGE = "dolfin"
 FFC_LICENSE  = "GNU GPL Version 2"
