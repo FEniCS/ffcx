1d196dd3f980c1972fcbbe8bd37048f774c2fddc
Anders Logg <logg@tti-c.org>
2006-04-01 09:54:16 -0500

    Updates for version 0.3.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os import chdir
 chdir("src")
 
 setup(name="FFC",
-      version="0.3.1",
+      version="0.3.2",
       description="The FEniCS Form Compiler",
       author="Anders Logg",
       author_email="logg@tti-c.org",
