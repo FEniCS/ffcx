1c7eabc4d7b4ef378e9212e2501e4c3035eea284
Anders Logg <logg@simula.no>
2006-09-05 18:45:39 +0200

    Version 0.3.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os import chdir
 chdir("src")
 
 setup(name="FFC",
-      version="0.3.2",
+      version="0.3.3",
       description="The FEniCS Form Compiler",
       author="Anders Logg",
       author_email="logg@simula.no",
