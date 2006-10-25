e5930431da59e1caacf3c7e00b3bf76eed8c11c5
Anders Logg <logg@simula.no>
2006-10-25 14:38:00 +0200

    Change node_map --> finite_element and other fixes

--- ufc.h
+++ ufc.h
@@ -1,13 +1,12 @@
-// This is UFC (Unified Form-assembly Code) specification v. 0.0.
-// This code has been released into the public domain.
+// This is UFC (Unified Form-assembly Code) v. 0.0.
+// This code is released into the public domain.
 //
-// Simula Research Laboratory, 2006.
-
-// FIXME: --> The FEniCS project?
-// FIXME: --> Finite Element Center?
+// The FEniCS Project (http://www.fenics.org/) 2006.
 
 #ifndef __UFC_H
 #define __UFC_H
+
+// FIXME: Should we have constructors and destructors for each class?
 
 namespace ufc
 {
@@ -17,8 +16,12 @@ namespace ufc
   class mesh
   {
   public:
+
+    /// Constructor
     mesh(): num_entities(0) {}
 
+    // FIXME: Do we need this? Length of num_entities is the topological
+    // FIXME: dimension + 1.
     /// Space dimension and length of num_entities
     unsigned int space_dimension;
     
@@ -33,6 +36,8 @@ namespace ufc
   class cell
   {
   public:
+
+    /// Constructor
     cell(): entities(0), coordinates(0) {}
 
     /// Array of global indices for mesh entities contained in the cell
@@ -43,54 +48,42 @@ namespace ufc
     
   };
 
-  /// This class defines the interface for the local-to-global mapping
-  /// of nodes (degrees of freedom) for a finite element space.
+  /// This class defines the interface for a tensor-valued function.
 
-  class node_map
-  {
-  public:
-    /// Destructor
-    virtual ~node_map() {}
-
-    /// Return dimension of the local finite element space
-    virtual unsigned int space_dimension() const = 0;
-
-    /// Tabulate local-to-global mapping of nodes (degrees of freedom)
-    virtual void tabulate(unsigned int *nodes, const mesh& m, const cell& c) const = 0;
- 
-  };
-
-
-  /// This class defines a generic function f: Input -> Output
-  // TODO: Define Input and Output statically? Which type?
-
-  template<class Input, class Output>
   class function
   {
   public:
+
     /// Destructor
     virtual ~function() {}
 
-    /// Evaluating
-    virtual Output eval(Input) = 0;
+    /// Evaluate component i = (i[0], i[1], ...) at the point x = (x[0], x[1], ...)
+    virtual double eval(const int* i, const double* x) const;
+
   };
 
+  /// This class defines the interface for a finite element.
 
-  /// This class defines a mapping of a generic function<Input, Output>
-  /// onto a local finite element space.
-
-  template<class Input, class Output>
-  class projection
+  class finite_element
   {
   public:
+
     /// Destructor
-    virtual ~projection() {}
+    virtual ~finite_element() {}
 
-    /// For a particular finite element, computes the nodal functional of f:
-    /// dofs[i] = nu_i(f)  for each local dof i
-    virtual void project(function<Input,Output> & f, const cell & c, double *dofs) = 0;
+    /// Return dimension of the finite element function space
+    virtual unsigned int space_dimension() const = 0;
+
+    /// Evaluate basis function n at given point in cell
+    virtual double evaluate_basis(unsigned int n, double* x, const cell& c) const = 0;
+    
+    /// Evaluate node n at given function
+    virtual double evaluate_node(unsigned int n, const function& f, const cell& c) const = 0;
+
+    /// Compute local-to-global mapping of nodes (degrees of freedom)
+    virtual void tabulate_nodes(unsigned int *nodes, const mesh& m, const cell& c) const = 0;
+
   };
-
 
   /// This class defines the interface for the computation of the
   /// element tensor corresponding to a form with r + n arguments,
@@ -113,22 +106,31 @@ namespace ufc
   public:
 
     /// Constructor
-    element_tensor() : node_maps(0) {}
+    element_tensor() : finite_elements(0) {}
 
     /// Destructor
     virtual ~element_tensor() {}
 
-    /// Return the rank of the tensor (r)
+    /// Return the rank of the element tensor (r)
     virtual unsigned int rank() const = 0;
 
     /// Return the number of coefficients (n)
     virtual unsigned int num_coefficients() const = 0;
 
-    /// Tabulate element tensor
-    virtual void tabulate(double* A, const double** w, const cell& c) const = 0;
+    /// Return true iff there is a contribution from the interior
+    virtual bool interior_contribution() const = 0;
+
+    /// Return true iff there is a contribution from the boundary
+    virtual bool boundary_contribution() const = 0;
+
+    /// Tabulate interior contribution to element tensor
+    virtual void tabulate_interior(double* A, const double** w, const cell& c) const = 0;
+
+    /// Tabulate boundary contribution to element tensor
+    virtual void tabulate_boundary(double* A, const double** w, const cell& c, unsigned int facet) const = 0;
  
-    /// Array of r + n node maps for argument functions and coefficients
-    node_map** node_maps;
+    /// Array of r + n finite elements for argument function spaces and coefficients
+    finite_element** finite_elements;
   
   };
 
--- ufc_poisson.h
+++ ufc_poisson.h
@@ -3,28 +3,36 @@
 namespace poisson
 {
 
-  class node_map_0 : public ufc::node_map
+  class finite_element_0 : public ufc::finite_element
   {
   public:
-    
+
     inline unsigned int space_dimension() const { return 3; }
+
+    double evaluate_basis(unsigned int n, double* x, const ufc::cell& c) const { return 0.0; }
     
-    void tabulate(unsigned int* nodes, const ufc::mesh& m, const ufc::cell& c) const
+    double evaluate_node(unsigned int n, const ufc::function& f, const ufc::cell& c) const { return 0.0; }
+
+    void tabulate_nodes(unsigned int *nodes, const ufc::mesh& m, const ufc::cell& c) const
     {
       nodes[0] = c.entities[0][0];
       nodes[1] = c.entities[0][1];
       nodes[2] = c.entities[0][2];
     }
-    
+
   };
 
-  class node_map_1 : public ufc::node_map
+  class finite_element_1 : public ufc::finite_element
   {
   public:
-    
+
     inline unsigned int space_dimension() const { return 3; }
+
+    double evaluate_basis(unsigned int n, double* x, const ufc::cell& c) const { return 0.0; }
     
-    void tabulate(unsigned int* nodes, const ufc::mesh& m, const ufc::cell& c) const
+    double evaluate_node(unsigned int n, const ufc::function& f, const ufc::cell& c) const { return 0.0; }
+
+    void tabulate_nodes(unsigned int *nodes, const ufc::mesh& m, const ufc::cell& c) const
     {
       nodes[0] = c.entities[0][0];
       nodes[1] = c.entities[0][1];
@@ -39,23 +47,27 @@ namespace poisson
     
     element_tensor() : ufc::element_tensor()
     {
-      node_maps = new ufc::node_map * [2];
-      node_maps[0] = new node_map_0();
-      node_maps[1] = new node_map_1();
+      finite_elements = new ufc::finite_element * [2];
+      finite_elements[0] = new finite_element_0();
+      finite_elements[1] = new finite_element_1();
     }
 
     ~element_tensor()
     {
-      delete node_maps[0];
-      delete node_maps[1];
-      delete [] node_maps;
+      delete finite_elements[0];
+      delete finite_elements[1];
+      delete [] finite_elements;
     }
 
     inline unsigned int rank() const { return 2; }
 
     inline unsigned int num_coefficients() const { return 0; }
 
-    void tabulate(double* A, const double** w, const ufc::cell& c) const
+    inline bool interior_contribution() const { return true; }
+
+    inline bool boundary_contribution() const { return false; }
+
+    void tabulate_interior(double* A, const double** w, const ufc::cell& c) const
     {
       A[0] = 0.0;
       A[1] = 0.0;
@@ -68,6 +80,8 @@ namespace poisson
       A[8] = 0.0;
     }
 
+    void tabulate_boundary(double* A, const double** w, const ufc::cell& c, unsigned int facet) const {}
+    
   };
 
 }
