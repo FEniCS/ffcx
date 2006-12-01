b39730460677f544538e9bedd194b0c14d01d793
Anders Logg <logg@simula.no>
2006-12-01 11:30:00 +0100

    Updates for version 0.3.5

--- src/ffc/common/constants.py
+++ src/ffc/common/constants.py
@@ -1,9 +1,9 @@
 __author__ = "Anders Logg (logg@simula.no)"
-__date__ = "2005-05-20 -- 2006-04-01"
+__date__ = "2005-05-20 -- 2006-12-01"
 __copyright__ = "Copyright (C) 2005-2006 Anders Logg"
 __license__  = "GNU GPL Version 2"
 
-FFC_VERSION  = "0.3.4"
+FFC_VERSION  = "0.3.5"
 FFC_EPSILON  = 1e-14
 FFC_LANGUAGE = "dolfin"
 FFC_LICENSE  = "GNU GPL Version 2"
