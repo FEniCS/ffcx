2dab8fe3d168b1f50bef736a0608529dd29fb504
Anders Logg <logg@simula.no>
2006-12-07 17:05:45 +0100

    Fix bug in dolfin.py that crept in during merge.

--- src/ffc/format/dolfin.py
+++ src/ffc/format/dolfin.py
@@ -509,7 +509,6 @@ public:
   bool interior_boundary_contribution() const;
 
   void eval(real block[], const AffineMap& map0, const AffineMap& map1, real det, unsigned int facet0, unsigned int facet1, unsigned int alignment) const;
-            unsigned int facet0, unsigned int facet1) const;
 """
 
         # Declare class members (if any)
@@ -647,7 +646,6 @@ void %s::eval(real block[], const AffineMap& map, real det, unsigned int facet) 
 bool %s::interior_boundary_contribution() const { return true; }
 
 void %s::eval(real block[], const AffineMap& map0, const AffineMap& map1, real det, unsigned int facet0, unsigned int facet1, unsigned int alignment) const
-              unsigned int facet0, unsigned int facet1) const
 {
 %s}
 
@@ -658,7 +656,6 @@ void %s::eval(real block[], const AffineMap& map0, const AffineMap& map1, real d
 bool %s::interior_boundary_contribution() const { return false; }
 
 void %s::eval(real block[], const AffineMap& map0, const AffineMap& map1, real det, unsigned int facet0, unsigned int facet1, unsigned int alignment) const {}
-              unsigned int facet0, unsigned int facet1) const {}
 
 """ % (subclass, subclass)
 
