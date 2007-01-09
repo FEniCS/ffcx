277e703a04eb9b56543dbcede64000b0ad53737a
kent-and <kent-and@localhost>
2007-01-09 13:52:47 +0100

    added setup.py

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="ufc", version='1.0',
+      description="A unified form-compiler interface",
+      author = "Martin Sandve Alnaes, Hans Petter Langtangen, Anders Logg, Kent-Andre Mardal, and Ola Skavhaug",
+      url="http://www.fenics.org/ufc",
+      packages=['ufc'])
+
+
