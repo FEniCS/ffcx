9f47206f02d1ffd2ef198c4b95425926fffe407d
Anders Logg <logg@simula.no>
2007-01-10 10:46:17 +0100

    More edits.

--- src/utils/python/ufc/__init__.py
+++ src/utils/python/ufc/__init__.py
@@ -1,28 +1,44 @@
-"""Defines a set of strings to help in the generation of UFC compliant code.
+"""Code generation utilities for UFC (Unified Form-assembly Code) v. 1.0.
 
-Three format strings are defined for each of the classes:
-function, dof_map, finite_element, cell_integral, exterior_facet_integral, and interior_facet_integral
+Three format strings are defined for each of the following UFC classes:
 
-The strings are named 'classname_header', 'classname_implementation', and 'classname_combined'.
-The header and implementation contain the definition and declaration respectively, and are meant to
-be placed in .h and .cpp files, while the combined version is for an implementation within the header.
+    function
+    dof_map
+    finite_element
+    cell_integral
+    exterior_facet_integral
+    interior_facet_integral
 
-Each string has the following format variables:
-'classname', 'members', 'constructor', 'destructor',
-plus one for each interface function with name equal to the function name.
+The strings are named '<classname>_header', '<classname>_implementation',
+and '<classname>_combined'. The header and implementation contain the
+definition and declaration respectively, and are meant to be placed in
+.h and .cpp files, while the combined version is for an implementation
+within a single .h header.
 
-In addition, a utility function generate_code(format_string, format_dict, num_indent_spaces=6) allows
-the user to specify a format dictionary without the constructor, destructor or extra members.
+Each string has the following format variables: 'classname',
+'members', 'constructor', 'destructor', plus one for each interface
+function with name equal to the function name.
 
-See http://www.fenics.org/ufc/ for more information about the UFC project.
+In addition, a utility function
+
+    generate_code(format_string, format_dict, num_indent_spaces=6)
+
+allows the user to specify a format dictionary without the
+constructor, destructor or extra members.
+
+For more information about UFC and the FEniCS project, visit
+
+    http://www.fenics.org/ufc/
+
 """
+
 # -*- coding: utf-8 -*-
-__author__  = "Martin Sandve Alnaes, Hans Petter Langtangen, Anders Logg, Kent-Andre Mardal, and Ola Skavhaug"
-__date__    = "9th January 2007"
+__author__  = "Martin Sandve Alnaes, Hans Petter Langtangen, Anders Logg, Kent-Andre Mardal and Ola Skavhaug"
+__date__    = "2007-01-10"
 __version__ = "1.0-rc1"
+__license__ = "This code is released into the public domain"
 
 UFC_VERSION = __version__
-
 
 shapes = ["interval", "triangle", "quadrilateral", "tetrahedron", "hexahedron"]
 
@@ -31,5 +47,4 @@ from finite_element import *
 from integrals import *
 from form import *
 
-import utility
-
+import utils
