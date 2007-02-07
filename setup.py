40ad4347de291e06c5964ad6a423c63b7c17bb6e
Anders Logg <logg@simula.no>
2007-02-07 10:50:28 +0100

    Generate ufc-1.pc in setup.py script

--- setup.py
+++ setup.py
@@ -3,14 +3,28 @@
 from distutils.core import setup
 import sys
 
+# Version number
+major = 1
+minor = 0
+
+# Set prefix
 try:    prefix = [ item for item in sys.argv[1:] if "--prefix=" in item ][0].split("=")[1]
 except: prefix = "/usr/local"
 
+# Generate pkgconfig file
+file = open("ufc-%d.pc" % major, "w")
+file.write("Name: UFC\n")
+file.write("Version: %d.%d\n" % (major, minor))
+file.write("Description: Unified Form-assembly Code\n")
+file.write("Cflags: -I%s/include\n" % prefix)
+file.close()
+
 setup(name = "UFC",
-      version = "1.0",
+      version = "%d.%d" % (major, minor),
       description = "Unified Form-assembly Code",
       author = "Martin Sandve Alnaes, Hans Petter Langtangen, Anders Logg, Kent-Andre Mardal and Ola Skavhaug",
       url = "http://www.fenics.org/ufc/",
       packages = ["ufc"],
       package_dir= {"ufc": "src/utils/python/ufc"},
-      data_files=[("%s/include" % prefix, ["src/ufc/ufc.h"])])
+      data_files=[("%s/include" % prefix, ["src/ufc/ufc.h"]),
+                  ("%s/lib/pkgconfig" % prefix, ["ufc-%d.pc" % major])])
