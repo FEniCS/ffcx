c6896a0ab1f28b5950c84a0188e80ffd31282b65
Anders Logg <logg@simula.no>
2007-02-28 12:49:14 +0100

    Minor fixes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ major = 1
 minor = 0
 
 # Set prefix
-try:    prefix = [ item for item in sys.argv[1:] if "--prefix=" in item ][0].split("=")[1]
+try:    prefix = [item for item in sys.argv[1:] if "--prefix=" in item][0].split("=")[1]
 except: prefix = "/usr/local"
 
 # Generate pkgconfig file
@@ -23,8 +23,9 @@ setup(name = "UFC",
       version = "%d.%d" % (major, minor),
       description = "Unified Form-assembly Code",
       author = "Martin Sandve Alnaes, Hans Petter Langtangen, Anders Logg, Kent-Andre Mardal and Ola Skavhaug",
+      author_email = "ufc@fenics.org",
       url = "http://www.fenics.org/ufc/",
       packages = ["ufc"],
-      package_dir= {"ufc": "src/utils/python/ufc"},
-      data_files=[("%s/include" % prefix, ["src/ufc/ufc.h"]),
-                  ("%s/lib/pkgconfig" % prefix, ["ufc-%d.pc" % major])])
+      package_dir = {"ufc": "src/utils/python/ufc/"},
+      data_files = [("%s/include" % prefix, ["src/ufc/ufc.h"]),
+                    ("%s/lib/pkgconfig" % prefix, ["ufc-%d.pc" % major])])
