d3991167f98225e53d3972994029ed050261cb23
Anders Logg <logg@simula.no>
2007-04-02 13:34:46 +0200

    Simplify indent() in utils.py (thanks to Martin Alnaes for the tip)

--- src/ffc/common/utils.py
+++ src/ffc/common/utils.py
@@ -1,6 +1,6 @@
 __author__ = "Anders Logg (logg@simula.no)"
-__date__ = "2005-02-04 -- 2007-10-20"
-__copyright__ = "Copyright (C) 2005 Anders Logg"
+__date__ = "2005-02-04 -- 2007-04-02"
+__copyright__ = "Copyright (C) 2005-2007 Anders Logg"
 __license__  = "GNU GPL Version 2"
 
 def pick_first(values):
@@ -18,7 +18,7 @@ def listcopy(l):
         return [object.__class__(object) for object in l]
 
 def permutations(l):
-    "Return a list of all permutations of the given list."
+    "Return a list of all permutations of the given list"
     if len(l) < 1:
         yield l
     for i in range(len(l)):
@@ -28,13 +28,9 @@ def permutations(l):
             yield [pivot] + p
     return
 
-def capall(s):
-    "Return a string in which all characters are capitalized."
-    return "".join([c.capitalize() for c in s])
-
 def indent(s, n):
-    "Indent each row of the given string s with n spaces."
-    indentation = "".join([" " for i in range(n)])
+    "Indent each row of the given string s with n spaces"
+    indentation = " "*n
     return indentation + ("\n" + indentation).join(s.split("\n"))
 
 if __name__ == "__main__":
