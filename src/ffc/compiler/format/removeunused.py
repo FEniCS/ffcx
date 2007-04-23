5757de946339f50103704afb902f56c3fcdef4e0
Anders Logg <logg@simula.no>
2007-04-23 23:39:29 +0200

    Fix bug in checking for unused variables (use regexp)

--- src/ffc/compiler/format/removeunused.py
+++ src/ffc/compiler/format/removeunused.py
@@ -1,15 +1,21 @@
 "Utility function for removing unused variables from a C++ code"
 
 __author__ = "Anders Logg (logg@simula.no)"
-__date__ = "2007-03-07 -- 2007-03-07"
+__date__ = "2007-03-07 -- 2007-04-23"
 __copyright__ = "Copyright (C) 2007 Anders Logg"
 __license__  = "GNU GPL Version 2"
+
+# Python modules
+import re
 
 # FFC common modules
 from ffc.common.debug import *
 
 # Declarations to examine
 types = [["double"], ["const", "double"]]
+
+# Special characters and delimiters
+special_characters = ["+", "-", "*", "/", "=", ".", " ", ";", "(", ")", "\\"]
 
 def remove_unused(code):
     """Remove unused variables from a given C++ code. This is useful
@@ -35,6 +41,8 @@ def remove_unused(code):
         for type in [type for type in types if len(words) > len(type)]:
             variable_type = words[0:len(type)]
             variable_name = words[len(type)]
+            if variable_name in special_characters:
+                continue
             if variable_type == type:
                 variables[variable_name] = (line_number, [])
                 if not variable_name in variable_names:
@@ -43,7 +51,7 @@ def remove_unused(code):
         # Mark line for used variables
         for variable_name in variables:
             (declaration_line, used_lines) = variables[variable_name]
-            if variable_name in line and line_number > declaration_line:
+            if __variable_in_line(variable_name, line) and line_number > declaration_line:
                 variables[variable_name] = (declaration_line, used_lines + [line_number])
 
     # Reverse the order of the variable names (to catch variables used
@@ -63,3 +71,12 @@ def remove_unused(code):
             removed_lines += [declaration_line]
         
     return "\n".join([line for line in lines if not line == None])
+
+def __variable_in_line(variable_name, line):
+    "Check if variable name is used in line"
+    if not variable_name in line:
+        return False
+    for character in special_characters:
+        line = line.replace(character, "\\" + character)
+    delimiter = "[" + ",".join("\\" + c for c in special_characters) + "]"
+    return not re.search(delimiter + variable_name + delimiter, line) == None
