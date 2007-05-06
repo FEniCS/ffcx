cbc80f3d5e915e878a54052e8da3f361d83ca6da
Anders Logg <logg@simula.no>
2007-05-06 22:53:38 +0200

    Minor fixes and comments in preprocessing of index ranges

--- src/ffc/compiler/analysis/ranges.py
+++ src/ffc/compiler/analysis/ranges.py
@@ -5,6 +5,8 @@ __date__ = "2007-04-30 -- 2007-04-30"
 __copyright__ = "Copyright (C) 2007 "
 __license__  = "GNU GPL Version 2"
 
+# Modified by Anders Logg 2007
+
 # Python modules
 import sys
 
@@ -21,26 +23,26 @@ def determine_index_ranges(form):
     "Go through the form and set appropriate index ranges."
     debug("Determining the range of the indices...")
     for monomial in form.monomials:
-        for constant in monomial.constants:
-            # NB: Does this make sense?
-            set_range(constant.number, 0)
+        # Skip constants for now (will be removed and replaced by DG(0) functions)
+        #for constant in monomial.constants:
+        #    set_range(constant.number, 0)
         for coefficient in monomial.coefficients:
-            # NB: Does this make sense?
+            # The index corresponds to the summation space dimension
             set_range(coefficient.n0, coefficient.e0.space_dimension())
             set_range(coefficient.n1, coefficient.e1.space_dimension())
             set_range(coefficient.index, coefficient.e1.space_dimension())
         for transform in monomial.transforms:
-            # The indices of the transforms correspond to the cell_dim.
+            # The indices of the transforms correspond to the cell dimension
             set_range(transform.index0, transform.element.cell_dimension())
             set_range(transform.index1, transform.element.cell_dimension())
         for basisfunction in monomial.basisfunctions:
-            # The index of the basisfunction corresponds to the space_dim.
+            # The index of the basisfunction corresponds to the space dimension
             set_range(basisfunction.index, basisfunction.element.space_dimension())
-            for c in basisfunction.component:
-                # The component corresponds to the value_dim.
-                set_range(c, basisfunction.element.value_dimension(0)) # FIXME
+            for i in range(len(basisfunction.component)):
+                # The component corresponds to the value dimension
+                set_range(basisfunction.component[i], basisfunction.element.value_dimension(i))
             for d in basisfunction.derivatives:
-                # The index of the derivatives corresponds to the cell_dim.
+                # The index of the derivatives corresponds to the cell dimension
                 set_range(d.index, d.element.cell_dimension())
     debug("done")
 
@@ -50,4 +52,4 @@ def set_range(index, dimension):
         if not index.range: # i.e. index.range == none:
             index.range = range(dimension)
     else:
-        raise FormError("Cannot set_range for non-indices!")
+        raise FormError("Cannot set range for non-indices!")
