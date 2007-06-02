eff5be9f6929db9d0f54f443dd265d2a199f450f
Anders Logg <logg@simula.no>
2007-06-03 01:00:47 +0200

    Remove details on command-line interface from ffc --help and refer to man page (so that the information does not need to be kept up to date in both places). From now on, the man page is the authoritative source on the FFC command-line interface.

--- src/ffc/common/constants.py
+++ src/ffc/common/constants.py
@@ -1,9 +1,9 @@
 __author__ = "Anders Logg (logg@simula.no)"
-__date__ = "2005-05-20 -- 2007-05-07"
+__date__ = "2005-05-20 -- 2007-06-03"
 __copyright__ = "Copyright (C) 2005-2007 Anders Logg"
 __license__  = "GNU GPL Version 2"
 
-FFC_VERSION        = "0.3.5"
+FFC_VERSION        = "0.4.0-pre1"
 FFC_LANGUAGE       = "ufc"
 FFC_DEBUG_LEVEL    = 0
 FFC_OPTIONS        = {"optimize": False,
