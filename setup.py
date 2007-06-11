d94ec0789585294bc1d772d8596b8ce5b22e85bb
Ola Skavhaug <skavhaug@simula.no>
2007-06-11 11:24:26 +0200

    Use platform independent constructs in setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@
 
 from distutils.core import setup
 from os import chdir
-
-chdir("src")
+from os.path import join
 
 setup(name = "FFC",
       version = "0.3.5-dev",
@@ -11,20 +10,21 @@ setup(name = "FFC",
       author = "Anders Logg",
       author_email = "logg@simula.no",
       url = "http://www.fenics.org/ffc/",
-      packages = ["ffc/",
-                  "ffc/common",
-                  "ffc/fem",
-                  "ffc/compiler",
-                  "ffc/compiler/optimization",
-                  "ffc/compiler/format",
-                  "ffc/compiler/codegeneration",
-                  "ffc/compiler/codegeneration/common",
-                  "ffc/compiler/codegeneration/quadrature",
-                  "ffc/compiler/codegeneration/tensor",
-                  "ffc/compiler/representation",
-                  "ffc/compiler/representation/quadrature",
-                  "ffc/compiler/representation/tensor",
-                  "ffc/compiler/language",
-                  "ffc/compiler/analysis"],
-      scripts = ["bin/ffc"],
-      data_files = [("share/man/man1", ["../doc/man/man1/ffc.1.gz"])])
+      packages = ["ffc",
+                  "ffc.common",
+                  "ffc.fem",
+                  "ffc.compiler",
+                  "ffc.compiler.optimization",
+                  "ffc.compiler.format",
+                  "ffc.compiler.codegeneration",
+                  "ffc.compiler.codegeneration.common",
+                  "ffc.compiler.codegeneration.quadrature",
+                  "ffc.compiler.codegeneration.tensor",
+                  "ffc.compiler.representation",
+                  "ffc.compiler.representation.quadrature",
+                  "ffc.compiler.representation.tensor",
+                  "ffc.compiler.language",
+                  "ffc.compiler.analysis"],
+      package_dir={"ffc": join("src", "ffc")},
+      scripts = [join("src","bin","ffc")],
+      data_files = [(join("share", "man", "man1"), [join("doc", "man", "man1", "ffc.1.gz")])])
