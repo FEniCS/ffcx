3a60aeecc6d394b37bf3e6fa2d6f13687a7a282a
Garth N. Wells <g.n.wells@tudelft.nl>
2007-06-22 09:30:52 +0200

    Prepare for release.

--- src/ffc/common/constants.py
+++ src/ffc/common/constants.py
@@ -1,9 +1,9 @@
 __author__ = "Anders Logg (logg@simula.no)"
-__date__ = "2005-05-20 -- 2007-06-20"
+__date__ = "2005-05-20 -- 2007-06-22"
 __copyright__ = "Copyright (C) 2005-2007 Anders Logg"
 __license__  = "GNU GPL Version 2"
 
-FFC_VERSION        = "0.4.0"
+FFC_VERSION        = "0.4.1"
 FFC_LANGUAGE       = "ufc"
 FFC_DEBUG_LEVEL    = 0
 FFC_OPTIONS        = {"optimize": False,
