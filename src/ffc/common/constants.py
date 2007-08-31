19ddc726851534b1b9c4246ad6becfead1c94953
Anders Logg <logg@simula.no>
2007-08-31 11:54:25 +0200

    Updates for version 0.4.2

--- src/ffc/common/constants.py
+++ src/ffc/common/constants.py
@@ -1,9 +1,9 @@
 __author__ = "Anders Logg (logg@simula.no)"
-__date__ = "2005-05-20 -- 2007-06-22"
+__date__ = "2005-05-20 -- 2007-08-31"
 __copyright__ = "Copyright (C) 2005-2007 Anders Logg"
 __license__  = "GNU GPL version 3 or any later version"
 
-FFC_VERSION        = "0.4.1"
+FFC_VERSION        = "0.4.2"
 FFC_LANGUAGE       = "ufc"
 FFC_DEBUG_LEVEL    = 0
 FFC_OPTIONS        = {"optimize": False,
