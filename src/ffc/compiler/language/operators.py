a04871d4c0b5339a44165badcc2058df65cd6177
Anders Logg <logg@simula.no>
2007-12-20 16:55:41 +0100

    Bug fix for operator sqrt() when applied to a float

--- src/ffc/compiler/language/operators.py
+++ src/ffc/compiler/language/operators.py
@@ -2,7 +2,7 @@
 based on the basic form algebra operations."""
 
 __author__ = "Anders Logg (logg@simula.no)"
-__date__ = "2005-09-07 -- 2007-03-20"
+__date__ = "2005-09-07 -- 2007-12-20"
 __copyright__ = "Copyright (C) 2005-2007 Anders Logg"
 __license__  = "GNU GPL version 3 or any later version"
 
@@ -245,6 +245,8 @@ def jump(v, n = None):
 
 def sqrt(v):
     "Return the square root (take square root of coefficients)"
+    if isinstance(v, float):
+        return numpy.sqrt(v)
     return Form(v).sqrt()
 
 def modulus(v):
