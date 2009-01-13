362922196493e42685e3fedbd4bf3adce9b4c827
Anders Logg <logg@simula.no>
2009-01-13 22:57:39 +0100

    Updates for new UFL log.py:
      ffc_logger = Logger("FFC")
    
      for foo in log_functions:
        exec("%s = lambda *message : ffc_logger.%s(*message)" % (foo, foo))

--- ffc/common/log.py
+++ ffc/common/log.py
@@ -6,12 +6,15 @@ is a wrapper for the standard Python logging module.
 """
 
 __author__ = "Anders Logg (logg@simula.no)"
-__date__ = "2009-01-12 -- 2009-01-12"
+__date__ = "2009-01-12 -- 2009-01-13"
 __copyright__ = "Copyright (C) 2009 Anders Logg"
 __license__  = "GNU GPL version 3 or any later version"
 
-# Import UFL log functions
 from ufl.log import *
 
-# Set up logger
-set_logger("FFC")
+# Create FFC logger
+ffc_logger = Logger("FFC")
+
+# Create FFC global log functions
+for foo in log_functions:
+    exec("%s = lambda *message : ffc_logger.%s(*message)" % (foo, foo))
