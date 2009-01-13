2ee0dab3607dc4ce8f773530ba0fd88ba4bbe9af
Johan Hake <hake@simula.no>
2009-01-13 11:42:11 +0100

    Added a swig interface file, ufc.i   - In this shared_ptr typemaps is added for the main classes in ufc.h.   - Changed name of the python module ufc/ to ufc_utils/
    NOTE:
      - If installing from source, i.e., python setup.py install, you need to
        delete your old build before you install it again. Otherwise you will reinstall
        the old python module ufc/

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, Extension
 from distutils import sysconfig
 from os.path import join as pjoin
-import sys
+from os.path import pardir
+import sys, os
 
 # Version number
 major = 1
@@ -18,6 +19,7 @@ except:
         prefix = sys.argv[sys.argv.index('--prefix')+1]
     except:
         prefix = sys.prefix
+
 print "Installing UFC under %s..." % prefix
 
 # Generate pkgconfig file
@@ -29,13 +31,33 @@ file.write("Cflags: -I%s\n" % repr(pjoin(prefix,"include"))[1:-1])
 # FIXME: better way for this? ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 file.close()
 
+# Create ufc.i file
+#ufc_i_file = open(pjoin("src","ufc","ufc.i.in"),"r").read()%{"ufc_path":pjoin(prefix,"include","")}
+#file = open(pjoin("src","ufc","ufc.i"),"w")
+#file.write(ufc_i_file)
+#file.close()
+
+
+# Run swig on the ufc.i file
+# FIXME: Add a way to set NO_SHARED_PTR in swig command
+os.chdir(pjoin("src","ufc"))
+os.system("swig -python -c++ -fcompact -O -I. -small ufc.i")
+os.chdir(os.pardir); os.chdir(os.pardir)
+sources = [pjoin("src","ufc","ufc_wrap.cxx")]
+
 setup(name = "UFC",
       version = "%d.%d" % (major, minor),
       description = "Unified Form-assembly Code",
       author = "Martin Sandve Alnaes, Hans Petter Langtangen, Anders Logg, Kent-Andre Mardal and Ola Skavhaug",
       author_email = "ufc@fenics.org",
       url = "http://www.fenics.org/ufc/",
-      packages = ["ufc"],
-      package_dir = {"ufc": pjoin("src","utils","python","ufc")},
+      packages = ["","ufc_utils"],
+      package_dir = {"":pjoin("src","ufc"),
+                     "ufc_utils": pjoin("src","utils","python","ufc")},
       data_files = [("include", [pjoin("src","ufc","ufc.h")]),
-                    (pjoin("lib","pkgconfig"), ["ufc-%d.pc" % major])])
+                    (pjoin("lib","pkgconfig"), ["ufc-%d.pc" % major]),
+                    (pjoin("include","swig"),[pjoin("src","ufc","ufc.i")])],
+      ext_modules = [Extension("_ufc",
+                     sources,
+                     include_dirs=[pjoin("src","ufc")]
+                               )])
