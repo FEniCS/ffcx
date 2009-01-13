9ccbb3045ffafa700bfcbc85f5ea64fee8ecbea5
Kent-Andre Mardal <kent-and@simula.no>
2009-01-13 21:50:13 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Extension
+from distutils.core import setup
 from distutils import sysconfig
 from os.path import join as pjoin
-from os.path import pardir
-import sys, os
+import sys
 
 # Version number
 major = 1
@@ -19,7 +18,6 @@ except:
         prefix = sys.argv[sys.argv.index('--prefix')+1]
     except:
         prefix = sys.prefix
-
 print "Installing UFC under %s..." % prefix
 
 # Generate pkgconfig file
@@ -31,26 +29,14 @@ file.write("Cflags: -I%s\n" % repr(pjoin(prefix,"include"))[1:-1])
 # FIXME: better way for this? ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 file.close()
 
-# Run swig on the ufc.i file
-# FIXME: Add a way to set NO_SHARED_PTR in swig command
-os.chdir(pjoin("src","ufc"))
-os.system("swig -python -c++ -fcompact -O -I. -small ufc.i")
-os.chdir(os.pardir); os.chdir(os.pardir)
-sources = [pjoin("src","ufc","ufc_wrap.cxx")]
-
 setup(name = "UFC",
       version = "%d.%d" % (major, minor),
       description = "Unified Form-assembly Code",
       author = "Martin Sandve Alnaes, Hans Petter Langtangen, Anders Logg, Kent-Andre Mardal and Ola Skavhaug",
       author_email = "ufc@fenics.org",
       url = "http://www.fenics.org/ufc/",
-      packages = ["","ufc_utils"],
-      package_dir = {"":pjoin("src","ufc"),
-                     "ufc_utils": pjoin("src","utils","python","ufc")},
+      packages = ["ufc"],
+      package_dir = {"ufc": pjoin("src","utils","python","ufc")},
       data_files = [("include", [pjoin("src","ufc","ufc.h")]),
                     (pjoin("lib","pkgconfig"), ["ufc-%d.pc" % major]),
-                    (pjoin("include","swig"),[pjoin("src","ufc","ufc.i")])],
-      ext_modules = [Extension("_ufc",
-                     sources,
-                     include_dirs=[pjoin("src","ufc")]
-                               )])
+                    (pjoin("include","swig"),[pjoin("src","ufc","ufc.i")])])
