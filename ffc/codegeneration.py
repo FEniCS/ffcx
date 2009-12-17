27c958522e8f8c47758590e3c903cbeeb6c222c9
Anders Logg <logg@simula.no>
2009-12-17 11:42:57 +0100

    Simplify code generatio by prefetching format["return"]

--- ffc/codegeneration.py
+++ ffc/codegeneration.py
@@ -16,11 +16,15 @@ from cpp import format
 def generate_element_code(ir):
     "Generate code for finite element from intermediate representation."
 
+    # Prefetch formatting to speedup code generation
+    ret = format["return"]
+
+    # Generate code
     code = {}
-    code["signature"] = format["return"]('"%s"' % ir["signature"])
+    code["signature"] = ret('"%s"' % ir["signature"])
     code["cell_shape"] = ""
-    code["space_dimension"] = format["return"](ir["space_dimension"])
-    code["value_rank"] = format["return"](ir["value_rank"])
+    code["space_dimension"] = ret(ir["space_dimension"])
+    code["value_rank"] = ret(ir["value_rank"])
     code["value_dimension"] = ""
     code["evaluate_basis"] = ""
     code["evaluate_basis_all"] = ""
@@ -29,7 +33,7 @@ def generate_element_code(ir):
     code["evaluate_dof"] = ""
     code["evaluate_dofs"] = ""
     code["interpolate_vertex_values"] = ""
-    code["num_sub_elements"] = ""
+    code["num_sub_elements"] = ret(ir["num_sub_elements"])
     code["create_sub_element"] = ""
 
     debug_code(code, "finite_element")
@@ -39,25 +43,27 @@ def generate_element_code(ir):
 def generate_dofmap_code(ir):
     "Generate code for dofmap from intermediate representation."
 
-    print ir
+    # Prefetch formatting to speedup code generation
+    ret = format["return"]
 
+    # Generate code
     code = {}
-    code["signature"] = format["return"](ir["signature"])
+    code["signature"] = ret(ir["signature"])
     code["needs_mesh_entities"] = ""
     code["init_mesh"] = ""
     code["init_cell"] = ""
     code["init_cell_finalize"] = ""
-    code["global_dimension"] = ""
-    code["local_dimension"] = format["return"](ir["local_dimension"])
-    code["max_local_dimension"] = ""
-    code["geometric_dimension"] = format["return"](ir["geometric_dimension"])
-    code["num_facet_dofs"] = format["return"](ir["local_dimension"])
+    code["global_dimension"] = ret("_global_dimension")
+    code["local_dimension"] = ret(ir["local_dimension"])
+    code["max_local_dimension"] = ret(ir["max_local_dimension"])
+    code["geometric_dimension"] = ret(ir["geometric_dimension"])
+    code["num_facet_dofs"] = ret(ir["local_dimension"])
     code["num_entity_dofs"] = ""
     code["tabulate_dofs"] = ""
     code["tabulate_facet_dofs"] = ""
     code["tabulate_entity_dofs"] = ""
     code["tabulate_coordinates"] = ""
-    code["num_sub_dof_maps"] = ""
+    code["num_sub_dof_maps"] = ret(ir["num_sub_dof_maps"])
     code["dof_map* create_sub_dof_map"] = ""
 
     debug_code(code, "dofmap")
