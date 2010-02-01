871169595fb8e3bdc05eec36f999464ef4f75d15
Kristian B. Ølgaard <k.b.oelgaard@gmail.com>
2010-02-01 16:01:12 +0100

    Added unit tests for evaluate_basis.

---
+++ test/unit/evaluate_basis/cppcode.py
@@ -0,0 +1,160 @@
+"This module provides simple C++ code for verification of UFC code."
+
+__author__ = "Kristian B. Oelgaard (k.b.oelgaard@gmail.com)"
+__date__ = "2010-01-18"
+__copyright__ = "Copyright (C) 2010 Kristian B. Oelgaard"
+__license__  = "GNU GPL version 3 or any later version"
+
+#evaluate_basis_code = """\
+##include <iostream>
+##include <ufc.h>
+##include "test.h"
+
+#int main()
+#{
+#  // Create element
+#  %(element)s element;
+
+#  // Size of dof_values
+#  // FIXME: This will not work for TensorElements
+#  int N = element.value_dimension(0);
+
+#  // Create values
+#  double* dof_values = new double[N];
+#  for (unsigned int i = 0; i < N; i++)
+#    dof_values[i] = 0.0;
+
+#  // Create cell and fill with some arbitrary data
+#  double cell_coordinates[8][3] = {{0.90, 0.34, 0.45},
+#                                   {0.56, 0.76, 0.83},
+#                                   {0.98, 0.78, 0.19},
+#                                   {0.12, 0.56, 0.66},
+#                                   {0.96, 0.78, 0.63},
+#                                   {0.11, 0.35, 0.49},
+#                                   {0.51, 0.88, 0.65},
+#                                   {0.98, 0.45, 0.01}};
+#  ufc::cell cell;
+#  cell.coordinates = new double * [8];
+#  for (int i = 0; i < 8; i++)
+#  {
+#    cell.coordinates[i] = new double[3];
+#    for (int j = 0; j < 3; j++)
+#      cell.coordinates[i][j] = cell_coordinates[i][j];
+#  }
+
+#  // Random coordinates where we want to evaluate the basis functions
+#  double coordinates[3] = {0.32, 0.51, 0.05};
+
+#  // Loop element space dimension and call evaluate_basis.
+#  for (unsigned int i = 0; i < element.space_dimension(); i++)
+#  {
+#    element.evaluate_basis(i, dof_values, coordinates, cell);
+#    // Print values
+#    for (unsigned int j = 0; j < N; j++)
+#      std::cout << dof_values[j] << " ";
+#  }
+#  std::cout << std::endl;
+#  return 0;
+#}
+#"""
+
+evaluate_basis_code_fiat = """\
+#include <iostream>
+#include <ufc.h>
+#include <cstdlib>
+#include "test.h"
+
+int main(int argc, char* argv[])
+{
+  // Create element
+  %(element)s element;
+
+
+  // Get derivative order
+  unsigned int n = std::atoi(argv[1]);
+
+  // Value dimension
+  // FIXME: This will not work for TensorElements
+  int N = element.value_dimension(0);
+
+  // Compute number of derivatives.
+  unsigned int  num_derivatives = 1;
+  for (unsigned int r = 0; r < n; r++)
+  {
+    num_derivatives *= %(dim)d;
+  }
+
+  // Create values
+  unsigned int num_dof_vals = N*num_derivatives;
+  double* dof_values = new double[num_dof_vals];
+  for (unsigned int i = 0; i < num_dof_vals; i++)
+    dof_values[i] = 0.0;
+
+  %(cell_ref_coords)s
+  ufc::cell cell;
+  cell.coordinates = new double * [%(num_coords)d];
+  for (int i = 0; i < %(num_coords)d; i++)
+  {
+    cell.coordinates[i] = new double[%(dim)d];
+    for (int j = 0; j < %(dim)d; j++)
+      cell.coordinates[i][j] = cell_ref_coords[i][j];
+  }
+
+  // Random points where we want to evaluate the basis functions
+  // coordinates of dofs and three arbitrary points on the reference cell.
+  double points[%(num_points)d][%(dim)d] = %(points)s
+
+  // Init array of coordinates
+  double coordinates[3] = {0,0,0};
+
+  std::cout.precision(8);
+  std::cout.setf(std::ios::fixed);
+
+  // If we're testing evaluate_basis, loop all points.
+  if (n == 0)
+  {
+    for (unsigned int p = 0; p < %(num_points)d; p++)
+    {
+      for (unsigned int d = 0; d < %(dim)d; d++)
+      {
+        coordinates[d] = points[p][d];
+      }
+      // Loop element space dimension and call evaluate_basis.
+      for (unsigned int i = 0; i < element.space_dimension(); i++)
+      {
+        element.evaluate_basis(i, dof_values, coordinates, cell);
+
+        // Print values
+        for (unsigned int j = 0; j < num_dof_vals; j++)
+          std::cout << dof_values[j] << " ";
+      }
+      std::cout << std::endl;
+    }
+  }
+  else
+  {
+    // Else loop the arbitrary 3 points, otherwise the number of tests explode
+    // with the element.space_dimension()^2.
+    for (unsigned int p = element.space_dimension(); p < %(num_points)d; p++)
+    {
+      for (unsigned int d = 0; d < %(dim)d; d++)
+      {
+        coordinates[d] = points[p][d];
+      }
+      // Loop element space dimension and call evaluate_basis.
+      for (unsigned int i = 0; i < element.space_dimension(); i++)
+      {
+        element.evaluate_basis_derivatives(i, n, dof_values, coordinates, cell);
+
+        // Print values
+        for (unsigned int j = 0; j < num_dof_vals; j++)
+          std::cout << dof_values[j] << " ";
+      }
+      std::cout << std::endl;
+    }
+  }
+
+  return 0;
+}
+"""
+
---
+++ test/unit/evaluate_basis/test_common.py
@@ -0,0 +1,171 @@
+__author__ = "Kristian B. Oelgaard (k.b.oelgaard@gmail.com)"
+__date__ = "2010-01-29"
+__copyright__ = "Copyright (C) 2010 Kristian B. Oelgaard"
+__license__  = "GNU GPL version 3 or any later version"
+
+# Last changed: 2010-02-01
+
+from ffc.log import info, info_red, info_blue, info_green
+import commands
+import numpy
+import os
+
+tol = 1e-14
+crit_tol = 1e-8
+
+def xcomb(items, n):
+    "Create n-tuples with combinations of items."
+    if n==0: yield []
+    else:
+        for i in xrange(len(items)):
+            for cc in xcomb(items[:i]+items[i+1:],n-1):
+                yield [items[i]]+cc
+
+# Global log file
+def log_error(message, log_file):
+    "Log error message."
+    log = open(os.path.join(os.pardir, log_file), "a")
+    log.write("\n" + "-"*79 + "\n" + message + "\n" + "-"*79 + "\n")
+    log.close()
+
+def print_results(num_tests, ffc_fail, gcc_fail, run_fail, dif_cri, dif_acc, correct):
+    "Check print summary."
+
+    num_ffc = len(ffc_fail)
+    num_gcc = len(gcc_fail)
+    num_run = len(run_fail)
+    num_cri = len(dif_cri)
+    num_acc = len(dif_acc)
+    num_cor = len(correct)
+
+    if ffc_fail == gcc_fail == run_fail == dif_cri == dif_acc == []:
+        info_green("\nAll %d tests OK" % num_tests)
+        return 0
+
+    num_tests = str(num_tests)
+    num_tot = str(num_ffc + num_gcc + num_run + num_cor + num_cri + num_acc)
+    num_ffc = str(num_ffc)
+    num_gcc = str(num_gcc)
+    num_run = str(num_run)
+    num_cor = str(num_cor)
+    num_cri = str(num_cri)
+    num_acc = str(num_acc)
+    num_ffc = " "*(len(num_tests) - len(num_ffc)) + num_ffc
+    num_gcc = " "*(len(num_tests) - len(num_gcc)) + num_gcc
+    num_run = " "*(len(num_tests) - len(num_run)) + num_run
+    num_cor = " "*(len(num_tests) - len(num_cor)) + num_cor
+    num_cri = " "*(len(num_tests) - len(num_cri)) + num_cri
+    num_acc = " "*(len(num_tests) - len(num_acc)) + num_acc
+    num_tot = " "*(len(num_tests) - len(num_tot)) + num_tot
+
+    info("\n\n*************** SUMMARY ***************")
+    info("\n  Number of tests:                  " + num_tests)
+    info("\n  Num ffc fail:                     " + num_ffc)
+    info("  Num gcc fail:                     " + num_gcc)
+    info("  Num run fail:                     " + num_run)
+    info(("  Num correct:        (tol. %g): " % tol) + num_cor)
+    info(("  Num diff. critical: (tol. %g): " % crit_tol) + num_cri)
+    info("  Num diff. acceptable:             " + num_acc)
+    info("  Total:                            " + num_tot)
+    info("")
+    # Return 0 if there was only acceptable errors.
+    if ffc_fail == gcc_fail == run_fail == dif_cri == []:
+        return 0
+    return 1
+
+def compile_element(ufl_element, ffc_fail, log_file):
+    "Create UFL form file with a single element in it and compile it with FFC"
+    f = open("test.ufl", "w")
+    f.write("element = " + repr(ufl_element))
+    f.close()
+    error, output = commands.getstatusoutput("ffc test.ufl")
+    if error:
+        info_red("FFC compilation failed.")
+        log_error("element: %s,\n%s\n" % (str(ufl_element), output), log_file)
+        ffc_fail.append(str(ufl_element))
+    return error
+
+def get_element_name(ufl_element):
+    "Extract relevant element name from header file."
+    f = open("test.h")
+    lines = f.readlines()
+    f.close()
+
+    signature = repr(ufl_element)
+    name = None
+    for e, l in enumerate(lines):
+        if "class" in l and "finite_element" in l:
+            name = l
+        if signature in l:
+            break
+    if name is None:
+        raise RuntimeError("No finite element class found")
+    return name.split()[1][:-1]
+
+def compile_gcc_code(ufl_element, code, gcc_fail, log_file):
+
+    # Write code.
+    f = open("evaluate_basis.cpp", "w")
+    f.write(code)
+    f.close()
+
+    # Compile g++ code
+    c = "g++ `pkg-config --cflags ufc-1` -Wall -Werror -o evaluate_basis evaluate_basis.cpp"
+    error, output = commands.getstatusoutput(c)
+    if error:
+        info_red("GCC compilation failed.")
+        log_error("element: %s,\n%s\n" % (str(ufl_element), output), log_file)
+        gcc_fail.append(str(ufl_element))
+        return error
+
+def run_code(ufl_element, deriv_order, run_fail, log_file):
+    "Compute values of basis functions for given element."
+
+    # Run compiled code and get values
+    error, output = commands.getstatusoutput("./evaluate_basis %d" % deriv_order)
+    if error:
+        info_red("Runtime error (segmentation fault?).")
+        log_error("element: %s,\n%s\n" % (str(ufl_element), output), log_file)
+        run_fail.append(str(ufl_element))
+        return None
+    values = [[float(value) for value in line.split(" ") if value] for line in output.split("\n")]
+    return numpy.array(values)
+
+def verify_values(ufl_element, ref_values, ffc_values, dif_cri, dif_acc, correct, log_file):
+    "Check the values from evaluate_basis*() against some reference values."
+
+    num_tests = len(ffc_values)
+    if num_tests != len(ref_values):
+        raise RuntimeError("The number of computed values is not equal to the number of reference values.")
+
+    errors = [str(ufl_element)]
+    for deriv_order in range(num_tests):
+        s = ""
+        if deriv_order == 0:
+            s = "  evaluate_basis()"
+        else:
+            s = "  evaluate_basis_derivatives(), order = %d" % deriv_order
+        info(s)
+        e = abs(ffc_values[deriv_order] - ref_values[deriv_order])
+        error = e.max()
+        if error > tol:
+            if error >  crit_tol:
+                m = "  error: %s (crit_tol: %s)" % (str(error), str(crit_tol))
+                info_red(m)
+                dif_cri.append(str(ufl_element))
+                s = s + "\n" + m
+            else:
+                m = "  error: %s (tol: %s)" % (str(error), str(tol))
+                info_blue(m)
+                dif_acc.append(str(ufl_element))
+                s = s + "\n" + m
+            errors.append(s)
+        else:
+            info_green("  OK")
+            correct.append(str(ufl_element))
+    # Log errors if any
+    if len(errors) > 1:
+        log_error("\n".join(errors), log_file)
+
+    return num_tests
+
