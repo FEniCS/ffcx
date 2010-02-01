98a82908327a9f8352090999081eb9efed2811d7
Anders Logg <logg@simula.no>
2010-02-01 09:33:15 +0100

    Import default_parameters in main interface

--- ffc/__init__.py
+++ ffc/__init__.py
@@ -4,11 +4,12 @@ FEniCS Form Compiler (FFC)
 
 FFC compiles finite element variational forms into C++ code.
 
-The interface consists of the following three functions:
+The interface consists of the following functions:
 
-  compile_form    - Compilation of forms
-  compile_element - Compilation of finite elements
-  jit             - Just-In-Time compilation of forms and elements
+  compile_form       - Compilation of forms
+  compile_element    - Compilation of finite elements
+  jit                - Just-In-Time compilation of forms and elements
+  default_parameters - Default parameter values for FFC
 """
 
 # Import compiler functions
@@ -16,3 +17,6 @@ from ffc.compiler import compile_form, compile_element
 
 # Import JIT compiler
 from ffc.jit import jit
+
+# Import default parameters
+from parameters import default_parameters
