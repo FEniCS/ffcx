f410a70afc4e92b33a2681e55fba6f789eb21867
Kristian B. Ølgaard <k.b.oelgaard@gmail.com>
2010-02-16 09:23:41 +0100

    Print info during quadrature optimisation stage.

--- ffc/quadrature/quadratureoptimization.py
+++ ffc/quadrature/quadratureoptimization.py
@@ -17,13 +17,16 @@ def optimize_integral_ir(ir):
         num_facets = ir["num_facets"]
         geo_consts = ir["geo_consts"]
         if domain_type == "cell":
+            info("Optimising expressions for cell integral")
             _optimise_integral(integrals, geo_consts)
         elif domain_type == "exterior_facet":
             for i in range(num_facets):
+                info("Optimising expressions for facet integral %d" % i)
                 _optimise_integral(integrals[i], geo_consts)
         elif domain_type == "interior_facet":
             for i in range(num_facets):
                 for j in range(num_facets):
+                    info("Optimising expressions for facet integral (%d, %d)" % (i, j))
                     _optimise_integral(integrals[i][j], geo_consts)
         else:
             error("Unhandled domain type: " + str(domain_type))
@@ -43,6 +46,4 @@ def _optimise_integral(integral, geo_consts):
                 data[2] = t_set
                 new_terms[key] = data
         terms = new_terms
-
-
 
