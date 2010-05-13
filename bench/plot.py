6f38073ef911510e4fbb4312b1bb993ef42f1dd9
Anders Logg <logg@simula.no>
2010-05-13 22:12:57 +0200

    Add benchmark results. Test cases are heavily skewed towards tensor representation as a result of the cases where quadrature outperforms tensor representation are hard to run (with tensor representation).

--- bench/plot.py
+++ bench/plot.py
@@ -18,22 +18,38 @@ for line in open("bench.log", "r").read().split("\n"):
     if not test_option in results[form]:
         results[form][test_option] = ([], [])
     results[form][test_option][0].append(int(degree))
-    results[form][test_option][1].append(timing)
+    results[form][test_option][1].append(float(timing))
 
 # Plot results
 forms = sorted([form for form in results])
-test_options = ["-r tensor", "-r tensor -O", "-r quadrature", "-r quadrature -O"]
+test_options = ["-r quadrature", "-r quadrature -O", "-r tensor", "-r tensor -O"]
 bullets = ["x-", "o-", "*-", "s-"]
 for (i, form) in enumerate(forms):
     figure(i)
+
+    # Plot timings
+    subplot(121)
     for (j, test_option) in enumerate(test_options):
         q, t = results[form][test_option]
         semilogy(q, t, bullets[j])
         hold(True)
     legend(test_options, loc="upper left")
     grid(True)
-    title(form)
     xlabel('degree')
-    ylabel('CPU time')
+    ylabel(form)
+    title('CPU time')
+
+    # Plot speedups
+    subplot(122)
+    q0, t0 = results[form]["-r quadrature"]
+    for (j, test_option) in enumerate(test_options):
+        q, t = results[form][test_option]
+        t = [t0[k] / t[k] for k in range(len(t))]
+        semilogy(q, t, bullets[j])
+        hold(True)
+    legend(test_options, loc="upper left")
+    grid(True)
+    xlabel('degree')
+    title("Speedup vs '-r quadrature'")
 
 show()
