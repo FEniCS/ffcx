b960a7df0026c72b2247cd7149eea7f01ed30066
Johan Hake <hake.dev@gmail.com>
2010-08-19 09:34:48 -0700

    Added an __init__ file to the python interface of ufc.

---
+++ src/ufc/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Johan Hake (hake.dev@gmail.com)"
+__copyright__ = "Copyright (C) 2010 Johan Hake"
+__date__ = "2010-08-19 -- 2010-08-19"
+__license__  = "Released to the public domain"
+
+# Import Python versions of the abstract classes in the UFC interface
+from ufc import (form, finite_element, dof_map, cell_integral,
+                 exterior_facet_integral, interior_facet_integral)
