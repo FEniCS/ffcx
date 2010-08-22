378250ad7d2eb0800c284dc47dff52cc5eb25526
Garth N. Wells <gnw20@cam.ac.uk>
2010-08-22 01:09:25 +0100

    Add some missing functions to __init__.py.

--- src/ufc/__init__.py
+++ src/ufc/__init__.py
@@ -5,4 +5,5 @@ __license__  = "Released to the public domain"
 
 # Import Python versions of the abstract classes in the UFC interface
 from ufc import (function, form, finite_element, dof_map, cell_integral,
-                 exterior_facet_integral, interior_facet_integral)
+                 exterior_facet_integral, interior_facet_integral, 
+                 __version__, __swigversion__)
