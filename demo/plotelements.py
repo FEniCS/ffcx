91fe21af0374dad9cd404092c0c76a6309921378
Anders Logg <logg@simula.no>
2010-12-08 17:38:04 +0100

    Cleanup in plot demo

--- demo/plotelements.py
+++ demo/plotelements.py
@@ -8,37 +8,36 @@ __license__  = "GNU GPL version 3 or any later version"
 from ufl import *
 from ffc import *
 
-
 #element = FiniteElement("Argyris", triangle, 5)
 
 #element = FiniteElement("Arnold-Winther", triangle)
 
 #element = FiniteElement("Brezzi-Douglas-Marini", triangle, 3)
-#element = FiniteElement("Brezzi-Douglas-Marini", tetrahedron, 3)
-
-#element = FiniteElement("Discontinuous Lagrange", triangle, 3)
-element = FiniteElement("Discontinuous Lagrange", tetrahedron, 3)
-
-#element = FiniteElement("Hermite", triangle, None)
-#element = FiniteElement("Hermite", tetrahedron, None)
-
-#element = FiniteElement("Lagrange", triangle, 2)
-#element = FiniteElement("Lagrange", tetrahedron, 5)
-
-#element = FiniteElement("Mardal-Tai-Winther", triangle, None)
-
-#element = FiniteElement("Morley", triangle, None)
-
-#element = FiniteElement("Nedelec 1st kind H(curl)", triangle, 3)
-#element = FiniteElement("Nedelec 1st kind H(curl)", tetrahedron, 3)
-
-#element = FiniteElement("Nedelec 2nd kind H(curl)", triangle, 1)
-#element = FiniteElement("Nedelec 2nd kind H(curl)", tetrahedron, 3)
+element = FiniteElement("Brezzi-Douglas-Marini", tetrahedron, 3)
 
 #element = FiniteElement("Crouzeix-Raviart", triangle, 1)
 #element = FiniteElement("Crouzeix-Raviart", tetrahedron, 1)
 
-#element = FiniteElement("Raviart-Thomas", triangle, 2)
-#element = FiniteElement("Raviart-Thomas", tetrahedron, 1)
+#element = FiniteElement("Discontinuous Lagrange", triangle, 3)
+#element = FiniteElement("Discontinuous Lagrange", tetrahedron, 3)
+
+#element = FiniteElement("Hermite", triangle)
+#element = FiniteElement("Hermite", tetrahedron)
+
+#element = FiniteElement("Lagrange", triangle, 3)
+#element = FiniteElement("Lagrange", tetrahedron, 3)
+
+#element = FiniteElement("Mardal-Tai-Winther", triangle)
+
+#element = FiniteElement("Morley", triangle)
+
+#element = FiniteElement("Nedelec 1st kind H(curl)", triangle, 3)
+#element = FiniteElement("Nedelec 1st kind H(curl)", tetrahedron, 3)
+
+#element = FiniteElement("Nedelec 2nd kind H(curl)", triangle, 3)
+#element = FiniteElement("Nedelec 2nd kind H(curl)", tetrahedron, 3)
+
+#element = FiniteElement("Raviart-Thomas", triangle, 3)
+#element = FiniteElement("Raviart-Thomas", tetrahedron, 3)
 
 plot(element)
