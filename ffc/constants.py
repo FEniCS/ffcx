afae94bbac2dcbc8fcf61edb57eaf52dfa1124d5
Anders Logg <logg@simula.no>
2011-02-23 20:11:44 +0100

    Updated references for new version number

--- ffc/constants.py
+++ ffc/constants.py
@@ -4,7 +4,7 @@ __copyright__ = "Copyright (C) 2005-2010 " + __author__
 __license__  = "GNU GPL version 3 or any later version"
 
 # Modified by Kristian B. Oelgaard, 2009
-# Last changed: 2010-09-01
+# Last changed: 2011-02-23
 
-FFC_VERSION = "0.9.4+"
-UFC_VERSION = "1.4.2"
+FFC_VERSION = "0.9.9"
+UFC_VERSION = "2.0.0"
