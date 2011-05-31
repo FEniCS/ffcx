a5e4a808da21d9156759bcb3c9dadcfdf8d01e8f
Marie E. Rognes <meg@simula.no>
2011-05-31 11:39:48 +0200

    Add VectorElement of reals to element tests.

--- test/regression/elements.py
+++ test/regression/elements.py
@@ -1,3 +1,4 @@
 elements = ["FiniteElement('N1curl', triangle, 2)",
             "MixedElement([FiniteElement('Lagrange', triangle, 3), \
-                           VectorElement('Lagrange', triangle, 3)['facet']])"]
+                           VectorElement('Lagrange', triangle, 3)['facet']])",
+            "VectorElement('R', triangle, 0, 3)"]
