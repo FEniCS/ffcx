3cc10af8c427175f86f4b5e59bd1c230169e6628
Anders Logg <logg@simula.no>
2011-12-07 12:46:29 +0100

    Updates for version 2.0.5

--- src/ufc/ufc.h
+++ src/ufc/ufc.h
@@ -1,4 +1,4 @@
-// This is UFC (Unified Form-assembly Code) v. 2.0.4.
+// This is UFC (Unified Form-assembly Code) v. 2.0.5.
 // This code is released into the public domain.
 //
 // The FEniCS Project (http://www.fenicsproject.org/) 2006-2011.
@@ -8,11 +8,11 @@
 
 #define UFC_VERSION_MAJOR 2
 #define UFC_VERSION_MINOR 0
-#define UFC_VERSION_MAINTENANCE 4
+#define UFC_VERSION_MAINTENANCE 5
 
 #include <stdexcept>
 
-const char UFC_VERSION[] = "2.0.4";
+const char UFC_VERSION[] = "2.0.5";
 
 namespace ufc
 {
--- src/utils/python/ufc_utils/finite_element.py
+++ src/utils/python/ufc_utils/finite_element.py
@@ -1,4 +1,4 @@
-# Code generation format strings for UFC (Unified Form-assembly Code) v. 2.0.4.
+# Code generation format strings for UFC (Unified Form-assembly Code) v. 2.0.5.
 # This code is released into the public domain.
 #
 # The FEniCS Project (http://www.fenicsproject.org/) 2006-2011.
