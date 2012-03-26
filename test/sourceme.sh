5d6d21a925f0073bae8eaf4e869bab9b9c4a936b
Martin Sandve Alnæs <martinal@simula.no>
2012-03-26 10:52:36 +0200

    Some restructuring of module, making the tests an importable module.

--- test/sourceme.sh
+++
@@ -1,5 +0,0 @@
-# Set python path to find the local uninstalled ufl version
-export PYTHONPATH="`pwd`/..:$PYTHONPATH"
-export PATH="`pwd`/../scripts:$PATH"
-echo PYTHONPATH is now $PYTHONPATH
-echo PATH is now $PATH
