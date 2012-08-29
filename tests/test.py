1150fb68f4a2a9f93cbdc31316c933507d64a32d
Martin Sandve Alnæs <martinal@simula.no>
2012-08-29 15:08:42 +0200

    Add test.py for buildbot running with logs.

---
+++ tests/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys
+import instant
+s1, o1 = instant.get_status_output("python runpytests.py")
+if s1 != 0:
+    print "Python tests failed, see python_tests.log"
+    f = open('python_tests.log', 'w')
+    f.write(o1)
+    f.close()
+s2, o2 = instant.get_status_output("make")
+if s2 != 0:
+    print "C++ tests failed, see cpp_tests.log"
+    f = open('cpp_tests.log', 'w')
+    f.write(o2)
+    f.close()
+sys.exit(s1 or s2)
