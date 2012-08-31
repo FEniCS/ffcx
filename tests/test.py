2c787951bcfb4daf36e1f6db85f50178f588700c
Martin Sandve Alnæs <martinal@simula.no>
2012-08-31 12:54:31 +0200

    Write to stdio on success as well.

--- tests/test.py
+++ tests/test.py
@@ -4,16 +4,18 @@ import instant
 
 s1, o1 = instant.get_status_output("python runpytests.py")
 if s1 != 0:
-    print "Python tests failed, see python_tests.log"
-
-s2, o2 = instant.get_status_output("make")
-if s2 != 0:
-    print "C++ tests failed, see cpp_tests.log"
-
+    print "Python tests FAILED, see python_tests.log"
+else:
+    print "Python tests PASSED"
 f = open('python_tests.log', 'w')
 f.write(o1)
 f.close()
 
+s2, o2 = instant.get_status_output("make")
+if s2 != 0:
+    print "C++ tests FAILED, see cpp_tests.log"
+else:
+    print "C++ tests PASSED"
 f = open('cpp_tests.log', 'w')
 f.write(o2)
 f.close()
