8c601c83ed638e20af2e983d0e3a72008189af55
Martin Sandve Alnæs <martinal@simula.no>
2012-09-20 22:47:47 +0200

    Add readme with explanation of test framework.

---
+++ tests/cpp/test_element_combinations.h
@@ -0,0 +1,13 @@
+#ifndef TEST_ELEMENT_COMBINATIONS_H_INCLUDED
+#define TEST_ELEMENT_COMBINATIONS_H_INCLUDED
+
+#include <gtest/gtest.h>
+
+#include "gc_test_element_combinations.h"
+
+TEST (test_element_combinations, test_dummy)
+{
+    ASSERT_EQ(1, 1);
+}
+
+#endif
