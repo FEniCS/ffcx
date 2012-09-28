5a2ab414581d0b487fa1f4fa9788889be9cf412b
Martin Sandve Alnæs <martinal@simula.no>
2012-09-28 11:27:51 +0200

    Add mock test setups for four tabulate_tensor variants for the interval.
    The four are point, cell, int./ext. facet.

--- tests/py/test_tabulate_tensor_body.py
+++ tests/py/test_tabulate_tensor_body.py
@@ -53,5 +53,115 @@ class test_tabulate_tensor_body(CodegenTestCase):
         code = "" #self.compile_integral(integral)
         self.emit_test(code)
 
+    def test_tabulate_tensor_interval_point(self):
+        """Test code generation of body of the ufc function:
+
+        void tabulate_tensor(
+            double* A,
+            const double * const * w,
+            const cell& c,
+            const double * x) const;
+
+        PRE:
+        double A[1];
+        memset(A, 0, sizeof(A));
+
+        double w[1][2] = { { 2.0, 3.0 } };
+
+        mock_interval c;
+        c.coordinates[0][0] = 0.1;
+        c.coordinates[1][0] = 0.2;
+
+        double x[1] = { 0.15 };
+
+        POST:
+        // TODO
+        """
+        code = "// TODO"
+        self.emit_test(code)
+
+    def test_tabulate_tensor_interval_cell(self):
+        """Test code generation of body of the ufc function:
+
+        void tabulate_tensor(
+            double* A,
+            const double * const * w,
+            const cell& c) const;
+
+        PRE:
+        double A[1];
+        memset(A, 0, sizeof(A));
+        double w[1][2] = { { 2.0, 3.0 } };
+        mock_interval c;
+        c.coordinates[0][0] = 0.1;
+        c.coordinates[1][0] = 0.2;
+
+        POST:
+        // TODO
+        """
+        code = "// TODO"
+        self.emit_test(code)
+
+    def test_tabulate_tensor_interval_facet(self):
+        """Test code generation of body of the ufc function:
+
+        void tabulate_tensor(
+            double* A,
+            const double * const * w,
+            const cell& c,
+            unsigned int facet) const;
+
+        PRE:
+        double A[1];
+        memset(A, 0, sizeof(A));
+
+        double w[1][2] = { { 2.0, 3.0 } };
+
+        mock_interval c;
+        c.coordinates[0][0] = 0.1;
+        c.coordinates[1][0] = 0.2;
+
+        unsigned int facet = 0;
+
+        POST:
+        // TODO
+        """
+        code = "// TODO"
+        self.emit_test(code)
+
+    def test_tabulate_tensor_interval_facet(self):
+        """Test code generation of body of the ufc function:
+
+        void tabulate_tensor(
+            double* A,
+            const double * const * w,
+            const cell& c0,
+            const cell& c1,
+            unsigned int facet0,
+            unsigned int facet1) const;
+
+        PRE:
+        double A[2];
+        memset(A, 0, sizeof(A));
+
+        double w[1][4] = { { 2.0, 3.0,
+                             4.0, 5.0 } };
+
+        mock_interval c0;
+        c0.coordinates[0][0] = 0.1;
+        c0.coordinates[1][0] = 0.2;
+        mock_interval c1;
+        c1.coordinates[0][0] = 0.2;
+        c1.coordinates[1][0] = 0.3;
+
+        unsigned int facet0 = 1;
+        unsigned int facet1 = 0;
+
+        POST:
+        // TODO
+        """
+        code = "// TODO"
+        self.emit_test(code)
+
 if __name__ == "__main__":
     unittest.main()
