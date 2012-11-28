1b4f0ce3d4cef69cc7ab584cad482280006a401d
Marie E. Rognes <meg@simula.no>
2012-11-28 18:44:43 +0100

    Update test code to allow different cell shape and geometrical/topological dimension.

--- test/regression/ufctest.h
+++ test/regression/ufctest.h
@@ -16,7 +16,7 @@
 // along with FFC. If not, see <http://www.gnu.org/licenses/>.
 //
 // First added:  2010-01-24
-// Last changed: 2011-07-04
+// Last changed: 2012-11-28
 //
 // Functions for calling generated UFC functions with "random" (but
 // fixed) data and print the output to screen. Useful for running
@@ -142,7 +142,7 @@ class test_cell : public ufc::cell
 {
 public:
 
-  test_cell(ufc::shape cell_shape, int offset=0)
+  test_cell(ufc::shape cell_shape, int gdim=0, int offset=0)
   {
     // Store cell shape
     this->cell_shape = cell_shape;
@@ -152,15 +152,24 @@ public:
     {
     case ufc::interval:
       topological_dimension = 1;
-      geometric_dimension = 1;
+      if (gdim == 0)
+        geometric_dimension = 1;
+      else
+        geometric_dimension = gdim;
       break;
     case ufc::triangle:
       topological_dimension = 2;
-      geometric_dimension = 2;
+      if (gdim == 0)
+        geometric_dimension = 2;
+      else
+        geometric_dimension = gdim;
       break;
     case ufc::tetrahedron:
       topological_dimension = 3;
-      geometric_dimension = 3;
+      if (gdim == 0)
+        geometric_dimension = 3;
+      else
+        geometric_dimension = gdim;
       break;
     default:
       throw std::runtime_error("Unhandled cell shape.");
@@ -230,7 +239,7 @@ void test_finite_element(ufc::finite_element& element)
   std::cout << "----------------------" << std::endl;
 
   // Prepare arguments
-  test_cell c(element.cell_shape());
+  test_cell c(element.cell_shape(), element.topological_dimension());
   uint value_size = 1;
   for (uint i = 0; i < element.value_rank(); i++)
     value_size *= element.value_dimension(i);
@@ -349,7 +358,7 @@ void test_dofmap(ufc::dofmap& dofmap, ufc::shape cell_shape)
 
   // Prepare arguments
   test_mesh m(cell_shape);
-  test_cell c(cell_shape);
+  test_cell c(cell_shape, dofmap.topological_dimension());
   uint n = dofmap.max_local_dimension();
   uint* dofs = new uint[n];
   for (uint i = 0; i < n; i++)
@@ -446,6 +455,7 @@ void test_dofmap(ufc::dofmap& dofmap, ufc::shape cell_shape)
 // Function for testing ufc::cell_integral objects
 void test_cell_integral(ufc::cell_integral& integral,
                         ufc::shape cell_shape,
+                        uint gdim,
                         uint tensor_size,
                         double** w,
                         bool bench)
@@ -455,7 +465,7 @@ void test_cell_integral(ufc::cell_integral& integral,
   std::cout << "---------------------" << std::endl;
 
   // Prepare arguments
-  test_cell c(cell_shape);
+  test_cell c(cell_shape, gdim);
   double* A = new double[tensor_size];
   for(uint i = 0; i < tensor_size; i++)
     A[i] = 0.0;
@@ -490,6 +500,7 @@ void test_cell_integral(ufc::cell_integral& integral,
 // Function for testing ufc::exterior_facet_integral objects
 void test_exterior_facet_integral(ufc::exterior_facet_integral& integral,
                                   ufc::shape cell_shape,
+                                  uint gdim,
                                   uint tensor_size,
                                   double** w,
                                   bool bench)
@@ -499,7 +510,7 @@ void test_exterior_facet_integral(ufc::exterior_facet_integral& integral,
   std::cout << "-------------------------------" << std::endl;
 
   // Prepare arguments
-  test_cell c(cell_shape);
+  test_cell c(cell_shape, gdim);
   uint num_facets = c.topological_dimension + 1;
   double* A = new double[tensor_size];
 
@@ -540,6 +551,7 @@ void test_exterior_facet_integral(ufc::exterior_facet_integral& integral,
 // Function for testing ufc::interior_facet_integral objects
 void test_interior_facet_integral(ufc::interior_facet_integral& integral,
                                   ufc::shape cell_shape,
+                                  uint gdim,
                                   uint macro_tensor_size,
                                   double** w,
                                   bool bench)
@@ -549,8 +561,8 @@ void test_interior_facet_integral(ufc::interior_facet_integral& integral,
   std::cout << "-------------------------------" << std::endl;
 
   // Prepare arguments
-  test_cell c0(cell_shape, 0);
-  test_cell c1(cell_shape, 1);
+  test_cell c0(cell_shape, gdim, 0);
+  test_cell c1(cell_shape, gdim, 1);
   uint num_facets = c0.topological_dimension + 1;
   double* A = new double[macro_tensor_size];
 
@@ -627,6 +639,7 @@ void test_form(ufc::form& form, bool bench)
   // Get cell shape
   ufc::finite_element* element = form.create_finite_element(0);
   ufc::shape cell_shape = element->cell_shape();
+  uint gdim = element->geometric_dimension();
   delete element;
   element = 0;
 
@@ -669,7 +682,8 @@ void test_form(ufc::form& form, bool bench)
   {
     ufc::cell_integral* integral = form.create_cell_integral(i);
     if (integral)
-      test_cell_integral(*integral, cell_shape, tensor_size, w, bench);
+      test_cell_integral(*integral, cell_shape, gdim,
+                         tensor_size, w, bench);
     delete integral;
   }
 
@@ -678,7 +692,8 @@ void test_form(ufc::form& form, bool bench)
   {
     ufc::exterior_facet_integral* integral = form.create_exterior_facet_integral(i);
     if (integral)
-      test_exterior_facet_integral(*integral, cell_shape, tensor_size, w, bench);
+      test_exterior_facet_integral(*integral, cell_shape, gdim,
+                                   tensor_size, w, bench);
     delete integral;
   }
 
@@ -687,7 +702,8 @@ void test_form(ufc::form& form, bool bench)
   {
     ufc::interior_facet_integral* integral = form.create_interior_facet_integral(i);
     if (integral)
-      test_interior_facet_integral(*integral, cell_shape, macro_tensor_size, w, bench);
+      test_interior_facet_integral(*integral, cell_shape, gdim,
+                                   macro_tensor_size, w, bench);
     delete integral;
   }
 
