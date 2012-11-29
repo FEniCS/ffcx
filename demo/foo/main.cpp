fa31b73d045ee9a8586da56ae25494c0d1eaf76b
Marie E. Rognes <meg@simula.no>
2012-11-29 16:57:21 +0100

    Extend demo to actually compute L^2 projection. Result looks correct, we can project 1 onto 1 over the boundary of a unit circle mesh :-)

--- demo/foo/main.cpp
+++ demo/foo/main.cpp
@@ -6,20 +6,29 @@ using namespace dolfin;
 int main()
 {
   // Create mesh and function space
-  UnitCircleMesh mesh(16);
+  UnitCircleMesh mesh(2);
   BoundaryMesh boundary(mesh);
 
   ProjectionManifold::FunctionSpace V(boundary);
 
   // Define variational forms
   ProjectionManifold::BilinearForm a(V, V);
+  ProjectionManifold::LinearForm L(V);
+
+  Function f(V);
+  (*f.vector()) = 1.0;
+  L.f = f;
+
+  // Assemble
   Matrix A;
+  Vector b;
   assemble(A, a);
+  assemble(b, L);
 
-  info(A, true);
-  plot(mesh);
-
-  interactive();
+  // Solve
+  Vector x;
+  solve(A, x, b);
+  info(x, true);
 
   return 0;
 }
