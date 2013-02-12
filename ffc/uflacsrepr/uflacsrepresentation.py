7e69c90d24719eb5ee870a85c8315fe898d74932
Martin Sandve Alnaes <martinal@simula.no>
2013-02-12 10:34:26 +0100

    Clean up uflacs representation calls.

--- ffc/uflacsrepr/uflacsrepresentation.py
+++ ffc/uflacsrepr/uflacsrepresentation.py
@@ -16,7 +16,7 @@
 # along with FFC. If not, see <http://www.gnu.org/licenses/>.
 #
 # First added:  2009-12-16
-# Last changed: 2013-02-11
+# Last changed: 2013-02-12
 
 from ffc.representationutils import initialize_integral_ir, initialize_integral_code
 from ffc.log import info, error, begin, end, debug_ir, ffc_assert, warning
@@ -33,12 +33,12 @@ def compute_integral_ir(itg_data,
     # Initialise representation
     ir = initialize_integral_ir("uflacs", itg_data, form_data, form_id)
 
-    # TODO: Call upon ffc to build element tabules or other intermediate representation and add to ir
+    # TODO: Call upon ffc to build ir for element tables etc
     ffc_data = None
 
-    # Call upon flacs to build graphs and ssa or other intermediate representation and add to ir
-    from uflacs.backends.ffc import compute_uflacs_integral_ir
-    uir = compute_uflacs_integral_ir(ffc_data, itg_data, form_data, parameters)
+    # Delegate to flacs to build its intermediate representation and add to ir
+    import uflacs.backends.ffc
+    uir = uflacs.backends.ffc.compute_tabulate_tensor_ir(ffc_data, itg_data, form_data, parameters)
     ir.update(uir)
 
     return ir
@@ -49,8 +49,8 @@ def optimize_integral_ir(ir, parameters):
     info("Optimizing uflacs representation")
 
     # Call upon uflacs to optimize ssa representation prior to code generation. Should be possible to skip this step.
-    from uflacs.backends.ffc import optimize_integral_ir
-    oir = optimize_integral_ir(ir, parameters)
+    import uflacs.backends.ffc
+    oir = uflacs.backends.ffc.optimize_tabulate_tensor_ir(ir, parameters)
 
     return oir
 
@@ -59,12 +59,15 @@ def generate_integral_code(ir, prefix, parameters):
 
     info("Generating code from uflacs representation")
 
-    # Generate code
+    # Generate generic ffc code snippets
     code = initialize_integral_code(ir, prefix, parameters)
 
-    # Call upon uflacs to render graphs into code
-    from uflacs.backends.ffc import generate_tabulate_tensor_code
-    code["tabulate_tensor"] = generate_tabulate_tensor_code(ir, parameters)
+    # TODO: Call upon ffc to generate code for element tables etc
+    #code["..."] = ...
+
+    # Delegate to uflacs to generate tabulate_tensor body
+    import uflacs.backends.ffc
+    code["tabulate_tensor"] = uflacs.backends.ffc.generate_tabulate_tensor_code(ir, parameters)
 
     code["tabulate_tensor_quadrature"] = format["do nothing"] # TODO: Remove
     return code
