a28d5d2fe145d6e8f50ebbfae3f203a81088e617
Martin Sandve Alnaes <martinal@simula.no>
2013-02-18 11:53:45 +0100

    Rename stuff to json_foo to simplify merging into ffc regression code.

--- sandbox/regressiontesting/example.cpp
+++ sandbox/regressiontesting/example.cpp
@@ -2,60 +2,59 @@
 #include "simplejson.h"
 
 #include <vector>
-#include <list>
 #include <map>
 #include <fstream>
 
 int main(int argc, char* argv[])
 {
     std::ofstream af("a.output");
-    SimpleJsonModel a(af);
-    a.begin();
-    a.begin("foo");
-    a.print_scalar("a", 1);
-    a.print_scalar("b", 2.3);
+    JsonPrinter a(af);
+    a.json_begin();
+    a.json_begin("foo");
+    a.json_print_scalar("a", 1);
+    a.json_print_scalar("b", 2.3);
     std::vector<double> c1(2);
     c1[0] = 5.6;
     c1[1] = 6.5;
-    a.print_vector("c", c1);
-    a.begin("bar");
-    a.print_scalar("a", 2);
-    a.print_scalar("b", 3.4);
-    a.end();
-    a.begin("only_a");
-    a.print_scalar("avaluei", 7);
-    a.print_scalar("avaluef", "3.14159", 1);
-    a.print_scalar("avalues", "somename", 1, 2);
+    a.json_print_vector("c", c1);
+    a.json_begin("bar");
+    a.json_print_scalar("a", 2);
+    a.json_print_scalar("b", 3.4);
+    a.json_end();
+    a.json_begin("only_a");
+    a.json_print_scalar("avaluei", 7);
+    a.json_print_scalar("avaluef", "3.14159", 1);
+    a.json_print_scalar("avalues", "somename", 1, 2);
     double aarr[3] = { 1.2, 3.4, 5.6 };
-    a.print_array("avaluearr", 3, aarr);
-    a.end();
-    a.end();
-    a.end();
- 
+    a.json_print_array("avaluearr", 3, aarr);
+    a.json_end();
+    a.json_end();
+    a.json_end();
+
     std::ofstream bf("b.output");
-    SimpleJsonModel b(bf);
-    b.begin();
-    b.begin("foo");
-    b.print_scalar("a", 1);
-    b.print_scalar("b", 2.3);
-    std::list<double> c2;
+    JsonPrinter b(bf);
+    b.json_begin();
+    b.json_begin("foo");
+    b.json_print_scalar("a", 1);
+    b.json_print_scalar("b", 2.3);
+    std::vector<double> c2;
     c2.push_back(5.6);
     c2.push_back(6.51);
-    b.print_list("c", c2);
-    b.begin("bar");
-    b.print_scalar("a", 2);
-    b.print_scalar("b", 5.4);
-    b.end();
-    b.begin("only_b");
-    b.print_scalar("bvaluei", 7);
-    b.print_scalar("bvaluef", 3.14159);
+    b.json_print_vector("c", c2);
+    b.json_begin("bar");
+    b.json_print_scalar("a", 2);
+    b.json_print_scalar("b", 5.4);
+    b.json_end();
+    b.json_begin("only_b");
+    b.json_print_scalar("bvaluei", 7);
+    b.json_print_scalar("bvaluef", 3.14159);
     std::string someothername("someothername");
-    b.print_scalar("bvalues", someothername);
+    b.json_print_scalar("bvalues", someothername);
     double barr[3] = { 1.2, 3.4, 5.6 };
-    b.print_array("bvaluearr", 3, barr, 4, 5);
-    b.end();
-    b.end();
-    b.end();
+    b.json_print_array("bvaluearr", 3, barr, 4, 5);
+    b.json_end();
+    b.json_end();
+    b.json_end();
 
     return 0;
 }
--- sandbox/regressiontesting/simplejson.h
+++ sandbox/regressiontesting/simplejson.h
@@ -2,6 +2,7 @@
 #define SIMPLE_JSON_H__INCLUDED
 
 #include <cassert>
+#include <cmath>
 #include <string>
 #include <vector>
 #include <list>
@@ -9,182 +10,172 @@
 #include <sstream>
 
 /// A simplified json data model, including only hierarchial dicts of single values
-class SimpleJsonModel
+class JsonPrinter
 {
-public:
-    SimpleJsonModel(std::ostream & out):
-        level(0),
-        out(out)
-    {
-    }
-
-    /// Begin an unnamed block
-    void begin()
-    {
-        out << "{" << std::endl;
-        ++level;
-    }
-
-    /// Begin a named block entry
-    void begin(const std::string & name)
-    {
-        begin_entry(name);
-        begin();
-    }
-
-    /// Set a named single value entry
-    template<typename T>
-    void print_scalar(std::string name, T value, int i=-1, int j=-1)
-    {
-        std::stringstream s;
-        s << name;
-        if (i >= 0) s << "_" << i;
-        if (j >= 0) s << "_" << j;
-        name = s.str();
-
-        begin_entry(name);
-        print_formatted(value);
-        out << ", " << std::endl;
-    }
-
-    /// Set a named array valued entry
-    template<typename T>
-    void print_array(std::string name, int n, T * values, int i=-1, int j=-1)
-    {
-        std::stringstream s;
-        s << name;
-        if (i >= 0) s << "_" << i;
-        if (j >= 0) s << "_" << j;
-        name = s.str();
-
-        begin_entry(name);
-        out << "[";
-        if (n > 0)
-          print_formatted(values[0]);
-        for (int k=1; k<n; ++k)
-        {
-            out << ", ";
-            print_formatted(values[k]);
-        }
-        out << "], " << std::endl;
-    }
-
-    /// Set a named vector valued entry
-    template<typename T>
-    void print_vector(std::string name, typename std::vector<T> values, int i=-1, int j=-1)
-    {
-        std::stringstream s;
-        s << name;
-        if (i >= 0) s << "_" << i;
-        if (j >= 0) s << "_" << j;
-        name = s.str();
-
-        begin_entry(name);
-        out << "[";
-        typename std::vector<T>::iterator k=values.begin();
-        if (k!=values.end())
-        {
-            print_formatted(*k);
-            ++k;
-        }
-        while (k!=values.end())
-        {
-            out << ", ";
-            print_formatted(*k);
-            ++k;
-        }
-        out << "], " << std::endl;
-    }
-
-    /// Set a named list valued entry
-    template<typename T>
-    void print_list(std::string name, typename std::list<T> values, int i=-1, int j=-1)
-    {
-        std::stringstream s;
-        s << name;
-        if (i >= 0) s << "_" << i;
-        if (j >= 0) s << "_" << j;
-        name = s.str();
-
-        begin_entry(name);
-        out << "[";
-        typename std::list<T>::iterator k=values.begin();
-        if (k!=values.end())
-        {
-            print_formatted(*k);
-            ++k;
-        }
-        while (k!=values.end())
-        {
-            out << ", ";
-            print_formatted(*k);
-            ++k;
-        }
-        out << "], " << std::endl;
-    }
-
-    /// End current named or unnamed block
-    void end()
-    {
-        --level;
-        assert(level >= 0);
-        indent();
-        if (level > 0)
-            out << "}," << std::endl;
-        else
-            out << "}" << std::endl;
-    }
+protected:
+  // Precision in output of floats
+  const std::size_t precision;
+  const double epsilon;
 
 protected:
+  std::ostream & json_os;
+  int level;
 
-    int level;
-    std::ostream & out;
+  /// Indent to current level
+  void json_indent()
+  {
+    for (int i=0; i<level; ++i)
+      json_os << "    ";
+  }
 
-    /// Indent to current level
-    void indent()
-    {
-        for (int i=0; i<level; ++i)
-            out << "    ";
-    }
+  /// Format a value type properly
+  template<typename T>
+  void json_print_value(T value);
 
-    /// Output line with indention and newline
-    void output(const std::string & line)
-    {
-        indent();
-        out << line << std::endl;
-    }
+  /// Format '<indent>"foo": ' properly
+  void json_begin_entry(const std::string & name)
+  {
+    json_indent();
+    json_os << '"' << name << '"' << ": ";
+  }
 
-    /// Format a value type properly
-    template<typename T>
-    void print_formatted(T value);
+public:
+  JsonPrinter(std::ostream & json_os):
+    precision(16),
+    epsilon(1e-16),
 
-    /// Format '<indent>"foo": ' properly
-    void begin_entry(const std::string & name)
-    {
-        indent();
-        out << '"' << name << '"' << ": ";
-    }
+    json_os(json_os),
+    level(0)
+  {
+  }
+
+  /// Begin an unnamed block
+  void json_begin()
+  {
+    json_os << "{" << std::endl;
+    ++level;
+  }
+
+  /// Begin a named block entry
+  void json_begin(const std::string & name)
+  {
+    json_begin_entry(name);
+    json_begin();
+  }
+
+  /// Format name with optional integers appended
+  std::string json_format_name(std::string name, int i=-1, int j=-1)
+  {
+    std::stringstream s;
+    s << name;
+    if (i >= 0) s << "_" << i;
+    if (j >= 0) s << "_" << j;
+    return s.str();
+  }
+
+  /// Set a named single value entry
+  template<typename T>
+  void json_print_scalar(std::string name, T value, int i=-1, int j=-1)
+  {
+    name = json_format_name(name, i, j);
+    json_begin_entry(name);
+    json_print_value(value);
+    json_os << ", " << std::endl;
+  }
+
+  /// Set a named array valued entry
+  template<typename T>
+  void json_print_array(std::string name, int n, T * values, int i=-1, int j=-1)
+  {
+    name = json_format_name(name, i, j);
+    json_begin_entry(name);
+    json_os << "[";
+    if (n > 0)
+      json_print_value(values[0]);
+    for (int k=1; k<n; ++k)
+      {
+        json_os << ", ";
+        json_print_value(values[k]);
+      }
+    json_os << "], " << std::endl;
+  }
+
+  /// Set a named vector valued entry
+  template<typename T>
+  void json_print_vector(std::string name, typename std::vector<T> values, int i=-1, int j=-1)
+  {
+    name = json_format_name(name, i, j);
+    json_begin_entry(name);
+    json_os << "[";
+    typename std::vector<T>::iterator k=values.begin();
+    if (k!=values.end())
+      {
+        json_print_value(*k);
+        ++k;
+      }
+    while (k!=values.end())
+      {
+        json_os << ", ";
+        json_print_value(*k);
+        ++k;
+      }
+    json_os << "], " << std::endl;
+  }
+
+  /// End current named or unnamed block
+  void json_end()
+  {
+    --level;
+    assert(level >= 0);
+    json_indent();
+    if (level > 0)
+      json_os << "}," << std::endl;
+    else
+      json_os << "}" << std::endl;
+  }
 };
 
 /// Fallback formatting for any value type
 template<typename T>
-void SimpleJsonModel::print_formatted(T value)
+void JsonPrinter::json_print_value(T value)
 {
-    out << value;
+  json_os << value;
+}
+
+/// Use precision for floats
+template<>
+void JsonPrinter::json_print_value(double value)
+{
+  json_os.precision(precision);
+  if (std::abs(static_cast<double>(value)) < epsilon)
+    json_os << "0.0";
+  else
+    json_os << value;
+}
+
+/// Use precision for floats
+template<>
+void JsonPrinter::json_print_value(float value)
+{
+  json_os.precision(precision);
+  if (std::abs(static_cast<double>(value)) < epsilon)
+    json_os << "0.0";
+  else
+    json_os << value;
 }
 
 /// Wrap strings in quotes
 template<>
-void SimpleJsonModel::print_formatted(std::string value)
+void JsonPrinter::json_print_value(std::string value)
 {
-    out << '"' << value << '"';
+  json_os << '"' << value << '"';
 }
 
 /// Wrap strings in quotes
 template<>
-void SimpleJsonModel::print_formatted(const char * value)
+void JsonPrinter::json_print_value(const char * value)
 {
-    out << '"' << value << '"';
+  json_os << '"' << value << '"';
 }
 
 #endif
