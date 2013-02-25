093130d298a476f410e15ca272386f19bcfd8d48
Martin Sandve Alnaes <martinal@simula.no>
2013-02-25 13:30:37 +0100

    Some minor refactoring and fixes in coefficient rendering.

--- sandbox/print_tabulate_tensors/example.py
+++ sandbox/print_tabulate_tensors/example.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.insert(0, "../../site-packages")
+uflacs = __import__("uflacs")
 
 import uflacs.backends.ffc as uffc
 from ufl import *
@@ -14,16 +17,25 @@ c = Constant(cell)
 v = VectorConstant(cell)
 t = TensorConstant(cell)#, symmetry=True) # symmetry is not fully implemented in uflacs
 
+V0 = FiniteElement("CG", cell, 1)
+u0 = Coefficient(V0)
+
+V = VectorElement("CG", cell, 1)
+u = Coefficient(V)
+
 a1 = (x*xi)*dx
 a2 = (detJ*J*K)*dx
 a3 = c*v[0]*t[0,1]*t[1,0]*dx
 
 forms = [a1, a2, a3]
+forms = [a3]
+
+#forms = [Constant(cell)*dx]
+forms = [u0*dx]
+forms = [(u[0] + u[1])*dx]
 
 for form in forms:
-    fd = form.compute_form_data()
-    ffc_data = None
-    code = uffc.compile_tabulate_tensor_code(ffc_data, fd.integral_data[0], fd, {})
+    code = uffc.compile_tabulate_tensor_code(form, optimize=True)
     print
     print '/'*60
     print code
