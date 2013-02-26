83f4cf46b4d4eeb4a294a07b970c2eee1a603244
Martin Sandve Alnæs <martinal@simula.no>
2013-02-26 15:52:57 +0100

    Refactor somewhat, collecting variable names in a class before things get too messy.

--- sandbox/print_tabulate_tensors/example.py
+++ sandbox/print_tabulate_tensors/example.py
@@ -26,24 +26,34 @@ ut = TrialFunction(V0)
 T = TensorElement("CG", cell, 1)
 TS = TensorElement("CG", cell, 1)#, symmetry=True) FIXME
 V = VectorElement("CG", cell, 1, dim=4)
+RT = FiniteElement("RT", cell, 1)
 u = Coefficient(V)
 ut = Coefficient(T)
 uts = Coefficient(TS)
 
-ua, ub = Coefficients(V*V)
-
 a1 = (x*xi)*dx
 a2 = (detJ*J*K)*dx
-a3 = c*v[0]*t[0,1]*t[1,0]*dx
+a3 = c * (v[0]*v[1]) * (t[0,1]*t[1,0]*t[1,1]) * dx
 
 forms = [a1, a2, a3]
 forms = [a3]
 
+#S = FiniteElement("CG", cell, 1)
+#V = VectorElement("CG", cell, 1, dim=1)
+#us, uvs = Coefficients(S*(V*S))
+#forms = [us*uvs[0]*uvs[1]*dx]
+
 #forms = [Constant(cell)*dx]
 #forms = [u0*dx]
-forms = [(u[0] + u[1])*dx]
-forms = [(u[0] + u[3])*dx]
-forms = [(ua[0] + ub[3])*dx]
+#forms = [u0.dx(0)*dx]
+#forms = [(u[0] + u[1])*dx]
+#forms = [(u[0] + u[3])*dx]
+
+#ua, ub = Coefficients(V*V)
+#forms = [(ua[0] + ub[3])*dx]
+
+#ua, ub = Coefficients(V*RT)
+#forms = [(ua[1] + ub[0] + ub[1])*dx]
 
 #forms = [u0*vt*dx]
 #forms = [u0*ut*vt*dx]
