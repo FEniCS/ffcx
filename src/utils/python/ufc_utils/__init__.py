900d91160e1ba24d7248632fa5b9414d91fb9cab
Johannes Ring <johannr@simula.no>
2013-03-24 16:47:05 +0100

    Update version string to 2.2.0+.

--- src/utils/python/ufc_utils/__init__.py
+++ src/utils/python/ufc_utils/__init__.py
@@ -1,4 +1,4 @@
-"""Code generation format strings for UFC (Unified Form-assembly Code) v. 2.2.0.
+"""Code generation format strings for UFC (Unified Form-assembly Code) v. 2.2.0+.
 
 Three format strings are defined for each of the following UFC classes:
 
@@ -30,7 +30,7 @@ For more information about UFC and the FEniCS project, visit
 # -*- coding: utf-8 -*-
 __author__  = "Martin Sandve Alnaes, Anders Logg, Kent-Andre Mardal, Ola Skavhaug, and Hans Petter Langtangen"
 __date__    = "2013-03-24"
-__version__ = "2.2.0"
+__version__ = "2.2.0+"
 __license__ = "This code is released into the public domain"
 
 UFC_VERSION_MAJOR = 2
