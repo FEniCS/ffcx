5f7ec2072db687dfb594d395c213e195aeb725d3
Martin Sandve Alnæs <martinal@simula.no>
2013-09-20 12:57:23 +0200

    Update dependency analysis script to dump dot files.

--- sandbox/pymod_analyse.py
+++ sandbox/pymod_analyse.py
@@ -4,9 +4,11 @@ import os
 from collections import defaultdict
 
 modulename = r"uflacs"
-skipmods = []#['utils']
+#skipmods = ['utils', 'commands']
+skipmods = ['utils', 'commands', 'geometry', 'backends']
+#skipmods = ['utils', 'commands', 'geometry', 'codeutils', 'backends']
 
-r = re.compile(r'from +' + modulename + r'[.](.*) +import')
+r = re.compile(r'from +' + modulename + r'[.]([^ ]*) +import')
 
 files = []
 os.path.walk(modulename, lambda x, y, z: files.extend(os.path.join(y,fn) for fn in z if fn.endswith(".py")), None)
@@ -15,8 +17,10 @@ imports = defaultdict(dict)
 pimports = {}
 #for f in files:
 for f in files[:]:
-    mod = f.replace('uflacs/','').replace('.py', '').replace('/', '.')
+    mod = f.replace('uflacs/','').replace('.py', '').replace('/', '.').strip()
     pmod = mod.split('.')[0]
+    if pmod in skipmods:
+        continue
 
     lines = [l for l in open(f).readlines() if "import" in l]
 
@@ -29,7 +33,7 @@ for f in files[:]:
     modules = [m for m in modules if not any(m.startswith(skipmod+'.') for skipmod in skipmods)]
     parent_modules = [m for m in parent_modules if m not in skipmods + [pmod]]
 
-    crap = [g for g in groups if len(g) != 1]   
+    crap = [g for g in groups if len(g) != 1]
     if crap:
         print "Not sure what to do with this:"
         print crap
@@ -37,7 +41,8 @@ for f in files[:]:
     imports[pmod][mod] = modules
     pimports[pmod] = sorted(set(parent_modules) | set(pimports.get(pmod,())))
 
-# TODO: Make topological sorting of pmods
+
+# TODO: Make topological sorting of pmods for neater prints
 pmods = sorted(pimports.keys())
 
 print '*'*80
@@ -52,3 +57,47 @@ for m in pmods:
     if v:
         print '%s: %s' % (m, v)
 
+
+def rename(x):
+    return x.replace('.','__')
+
+# Write submodule imports to graph
+names = set()
+lines = []
+for m in pmods:
+    lines.append("    subgraph {\n")
+    for k,v in imports[m].iteritems():
+        if v:
+            names.add(k)
+        for t in v:
+            names.add(t)
+            lines.append("        %s -> %s;\n" % (rename(k), rename(t)))
+    lines.append("    }\n")
+
+for n in names:
+    lines.append('    %s [label="%s"]\n' % (rename(n), n))
+
+with open("deps.dot", "w") as df:
+    df.write("digraph {\n")
+    df.writelines(lines)
+    df.write("}\n")
+
+
+# Write coarse module dependencies to graph
+names = set()
+lines = []
+for m in pmods:
+    v = pimports[m]
+    if v:
+        names.add(m)
+    for t in v:
+        names.add(t)
+        lines.append("        %s -> %s;\n" % (rename(m), rename(t)))
+
+for n in names:
+    lines.append('    %s [label="%s"]\n' % (rename(n), n))
+
+with open("pdeps.dot", "w") as df:
+    df.write("digraph {\n")
+    df.writelines(lines)
+    df.write("}\n")
