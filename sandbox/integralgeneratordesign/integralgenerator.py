e944a2a7811978ae44a913f698cfcf5cb42912b3
Martin Sandve Alnæs <martinal@simula.no>
2013-10-02 23:05:57 +0200

    Some clean up in new integralgenerator.py design.

--- sandbox/integralgeneratordesign/integralgenerator.py
+++ sandbox/integralgeneratordesign/integralgenerator.py
@@ -9,29 +9,32 @@ from uflacs.geometry.default_names import names
 from uflacs.backends.ffc.ffc_statement_formatter import CppStatementFormatterRules
 langfmt = CppStatementFormatterRules()
 
+def create_fake_ir():
+    # TODO: This is just fake test data
+    ir = {}
+    ir["prim_idims"] = [7, 8]
+    ir["quadrature_weights"] = {
+        1: ( [0.5, 0.5],  [(0.1, 0.2), (0.3, 0.4)] ),
+        2: ( [3.5, 3.5],  [(1.1, 1.2), (2.3, 2.4)] ),
+        }
+    ir["dof_ranges"] = {
+        1: [
+            ((0,3), (3,6)),
+            ((1,2), (4,5))
+            ],
+        2: [
+            ((1,2), (4,6)),
+            ((0,5),)
+            ],
+        }
+    return ir
 
 class IntegralGenerator(object):
     def __init__(self):
         # TODO: This is just fake test data
-        ir = {}
-        ir["prim_idims"] = [7, 8]
-        ir["quadrature_weights"] = {
-            1: ( [0.5, 0.5],  [(0.1, 0.2), (0.3, 0.4)] ),
-            2: ( [3.5, 3.5],  [(1.1, 1.2), (2.3, 2.4)] ),
-            }
-        ir["dof_ranges"] = {
-            1: [
-                ((0,3), (3,6)),
-                ((1,2), (4,5))
-                ],
-            2: [
-                ((1,2), (4,6)),
-                ((0,5),)
-                ],
-            }
+        ir = create_fake_ir()
 
-        # ...
-
+        # Parse quadrature rules
         quadrature_rules = ir["quadrature_weights"]
         self._num_points = []
         self._weights = {}
@@ -41,33 +44,33 @@ class IntegralGenerator(object):
             w, p = quadrature_rules[num_points]
             self._weights[num_points] = w
             self._points[num_points] = p
-
         if len(quadrature_rules) > 1:
             warning("Multiple quadrature rules not fully implemented, will likely crash somewhere.")
 
+        # Parse argument space dimensions etc.
         self._dof_ranges = ir["dof_ranges"]
         self._argument_space_dimensions = ir["prim_idims"] # FIXME: *2 for dS?
         self._A_size = product(self._argument_space_dimensions)
-
         self._num_arguments = len(self._argument_space_dimensions)
         self._idofs = ["%s%d" % (names.ia, i) for i in range(self._num_arguments)]
 
-
     def generate(self):
-        """Generate entire tabulate_tensor body."""
+        """Generate entire tabulate_tensor body.
+
+        Assumes that the code returned from here will be wrapped in a context
+        that matches a suitable version of the UFC tabulate_tensor signatures.
+        """
         parts = []
-        parts += [self.generate_static_tables()]
-        parts += [self.generate_pre_quadrature()]
+        parts += [self.generate_quadrature_tables()]
+        parts += [self.generate_element_tables()]
+        parts += [self.generate_tensor_reset()]
+        parts += [self.generate_pre_quadrature_loops()]
         parts += [self.generate_quadrature_loops()]
-        parts += [self.generate_post_quadrature()]
+        parts += [self.generate_post_quadrature_loops()]
         return format_code_structure(Indented(parts))
 
-    def generate_static_tables(self): # FIXME
-        """Generate static tables of precomputed values.
-
-        This typically includes quadrature points and weights,
-        as well as basis function values in quadrature points.
-        """
+    def generate_quadrature_tables(self):
+        "Generate static tables of quadrature points and weights."
         parts = ["// Quadrature weights and points"]
 
         for num_points in self._num_points:
@@ -78,8 +81,10 @@ class IntegralGenerator(object):
             weights = "{ %s }" % langfmt.precision_floats(weights)
             points = "{ %s }" % langfmt.precision_floats(x for p in points for x in p)
 
-            wname = "%s%d" % (names.weights, num_points) # FIXME: Make sure we use this everywhere
-            pname = "%s%d" % (names.points, num_points)
+            #wname = "%s%d" % (names.weights, num_points) # TODO: Use this everywhere
+            #pname = "%s%d" % (names.points, num_points) # TODO: Use this everywhere
+            wname = "%s" % names.weights
+            pname = "%s" % names.points
 
             parts += [langfmt.array_decl("static const double", wname, num_points, weights)]
             parts += [langfmt.array_decl("static const double", pname, num_points*pdim, points)]
@@ -87,37 +92,43 @@ class IntegralGenerator(object):
 
         return parts
 
-    def generate_pre_quadrature(self): # FIXME
-        """Generate initialization statements.
-
-        This includes initial piecewise constant geometry computations and such.
-        """
-        parts = []
-
-        memset = "memset(%s, 0, %d * sizeof(%s[0]));" % (names.A, self._A_size, names.A)
-        parts += [langfmt.comment("Reset element tensor"), memset, ""]
-
-        parts += ["// Piecewise constant stage"]
-        parts += ["// FIXME: Implement this"]
-
+    def generate_element_tables(self): # FIXME
+        "Generate static tables with precomputed element basis function values in quadrature points."
+        parts = ["// FIXME: Precomputed element basis function values"]
         return parts
 
-    def generate_post_quadrature(self): # TODO
+    def generate_tensor_reset(self):
+        "Generate statements for resetting the element tensor to zero."
+        memset = "memset(%s, 0, %d * sizeof(%s[0]));" % (names.A, self._A_size, names.A)
+        parts = [langfmt.comment("Reset element tensor"), memset, ""]
+        return parts
+
+    def generate_pre_quadrature_loops(self): # FIXME
+        """Generate statements prior to the quadrature loop.
+
+        This mostly includes computations involving piecewise constant geometry and coefficients.
+        """
+        parts = []
+        parts += ["// Piecewise constant stage"]
+        parts += ["// FIXME: Implement this"]
+        return parts
+
+    def generate_quadrature_loops(self):
+        "Generate all quadrature loops."
+        parts = [self.generate_quadrature_loop(num_points) for num_points in self._num_points]
+        return parts
+
+    def generate_post_quadrature_loops(self): # TODO
         """Generate finishing statements.
 
         This includes assigning to output array if there is no integration.
         """
-        parts = ["// Finishing statements"]
-        parts += ["// TODO: Implement this"]
-        return parts
-
-    def generate_quadrature_loops(self):
-        """Generate all quadrature loops."""
-        parts = [self.generate_quadrature_loop(num_points) for num_points in self._num_points]
+        parts = []
+        # TODO: If no integration, assuming we generate an expression, and assign results here
         return parts
 
     def generate_quadrature_loop(self, num_points):
-        """Generate single quadrature loop."""
+        "Generate a single quadrature loop."
         return ForRange(names.iq, 0, num_points,
                         body=self.generate_quadrature_body(num_points))
 
