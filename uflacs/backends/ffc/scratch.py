76915eed179d8f332cf5cc30c1203f1dcbee4051
Martin Sandve Alnæs <martinal@simula.no>
2014-03-06 08:44:34 +0000

    Work on new ffc backend formatter.

--- uflacs/backends/ffc/scratch.py
+++ uflacs/backends/ffc/scratch.py
@@ -13,48 +13,6 @@ from uflacs.backends.ffc.ffc_statement_formatter import (format_element_table_ac
 from uflacs.elementtables.table_utils import derivative_listing_to_counts, flatten_component
 
 
-class FFCLanguageFormatter(MultiFunction, CppFormattingRules):
-    """FFC specific cpp formatter class."""
-    def __init__(self, ir):
-        MultiFunction.__init__(self)
-        CppFormattingRules.__init__(self)
-
-    def modified_terminal(self, o, mt):
-        return str(o) # FIXME
-
-    # Core terminals
-    coefficient = modified_terminal
-    argument = modified_terminal
-    geometric_quantity = modified_terminal
-
-    # Modifiers
-    grad = modified_terminal
-    local_grad = modified_terminal
-    restriction = modified_terminal
-    indexed = modified_terminal
-    facet_avg = modified_terminal
-    cell_avg = modified_terminal
-
-class Backend(object):
-    def generate_modified_terminal_definition(self, mt):
-        if isinstance(mt.terminal, Argument):
-            return self.generate_argument_definition(mt)
-        if isinstance(mt.terminal, Coefficient):
-            return self.generate_coefficient_definition(mt)
-        if isinstance(mt.terminal, GeometricQuantity):
-            return self.generate_geometric_quantity_definition(mt)
-        error("Unknown terminal type {0}.".format(type(mt.terminal)))
-
-    def generate_modified_terminal_access(self, mt):
-        if isinstance(mt.terminal, Argument):
-            return self.generate_argument_access(mt)
-        if isinstance(mt.terminal, Coefficient):
-            return self.generate_coefficient_access(mt)
-        if isinstance(mt.terminal, GeometricQuantity):
-            return self.generate_geometric_quantity_access(mt)
-        error("Unknown terminal type {0}.".format(type(mt.terminal)))
-
-
 def format_entity_name(entitytype, r):
     if entitytype == "cell":
         entity = "0" #None # FIXME: Keep 3D tables and use entity 0 for cells or make tables 2D and use None?
@@ -65,13 +23,17 @@ def format_entity_name(entitytype, r):
     return entity
 
 
-class FFCBackend(Backend):
+class FFCLanguageFormatter(MultiFunction, CppFormattingRules):
+    """FFC specific cpp formatter class."""
+    def __init__(self, ir):
+        MultiFunction.__init__(self)
+        CppFormattingRules.__init__(self)
 
     def generate_element_table_access(self, mt):
-        return "FE[0]"
+        return "FE[0]" # FIXME
 
     def generate_geometry_table_access(self, mt):
-        return "FJ[0]"
+        return "FJ[0]" # FIXME
 
     def generate_coefficient_dof_access(self, coefficient_number, dof_number):
         # TODO: Add domain number
@@ -83,12 +45,22 @@ class FFCBackend(Backend):
         name = names.vertex_coordinates
         return "{name}[{gdim}*{vertex} + {component}]".format(**locals())
 
+    # === Multifunction handlers for all modified terminal types, basic C++ types are covered by base class ===
 
-    def generate_argument_definition(self, mt):
-        code = []
-        return code
+    def modified_terminal(self, o):
+        "Analyse underlying modified terminal structure and pass on to handler of terminal type."
+        mt = analyse_modified_terminal2(o)
+        return self(mt.terminal, mt)
 
-    def generate_argument_access(self, mt):
+    # Terminal modifiers TODO: Any more? Add local_value/pullback
+    grad = modified_terminal
+    local_grad = modified_terminal
+    restriction = modified_terminal
+    indexed = modified_terminal
+    facet_avg = modified_terminal
+    cell_avg = modified_terminal
+
+    def argument(self, e, mt):
         # Expecting only local derivatives and values here
         assert not (mt.global_derivatives and any(mt.global_derivatives))
         #assert mt.global_component is None
@@ -106,20 +78,19 @@ class FFCBackend(Backend):
 
         # No need to store basis function value in its own variable, just get table value directly
         access = format_element_table_access(self._ir, self._entitytype, self._num_points,
-                                             element, flat_component, (), entity, idof, True)
+                                             element, flat_component, (), entity, idof, True) # FIXME
 
         return access
 
-
-    def generate_coefficient_access(self, mt):
+    def coefficient(self, e, mt):
         t = mt.terminal
         if t.is_cellwise_constant():
-            access = self.generate_constant_coefficient_access(mt)
+            access = self._constant_coefficient(e, mt)
         else:
-            access = self.generate_varying_coefficient_access(mt)
+            access = self._varying_coefficient(e, mt)
         return access
 
-    def generate_constant_coefficient_access(self, mt):
+    def _constant_coefficient(self, e, mt):
         # Map component to flat index
         vi2si, si2vi = build_component_numbering(mt.terminal.shape(), mt.terminal.element().symmetry())
         flat_component = vi2si[mt.component]
@@ -131,7 +102,7 @@ class FFCBackend(Backend):
         # Return direct reference to dof array
         return self.generate_coefficient_dof_access(mt.terminal.count(), flat_component)
 
-    def generate_varying_coefficient_access(self, mt):
+    def _varying_coefficient(self, e, mt):
         t = mt.terminal
 
         # Expecting only local derivatives and values here
@@ -156,8 +127,59 @@ class FFCBackend(Backend):
         res = names.restriction_postfix[mt.restriction]
 
         # Format base coefficient (derivative) name
-        access = "%s%d%s%s%s" % (names.w, t.number(), der, comp, res)
+        access = "{name}{count}{der}{comp}{res}".format(name=names.w, count=t.count(), der=der, comp=comp, res=res)
         return access
+
+    def spatial_coordinate(self, e, mt):
+        access = "x" # FIXME
+        return access
+
+    def local_coordinate(self, e, mt):
+        access = "X" # FIXME
+        return access
+
+    def jacobian(self, e, mt):
+        access = "J" # FIXME
+        return access
+
+    def jacobian_inverse(self, e, mt):
+        access = "K" # FIXME
+        return access
+
+    def jacobian_determinant(self, e, mt):
+        access = "detJ" # FIXME
+        return access
+
+    def facet_jacobian(self, e, mt):
+        access = "J" # FIXME
+        return access
+
+    def facet_jacobian_inverse(self, e, mt):
+        access = "K" # FIXME
+        return access
+
+    def facet_jacobian_determinant(self, e, mt):
+        access = "detJ" # FIXME
+        return access
+
+    def facet_normal(self, e, mt):
+        access = "n" # FIXME
+        return access
+
+    # === Generate code definitions ===
+
+    def generate_modified_terminal_definition(self, mt):
+        if isinstance(mt.terminal, Argument):
+            return self.generate_argument_definition(mt)
+        if isinstance(mt.terminal, Coefficient):
+            return self.generate_coefficient_definition(mt)
+        if isinstance(mt.terminal, GeometricQuantity):
+            return self.generate_geometric_quantity_definition(mt)
+        error("Unknown terminal type {0}.".format(type(mt.terminal)))
+
+    def generate_argument_definition(self, mt):
+        code = []
+        return code
 
     def generate_coefficient_definition(self, mt):
         code = []
@@ -167,24 +189,7 @@ class FFCBackend(Backend):
             code += ["const double {name} = {value};".format(name, value)]
         return code
 
-
     def generate_geometric_quantity_definition(self, mt):
         code = []
         code += ["double geometry = 1.0;"] # FIXME
         return code
-
-    def generate_geometric_quantity_access(self, mt):
-        access = "geometry" # FIXME
-        return access
-
-
-def generate_piecewise_partition():
-
-    for o in modified_terminals:
-        mt = analyse_modified_terminals2(o)
-
-        definition = backend.generate_modified_terminal_definition(mt)
-        access = backend.generate_modified_terminal_access(mt)
-
-        code += definition
-        langfmt.cache[o] = access
