b038802f4dc0be128100e9994be83a78973022d0
Martin Sandve Alnæs <martinal@simula.no>
2014-07-23 14:49:01 +0200

    Remove more unused imports.

--- uflacs/backends/ffc/definitions.py
+++ uflacs/backends/ffc/definitions.py
@@ -1,13 +1,10 @@
 
-from six import iterkeys
 from six.moves import xrange as range
 
-import ufl
-from ufl.permutation import build_component_numbering
-from ufl.classes import Argument, Coefficient, GeometricQuantity
 from ufl.algorithms import MultiFunction
 
-from ffc.log import ffc_assert, warning, error
+from ffc.log import error
+from ffc.log import ffc_assert
 
 from uflacs.codeutils.cpp_statement_formatting_rules import CppStatementFormattingRules
 langfmt = CppStatementFormattingRules()
