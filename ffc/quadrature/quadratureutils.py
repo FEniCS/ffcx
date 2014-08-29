9728f17a4d57a6be4d3e09cc906ef9e8a3b03b33
Martin Sandve Alnæs <martinal@simula.no>
2014-08-29 15:15:21 +0200

    Change a couple of ffc_assert calls that format tables in inner loops even when there's no error.

--- ffc/quadrature/quadratureutils.py
+++ ffc/quadrature/quadratureutils.py
@@ -111,11 +111,11 @@ def flatten_psi_tables(tables, entity_type):
                             name = generate_psi_name(counter, entity_type, entity, component, derivs, avg)
 
                             # Verify shape of basis (can be omitted for speed if needed).
-                            ffc_assert(num_points is None or (len(numpy.shape(psi_table)) == 2 and numpy.shape(psi_table)[0] == num_points),
-                                       "This table has the wrong shape: " + str(psi_table))
+                            #if not (num_points is None or (len(numpy.shape(psi_table)) == 2 and numpy.shape(psi_table)[0] == num_points)):
+                            #    error("This table has the wrong shape: " + str(psi_table))
                             # Verify uniqueness of names
-                            ffc_assert(name not in flat_tables,
-                                        "Table name is not unique, something is wrong:\n  name = %s\n  table = %s\n" % (name, flat_tables))
+                            if name in flat_tables:
+                                error("Table name is not unique, something is wrong:\n  name = %s\n  table = %s\n" % (name, flat_tables))
 
                             # Store table with unique name
                             flat_tables[name] = psi_table
