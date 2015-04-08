8217604d1c8aa075fdbf038c99faca1bdc742d71
Martin Sandve Alnæs <martinal@simula.no>
2015-04-08 20:06:02 +0200

    Add licence headers to all files.

--- uflacs/__init__.py
+++ uflacs/__init__.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
-"""This is uflacs, the UFL Analyser and Compiler System.
+# Copyright (C) 2011-2015 Martin Sandve Alnæs
+#
+# This file is part of UFLACS.
+#
+# UFLACS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# UFLACS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with UFLACS. If not, see <http://www.gnu.org/licenses/>
 
-Uflacs is currently in an experimental stage, however it is
-planned to fairly soon become a useful part of the FEniCS
-project, eventually being merged into UFL and/or FFC.
-"""
+"""This is UFLACS, the UFL Analyser and Compiler System."""
 
-__author__ = "Martin Sandve Alnaes"
+__author__ = u"Martin Sandve Alnæs"
 __version__ = "1.6.0dev"
-__date__ = '2015-01-12'
+__date__ = '2015-08-04'
 __licence__ = 'LGPL v3'
--- uflacs/codeutils/indexmapping.py
+++ uflacs/codeutils/indexmapping.py
@@ -1,12 +1,29 @@
-from ufl.common import product
+# Copyright (C) 2011-2015 Martin Sandve Alnes
+#
+# This file is part of UFLACS.
+#
+# UFLACS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# UFLACS is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with UFLACS. If not, see <http://www.gnu.org/licenses/>
 
+"""Tools for formatting of multi-dimensional indexing."""
+
+from ufl.common import product
 
 class Range(object):
 
     def __init__(self, begin, end):
         self.begin = begin
         self.end = end
-
 
 class IndexMapping(object):
 
