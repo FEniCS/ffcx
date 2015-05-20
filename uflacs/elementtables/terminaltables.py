afd998d9e525e1b3cec19d2f84bab419713cbd6e
Martin Sandve Alnæs <martinal@simula.no>
2015-05-20 14:12:50 +0200

    Include number of quadrature points in FE table name.

--- uflacs/elementtables/terminaltables.py
+++ uflacs/elementtables/terminaltables.py
@@ -131,9 +131,8 @@ def build_element_tables(psi_tables, num_points, entitytype, terminal_data):
             assert not any(global_derivatives), "TODO: Does it make sense to have global derivatives in here now?"
 
             # Build name for this particular table
-            # TODO: Include num_points in table name?
             name = generate_psi_table_name(element_counter, fc,
-                                           local_derivatives, mt.averaged, entitytype)
+                                         local_derivatives, mt.averaged, entitytype, num_points)
 
             # Extract the values of the table from ffc table format
             table = tables.get(name)
