27228856546d69b1de51f639899e7c0b64ecd072
Martin Sandve Alnæs <martinal@simula.no>
2015-10-23 15:08:44 +0200

    Remove comments from coordinate_mapping template. Simplifies future maintenance of the interface. Comments from other class templates will be removed later in another branch.

--- ffc/backends/ufc/coordinate_mapping.py
+++ ffc/backends/ufc/coordinate_mapping.py
@@ -4,67 +4,52 @@
 # The FEniCS Project (http://www.fenicsproject.org/) 2006-2015.
 
 coordinate_mapping_header = """\
-/// A representation of a coordinate mapping parameterized by a local finite element basis on each cell
+/// Documentation for the ufc::coordinate_mapping interface can be found in ufc.h
 class %(classname)s: public ufc::coordinate_mapping
 {%(members)s
 public:
 
-  /// Constructor
   %(classname)s(%(constructor_arguments)s);
 
-  /// Destructor
   ~%(classname)s() override;
 
-  /// Return coordinate_mapping signature string
   const char * signature() const final override;
 
-  /// Create object of the same type
   coordinate_mapping * create() const final override;
 
-  /// Return geometric dimension of the coordinate_mapping
   std::size_t geometric_dimension() const final override;
 
-  /// Return topological dimension of the coordinate_mapping
   std::size_t topological_dimension() const final override;
 
-  /// Return cell shape of the coordinate_mapping
   ufc::shape cell_shape() const final override;
 
-  /// Create finite_element object representing the coordinate parameterization
   ufc::finite_element * create_coordinate_finite_element() const final override;
 
-  /// Create dofmap object representing the coordinate parameterization
   ufc::dofmap * create_coordinate_dofmap() const final override;
 
-  /// Compute physical coordinates x from reference coordinates X, the inverse of compute_reference_coordinates
   void compute_physical_coordinates(
       double * x, std::size_t num_points,
       const double * X,
       const double * coordinate_dofs, int cell_orientation) const final override;
 
-  /// Compute reference coordinates X from physical coordinates x, the inverse of compute_physical_coordinates
   void compute_reference_coordinates(
       double * X, std::size_t num_points,
       const double * x,
       const double * coordinate_dofs, int cell_orientation) const final override;
 
-  /// Compute Jacobian of coordinate mapping J = dx/dX at reference coordinates X
   void compute_jacobians(
       double * J, std::size_t num_points,
       const double * X,
       const double * coordinate_dofs, int cell_orientation) const final override;
 
-  /// Compute determinants of (pseudo-)Jacobians J
   void compute_jacobian_determinants(
       double * detJ, std::size_t num_points,
       const double * J) const final override;
 
-  /// Compute (pseudo-)inverses K of (pseudo-)Jacobians J
   void compute_jacobian_inverses(
       double * K, std::size_t num_points,
       const double * J, const double * detJ) const final override;
 
-  /// Combined (for convenience) computation of x, J, detJ, K from X and coordinate_dofs on a cell
   void compute_geometry(
       double * x, double * J, double * detJ, double * K, std::size_t num_points,
       const double * X,
@@ -73,61 +58,51 @@ public:
 """
 
 coordinate_mapping_implementation = """\
-/// Constructor
 %(classname)s::%(classname)s(%(constructor_arguments)s) : ufc::coordinate_mapping()%(initializer_list)s
 {
 %(constructor)s
 }
 
-/// Destructor
 %(classname)s::~%(classname)s() override
 {
 %(destructor)s
 }
 
-/// Return coordinate_mapping signature string
 const char * %(classname)s::signature() const final override
 {
 %(signature)s
 }
 
-/// Create object of the same type
 coordinate_mapping * %(classname)s::create() const final override
 {
 %(create)s
 }
 
-/// Return geometric dimension of the coordinate_mapping
 std::size_t %(classname)s::geometric_dimension() const final override
 {
 %(geometric_dimension)s
 }
 
-/// Return topological dimension of the coordinate_mapping
 std::size_t %(classname)s::topological_dimension() const final override
 {
 %(topological_dimension)s
 }
 
-/// Return cell shape of the coordinate_mapping
 ufc::shape %(classname)s::cell_shape() const final override
 {
 %(cell_shape)s
 }
 
-/// Create finite_element object representing the coordinate parameterization
 ufc::finite_element * %(classname)s::create_coordinate_finite_element() const final override
 {
 %(create_coordinate_finite_element)s
 }
 
-/// Create dofmap object representing the coordinate parameterization
 ufc::dofmap * %(classname)s::create_coordinate_dofmap() const final override
 {
 %(create_coordinate_dofmap)s
 }
 
-/// Compute physical coordinates x from reference coordinates X, the inverse of compute_reference_coordinates
 void %(classname)s::compute_physical_coordinates(
     double * x, std::size_t num_points,
     const double * X,
@@ -136,7 +111,6 @@ void %(classname)s::compute_physical_coordinates(
 %(compute_physical_coordinates)s
 }
 
-/// Compute reference coordinates X from physical coordinates x, the inverse of compute_physical_coordinates
 void %(classname)s::compute_reference_coordinates(
     double * X, std::size_t num_points,
     const double * x,
@@ -145,7 +119,6 @@ void %(classname)s::compute_reference_coordinates(
 %(compute_reference_coordinates)s
 }
 
-/// Compute Jacobian of coordinate mapping J = dx/dX at reference coordinates X
 void %(classname)s::compute_jacobians(
     double * J, std::size_t num_points,
     const double * X,
@@ -154,7 +127,6 @@ void %(classname)s::compute_jacobians(
 %(compute_jacobians)s
 }
 
-/// Compute determinants of (pseudo-)Jacobians J
 void %(classname)s::compute_jacobian_determinants(
     double * detJ, std::size_t num_points,
     const double * J) const final override
@@ -162,7 +134,6 @@ void %(classname)s::compute_jacobian_determinants(
 %(compute_jacobian_determinants)s
 }
 
-/// Compute (pseudo-)inverses K of (pseudo-)Jacobians J
 void %(classname)s::compute_jacobian_inverses(
     double * K, std::size_t num_points,
     const double * J, const double * detJ) const final override
@@ -170,7 +141,6 @@ void %(classname)s::compute_jacobian_inverses(
 %(compute_jacobian_inverses)s
 }
 
-/// Combined (for convenience) computation of x, J, detJ, K from X and coordinate_dofs on a cell
 void %(classname)s::compute_geometry(
     double * x, double * J, double * detJ, double * K, std::size_t num_points,
     const double * X,
@@ -181,66 +151,56 @@ void %(classname)s::compute_geometry(
 """
 
 coordinate_mapping_combined = """\
-/// A representation of a geometric domain parameterized by a local basis on each cell
+/// Documentation for the ufc::coordinate_mapping interface can be found in ufc.h
 class %(classname)s: public ufc::coordinate_mapping
 {%(members)s
 public:
 
-  /// Constructor
   %(classname)s(%(constructor_arguments)s) : ufc::coordinate_mapping()%(initializer_list)s
   {
 %(constructor)s
   }
 
-  /// Destructor
   ~%(classname)s() override
   {
 %(destructor)s
   }
 
-  /// Return coordinate_mapping signature string
   const char * signature() const final override
   {
 %(signature)s
   }
 
-  /// Create object of the same type
   coordinate_mapping * create() const final override
   {
 %(create)s
   }
 
-  /// Return geometric dimension of the coordinate_mapping
   std::size_t geometric_dimension() const final override
   {
 %(geometric_dimension)s
   }
 
-  /// Return topological dimension of the coordinate_mapping
   std::size_t topological_dimension() const final override
   {
 %(topological_dimension)s
   }
 
-  /// Return cell shape of the coordinate_mapping
   ufc::shape cell_shape() const final override
   {
 %(cell_shape)s
   }
 
-  /// Create finite_element object representing the coordinate parameterization
   ufc::finite_element * create_coordinate_finite_element() const final override
   {
 %(create_coordinate_finite_element)s
   }
 
-  /// Create dofmap object representing the coordinate parameterization
   ufc::dofmap * create_coordinate_dofmap() const final override
   {
 %(create_coordinate_dofmap)s
   }
 
-  /// Compute physical coordinates x from reference coordinates X, the inverse of compute_reference_coordinates
   void compute_physical_coordinates(
       double * x, std::size_t num_points,
       const double * X,
@@ -249,7 +209,6 @@ public:
 %(compute_physical_coordinates)s
   }
 
-  /// Compute reference coordinates X from physical coordinates x, the inverse of compute_physical_coordinates
   void compute_reference_coordinates(
       double * X, std::size_t num_points,
       const double * x,
@@ -258,7 +217,6 @@ public:
 %(compute_reference_coordinates)s
   }
 
-  /// Compute Jacobian of coordinate mapping J = dx/dX at reference coordinates X
   void compute_jacobians(
       double * J, std::size_t num_points,
       const double * X,
@@ -267,7 +225,6 @@ public:
 %(compute_jacobians)s
   }
 
-  /// Compute determinants of (pseudo-)Jacobians J
   void compute_jacobian_determinants(
       double * detJ, std::size_t num_points,
       const double * J) const final override
@@ -275,7 +232,6 @@ public:
 %(compute_jacobian_determinants)s
   }
 
-  /// Compute (pseudo-)inverses K of (pseudo-)Jacobians J
   void compute_jacobian_inverses(
       double * K, std::size_t num_points,
       const double * J, const double * detJ) const final override
@@ -283,7 +239,6 @@ public:
 %(compute_jacobian_inverses)s
   }
 
-  /// Combined (for convenience) computation of x, J, detJ, K from X and coordinate_dofs on a cell
   void compute_geometry(
       double * x, double * J, double * detJ, double * K, std::size_t num_points,
       const double * X,
