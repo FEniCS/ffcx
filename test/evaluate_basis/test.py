411a18cf15ff0096182f0772ca73d42c809cc8e8
Garth N. Wells <gnw20@cam.ac.uk>
2016-01-29 14:45:23 +0000

    Fix typo in import.

--- test/evaluate_basis/test.py
+++ test/evaluate_basis/test.py
@@ -18,7 +18,7 @@
 # First added:  2010-01-18
 # Last changed: 2010-01-18
 
-from __funture__ import print_function
+from __future__ import print_function
 from cppcode import evaluate_basis_code
 from ufl import FiniteElement, MixedElement
 from instant.output import get_status_output
