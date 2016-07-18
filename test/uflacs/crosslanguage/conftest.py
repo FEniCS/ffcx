1a1702181c8dc2a60269f2c8959e3e1cc0323c84
Garth N. Wells <gnw20@cam.ac.uk>
2016-07-18 12:47:57 +0100

    Let test sytem build gtest.

--- test/uflacs/crosslanguage/conftest.py
+++ test/uflacs/crosslanguage/conftest.py
@@ -1,14 +1,14 @@
-"""
-This file contains code for setting up C++ unit tests with gtest from within Python tests using py.test.
+"""This file contains code for setting up C++ unit tests with gtest
+from within Python tests using py.test.
+
 """
 
 import pytest
 import os
 import inspect
 from collections import defaultdict
-
+import subprocess
 from instant.output import get_status_output
-
 
 # TODO: For a generic framework, this needs to change somewhat:
 _supportcode = '''
@@ -17,7 +17,6 @@ _supportcode = '''
 #include "mock_cells.h"
 //#include "debugging.h"
 '''
-
 
 _gtest_runner_template = """
 #include <gtest/gtest.h>
@@ -45,6 +44,7 @@ TEST ({suite}, {case})
 }}
 """
 
+
 def find_parent_test_function():
     """Return (filename, lineno, functionname) of the
     first function named "test_*" found on the stack."""
@@ -62,13 +62,12 @@ def find_parent_test_function():
     filename = info[0]
     lineno = info[1]
     function = info[2]
-    #context = info[3]
-    #contextindex = info[4]
 
     assert len(info) == 5
     assert function.startswith("test_")
 
     return filename, lineno, function
+
 
 class GTestContext:
     _all = []
@@ -101,7 +100,8 @@ class GTestContext:
         # Look through stack to find frame with test_... function name
         filename, lineno, function = find_parent_test_function()
 
-        # Using function testcase class name as suite and function name as case
+        # Using function testcase class name as suite and function
+        # name as case
         basename = os.path.basename(filename)
         suite = basename.replace(".py", "")
         case = function
@@ -141,11 +141,40 @@ class GTestContext:
         testincludes = '\n'.join('#include "{0}"'.format(h) for h in self._test_header_names)
 
         # Write test runner code to file
-        runner_code = _gtest_runner_template.format(supportcode=_supportcode, testincludes=testincludes)
+        runner_code = _gtest_runner_template.format(supportcode=_supportcode,
+                                                    testincludes=testincludes)
 
         self._main_filename = os.path.join(self._gendir, "main.cpp")
         with open(self._main_filename, "w") as f:
             f.write(runner_code)
+
+    def build_gtest(self):
+        "Build gtest library"
+
+        # Source and build directories
+        gtest_dir = "../../../libs/gtest-1.7.0"
+        build_dir = os.path.join(gtest_dir, "lib")
+
+        # Check if GTest source can be found
+        if os.path.isdir(gtest_dir):
+
+            # Make build directory, if required
+            if not os.path.isdir(build_dir):
+                os.mkdir(build_dir)
+
+            # Configure gtest using cmake
+            error = subprocess.call("cmake ..", cwd=build_dir, shell=True)
+            if error:
+                RuntimeError("Could not call make successfully to build gtest")
+
+            # Build gtest library
+            err = subprocess.call("make", cwd=build_dir, shell=True)
+            if error:
+                print("Could not call make successfully to build gtest")
+
+        else:
+            RuntimeError("Cannot find gtest source")
+
 
     def build(self):
         s, o = get_status_output("make")
@@ -168,16 +197,18 @@ class GTestContext:
         self.info(o)
 
     def finalize(self):
-        # Write generated test code to files, build and run, all from within a stable basedir
+        # Write generated test code to files, build and run, all from
+        # within a stable basedir
         self.pushdir()
         try:
+            self.build_gtest()
             self.write()
             self.build()
             self.run()
         finally:
             self.popdir()
 
-#@pytest.fixture("module")
+
 @pytest.fixture("session")
 def gtest():
     "create initial files for gtest generation"
@@ -185,12 +216,14 @@ def gtest():
     gtc = GTestContext(config)
     return gtc
 
+
 def gtest_sessionfinish(session):
     session.trace("finalizing gtest contexts")
     while GTestContext._all:
         gtc = GTestContext._all.pop()
         gtc.finalize()
     session.trace("done finalizing gtest contexts")
+
 
 def pytest_sessionfinish(session):
     gtest_sessionfinish(session)
