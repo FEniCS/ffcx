00a11a9811935f7b94cc87acb568165ec822f68b
Martin Sandve Alnæs <martinal@simula.no>
2016-08-05 15:47:48 +0200

    Update classifiers in setup.py.

--- setup.py
+++ setup.py
@@ -38,16 +38,21 @@ CLASSIFIERS = """\
 Development Status :: 5 - Production/Stable
 Intended Audience :: Developers
 Intended Audience :: Science/Research
-License :: OSI Approved :: GNU General Public License v2 (GPLv2)
-License :: Public Domain
-Operating System :: MacOS :: MacOS X
-Operating System :: Microsoft :: Windows
+License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)
 Operating System :: POSIX
 Operating System :: POSIX :: Linux
-Programming Language :: C++
+Operating System :: MacOS :: MacOS X
+Operating System :: Microsoft :: Windows
 Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 Topic :: Scientific/Engineering :: Mathematics
-Topic :: Software Development :: Libraries
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Software Development :: Code Generators
 """
 
 
