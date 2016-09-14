d4bc0aa877ae31f6bd16700cb813f3664c8e84d1
Jan Blechta <blechta@karlin.mff.cuni.cz>
2016-09-14 17:34:09 +0200

    Remove some blank lines from tsfc-generated code
    There is still some in the loops coming already from tsfc

--- ffc/tsfcrepr/tsfcgenerator.py
+++ ffc/tsfcrepr/tsfcgenerator.py
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with FFC. If not, see <http://www.gnu.org/licenses/>.
 
-import os
-
 from ffc.log import info
 from ffc.representationutils import initialize_integral_code
 
@@ -31,7 +29,7 @@ def generate_integral_code(ir, prefix, parameters):
 
     # Generate tabulate_tensor body from ast
     ast = ir["tsfc"].ast
-    tsfc_code = os.linesep.join(b.gencode() for b in ast.body)
+    tsfc_code = "".join(b.gencode() for b in ast.body)
     tsfc_code = tsfc_code.replace("#pragma coffee", "//#pragma coffee") # FIXME
     code["tabulate_tensor"] = tsfc_code
 
