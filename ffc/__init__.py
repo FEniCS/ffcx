845beede855c64241f9957290858c5385b887a9c
Martin Sandve Alnæs <martinal@simula.no>
2016-10-03 10:04:29 +0200

    Add import.

--- ffc/__init__.py
+++ ffc/__init__.py
@@ -23,7 +23,7 @@ from ffc.compiler import compile_form, compile_element
 from ffc.jitcompiler import jit
 
 # Import UFC config functions
-from ffc.backends.ufc import get_ufc_cxx_flags, get_ufc_signature, ufc_signature
+from ffc.backends.ufc import get_include_path, get_ufc_cxx_flags, get_ufc_signature, ufc_signature
 
 # Import default parameters
 from .parameters import default_parameters, default_jit_parameters
