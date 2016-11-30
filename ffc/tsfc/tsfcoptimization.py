e1abf0c31c58ec757868ac2236f15f8ae0d13b94
Jan Blechta <blechta@karlin.mff.cuni.cz>
2016-11-30 19:03:53 +0100

    Fail only when tsfc optimization requested

--- ffc/tsfc/tsfcoptimization.py
+++ ffc/tsfc/tsfcoptimization.py
@@ -1,8 +1,19 @@
-import coffee
-from coffee.plan import ASTKernel
+from ffc.log import error
+
+import traceback
+import os
 
 
-def optimize_integral_ir(ir, parameters):
-    knl = ASTKernel(ir["tsfc"])
-    knl.plan_cpu(dict(optlevel='O2'))  # TODO: optlevel from parameters
-    return ir  # AST was modified in-place
+try:
+    from coffee.plan import ASTKernel
+except ImportError:
+    msg = traceback.format_exc()
+    def optimize_integral_ir(ir, parameters):
+        error("Failed to import coffee.plan.ASTKernel needed for optimized tsfc"
+              " representation; the error message was: {}{}"
+              .format(os.linesep, msg))
+else:
+    def optimize_integral_ir(ir, parameters):
+        knl = ASTKernel(ir["tsfc"])
+        knl.plan_cpu(dict(optlevel='O2'))  # TODO: optlevel from parameters
+        return ir  # AST was modified in-place
