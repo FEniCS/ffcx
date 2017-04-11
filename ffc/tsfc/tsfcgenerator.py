0ca1573ccc4c61723a2b7cdd7d49591eecaf8aa0
Miklos Homolya <m.homolya14@imperial.ac.uk>
2017-04-11 13:21:19 +0100

    tsfc representation: include boost special function if needed

--- ffc/tsfc/tsfcgenerator.py
+++ ffc/tsfc/tsfcgenerator.py
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with FFC. If not, see <http://www.gnu.org/licenses/>.
 
+import coffee.base as coffee
+from coffee.visitors import Find
+
 from ffc.log import info
 from ffc.representationutils import initialize_integral_code
 
@@ -33,9 +36,13 @@ def generate_integral_code(ir, prefix, parameters):
     tsfc_code = tsfc_code.replace("#pragma coffee", "//#pragma coffee") # FIXME
     code["tabulate_tensor"] = tsfc_code
 
-    code["additional_includes_set"] = set()
-    code["additional_includes_set"].update(ir.get("additional_includes_set",()))
-    code["additional_includes_set"].update(ast.headers)
-    code["additional_includes_set"].add("#include <cstring>") # memset
+    includes = set()
+    includes.update(ir.get("additional_includes_set", ()))
+    includes.update(ast.headers)
+    includes.add("#include <cstring>")  # memset
+    if any(node.funcall.symbol.startswith("boost::math::")
+           for node in Find(coffee.FunCall).visit(ast)[coffee.FunCall]):
+        includes.add("#include <boost/math/special_functions.hpp>")
+    code["additional_includes_set"] = includes
 
     return code
