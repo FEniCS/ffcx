5da92db502e3c0b6e090346e171af6dd41926032
Miklos Homolya <m.homolya14@imperial.ac.uk>
2017-07-31 15:31:26 +0100

    switch to new and incomplete coffee optimizations

--- ffc/tsfc/tsfcoptimization.py
+++ ffc/tsfc/tsfcoptimization.py
@@ -1,19 +1,7 @@
-from ffc.log import error
-
-import traceback
-import os
-
-
-try:
-    from coffee.plan import ASTKernel
-except ImportError:
-    msg = traceback.format_exc()
-    def optimize_integral_ir(ir, parameters):
-        error("Failed to import coffee.plan.ASTKernel needed for optimized tsfc"
-              " representation; the error message was: {}{}"
-              .format(os.linesep, msg))
-else:
-    def optimize_integral_ir(ir, parameters):
-        knl = ASTKernel(ir["tsfc"])
-        knl.plan_cpu(dict(optlevel='O2'))  # TODO: optlevel from parameters
-        return ir  # AST was modified in-place
+def optimize_integral_ir(ir, parameters):
+    ir = ir.copy()
+    integral_data, form_data, prefix, parameters = ir["compile_integral"]
+    parameters = parameters.copy()
+    parameters["mode"] = "coffee"  # default optimization mode
+    ir["compile_integral"] = (integral_data, form_data, prefix, parameters)
+    return ir
