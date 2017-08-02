4c04ada86c8cefef9fe1d3fec76d8a5a1ad3cc18
Miklos Homolya <m.homolya14@imperial.ac.uk>
2017-08-02 17:37:06 +0100

    default mode changes to spectral

--- ffc/tsfc/tsfcoptimization.py
+++ ffc/tsfc/tsfcoptimization.py
@@ -2,6 +2,6 @@ def optimize_integral_ir(ir, parameters):
     ir = ir.copy()
     integral_data, form_data, prefix, parameters = ir["compile_integral"]
     parameters = parameters.copy()
-    parameters["mode"] = "coffee"  # default optimization mode
+    parameters["mode"] = "spectral"  # default optimization mode
     ir["compile_integral"] = (integral_data, form_data, prefix, parameters)
     return ir
