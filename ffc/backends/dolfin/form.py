87a48ccee265158b3ecb5a3b9be0c409e9d92c28
Garth N. Wells <gnw20@cam.ac.uk>
2018-04-10 22:57:43 +0100

    Cut more wrapper code.

--- ffc/backends/dolfin/form.py
+++ ffc/backends/dolfin/form.py
@@ -72,9 +72,6 @@ def generate_form(form, classname):
 def generate_form_class(form, classname):
     "Generate dolfin wrapper code for a single Form class."
 
-    # Generate constructors
-    constructors = generate_form_constructors(form, classname)
-
     # Generate data for coefficient assignments
     (number, name) = generate_coefficient_map_data(form)
 
@@ -92,46 +89,12 @@ def generate_form_class(form, classname):
 
     # Wrap functions in class body
     code = ""
-    code += apply_form_template(classname, form, constructors, number, name,
+    code += apply_form_template(classname, form, number, name,
                                 additionals, form.superclassname)
     code += "\n"
 
     # Return code
     return code
-
-
-# def generate_coefficient_map_data(form):
-#     """Generate data for code for the functions
-#     Form::coefficient_number and Form::coefficient_name."""
-
-#     # Write error if no coefficients
-#     if form.num_coefficients == 0:
-#         message = '''\
-# dolfin::log::dolfin_error("generated code for class %s",
-#                          "access coefficient data",
-#                          "There are no coefficients");''' % form.superclassname
-#         num = "\n    %s\n    return 0;" % message
-#         name = '\n    %s\n    return "unnamed";' % message
-#         return (num, name)
-
-#     # Otherwise create switch
-#     ifstr = "if "
-#     num = ""
-#     name = '    switch (i)\n    {\n'
-#     for i, coeff in enumerate(form.coefficient_names):
-#         num += '    %s(name == "%s")\n      return %d;\n' % (ifstr, coeff, i)
-#         name += '    case %d:\n      return "%s";\n' % (i, coeff)
-#         ifstr = 'else if '
-
-#     # Create final return
-#     message = '''\
-# dolfin::log::dolfin_error("generated code for class %s",
-#                          "access coefficient data",
-#                          "Invalid coefficient");''' % form.superclassname
-#     num += "\n    %s\n    return 0;" % message
-#     name += '    }\n\n    %s\n    return "unnamed";' % message
-
-#     return (num, name)
 
 
 def generate_coefficient_map_data(form):
@@ -164,89 +127,14 @@ dolfin::log::dolfin_error("generated code for class %s",
     return (num, name)
 
 
-def generate_form_constructors(form, classname):
-    """Generate the dolfin::fem::Form constructors for different
-    combinations of references/shared pointers etc."""
-
-    # FIXME: This can be simplied now that we don't create reference version
-
-    coeffs = ("shared_ptr_coefficient",)
-    spaces = ("shared_ptr_space",)
-
-    # Treat functionals a little special
-    if form.rank == 0:
-        spaces = ("shared_ptr_mesh",)
-
-    # Generate permutations of constructors
-    constructors = []
-    for space in spaces:
-        constructors += [generate_constructor(form, classname, space)]
-        # if form.num_coefficients > 0:
-        #     constructors += [generate_constructor(form, classname, space, coeff)
-        #                      for coeff in coeffs]
-
-    # Return joint constructor code
-    return "\n\n".join(constructors)
-
-
-def generate_constructor(form, classname, space_tag, coefficient_tag=None):
-    "Generate a single Form constructor according to the given parameters."
-
-    # Extract correct code snippets
-    (argument, assign) = snippets[space_tag]
-
-    # Construct list of arguments and function space assignments
-    name = "V%d"
-
-    if form.rank > 0:
-        arguments = [argument % (name % i) for i in reversed(range(form.rank))]
-    else:
-        arguments = [argument]
-
-    f_spaces = "{" + ", ".join([name % i for i in range(form.rank)]) + "}"
-
-    assignments = []
-
-    # Add coefficients to argument/assignment lists if specified
-    if coefficient_tag is not None:
-        (argument, assign) = snippets[coefficient_tag]
-        arguments += [argument % name for name in form.coefficient_names]
-        if form.rank > 0:  # FIXME: To match old generated code only
-            assignments += [""]
-        assignments += [assign % (name, name)
-                        for name in form.coefficient_names]
-
-    # Construct list for initialization of Coefficient references
-    # initializers = ["%s(*this, %d)" % (name, number)
-    #                 for (number, name) in enumerate(form.coefficient_names)]
-    initializers = []
-
-    # Join lists together
-    arguments = ", ".join(arguments)
-    initializers = ", " + ", ".join(initializers) if initializers else ""
-    body = "\n".join(assignments)
-
-    # Wrap code into template
-    args = {"classname": classname,
-            "ufc_form": "std::make_shared<const %s>()" % form.ufc_form_classname,
-            "spaces": f_spaces,
-            "arguments": arguments,
-            "initializers": initializers,
-            "body": body,
-            "superclass": form.superclassname
-            }
-    code = form_constructor_template % args
-    return code
-
-
 form_class_template = """\
-/// Return the number of the coefficient with this name
+// Return the number of the coefficient with this name
 int %(classname)s_coefficient_number(const char* name)
 {
 %(coefficient_number)s
 }
 
-/// Return the name of the coefficient with this number
+// Return the name of the coefficient with this number
 const char* %(classname)s_coefficient_name(int i)
 {
 %(coefficient_name)s
@@ -263,22 +151,12 @@ dolfin_form* %(classname)s_factory()
 
 """
 
-# Template code for Form constructor
-form_constructor_template = """\
-  // Constructor
-  %(classname)s(%(arguments)s):
-    dolfin::fem::%(superclass)s(%(ufc_form)s, %(spaces)s)%(initializers)s
-  {
-%(body)s
-  }"""
 
-
-def apply_form_template(classname, form, constructors, number, name, members,
+def apply_form_template(classname, form, number, name, members,
                         superclass):
     args = {"classname": classname,
             "ufc_form": form.ufc_form_classname,
             "superclass": superclass,
-            "constructors": constructors,
             "coefficient_number": number,
             "coefficient_name": name,
             "members": members}
