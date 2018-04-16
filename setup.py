87ac6e0ebf9fc4913461f9bcf02d794ce84d5089
Garth N. Wells <gnw20@cam.ac.uk>
2018-04-16 11:24:55 +0100

    Update setup.py for module paths.

--- setup.py
+++ setup.py
@@ -24,11 +24,9 @@ else:
         "fenics-dijitso%s" % RESTRICT_REQUIREMENTS,
     ]
 
-
 URL = "https://bitbucket.org/fenics-project/ffc/"
 
-ENTRY_POINTS = {'console_scripts': ['ffc = ffc.__main__:main',
-                                    'ffc-3 = ffc.__main__:main']}
+ENTRY_POINTS = {'console_scripts': ['ffc = ffc.__main__:main', 'ffc-3 = ffc.__main__:main']}
 
 AUTHORS = """\
 Anders Logg, Kristian Oelgaard, Marie Rognes, Garth N. Wells,
@@ -97,8 +95,10 @@ def get_git_commit_hash():
 
 def write_config_file(infile, outfile, variables={}):
     "Write config file based on template"
+
     class AtTemplate(string.Template):
         delimiter = "@"
+
     s = AtTemplate(open(infile, "r").read())
     s = s.substitute(**variables)
     with open(outfile, "w") as a:
@@ -107,9 +107,10 @@ def write_config_file(infile, outfile, variables={}):
 
 def generate_git_hash_file(GIT_COMMIT_HASH):
     "Generate module with git hash"
-    write_config_file(os.path.join("ffc", "git_commit_hash.py.in"),
-                      os.path.join("ffc", "git_commit_hash.py"),
-                      variables=dict(GIT_COMMIT_HASH=GIT_COMMIT_HASH))
+    write_config_file(
+        os.path.join("ffc", "git_commit_hash.py.in"),
+        os.path.join("ffc", "git_commit_hash.py"),
+        variables=dict(GIT_COMMIT_HASH=GIT_COMMIT_HASH))
 
 
 def run_install():
@@ -127,38 +128,38 @@ def run_install():
 
     # FFC data files
     data_files = [(os.path.join("share", "man", "man1"),
-                  [os.path.join("doc", "man", "man1", "ffc.1.gz")])]
+                   [os.path.join("doc", "man", "man1", "ffc.1.gz")])]
 
     # Call distutils to perform installation
-    setup(name="fenics-ffc",
-          description="The FEniCS Form Compiler",
-          version=VERSION,
-          author=AUTHORS,
-          classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
-          license="LGPL version 3 or later",
-          author_email="fenics-dev@googlegroups.com",
-          maintainer_email="fenics-dev@googlegroups.com",
-          url=URL,
-          download_url=tarball(),
-          platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
-          packages=["ffc",
-                    "ffc.backends",
-                    "ffc.backends.ufc",
-                    "ffc.tsfc",
-                    "ffc.uflacs",
-                    "ffc.uflacs.analysis",
-                    "ffc.uflacs.backends",
-                    "ffc.uflacs.backends.ffc",
-                    "ffc.uflacs.backends.ufc",
-                    "ffc.uflacs.language",
-                    ],
-          package_dir={"ffc": "ffc"},
-          package_data={"ffc": [os.path.join('backends', 'ufc', '*.h')]},
-          #scripts=scripts,  # Using entry_points instead
-          entry_points=entry_points,
-          data_files=data_files,
-          install_requires=REQUIREMENTS,
-          zip_safe=False)
+    setup(
+        name="fenics-ffc",
+        description="The FEniCS Form Compiler",
+        version=VERSION,
+        author=AUTHORS,
+        classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+        license="LGPL version 3 or later",
+        author_email="fenics-dev@googlegroups.com",
+        maintainer_email="fenics-dev@googlegroups.com",
+        url=URL,
+        download_url=tarball(),
+        platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+        packages=[
+            "ffc",
+            "ffc.backends",
+            "ffc.backends.ffc",
+            "ffc.backends.ufc",
+            "ffc.tsfc",
+            "ffc.uflacs",
+            "ffc.uflacs.analysis",
+            "ffc.uflacs.language",
+        ],
+        package_dir={"ffc": "ffc"},
+        package_data={"ffc": [os.path.join('backends', 'ufc', '*.h')]},
+        #scripts=scripts,  # Using entry_points instead
+        entry_points=entry_points,
+        data_files=data_files,
+        install_requires=REQUIREMENTS,
+        zip_safe=True)
 
 
 if __name__ == "__main__":
