5f901bcd37f5a93a821e7ebe7d8187832653174c
Fabian Löschner <fabian.loeschner@rwth-aachen.de>
2018-07-26 16:02:00 +0200

    Last whitespace error, I hope...

--- ffc/analysis.py
+++ ffc/analysis.py
@@ -377,8 +377,8 @@ def _extract_common_metadata(integral_metadatas, ida, form_data, form_r_family, 
     # Functions that generate sets of metadata parameter values
     def equal_set_gen(em):
         return {md[em] for md in integral_metadatas}
-    
-    def equal_or_auto_set_gen(em): 
+
+    def equal_or_auto_set_gen(em):
         return {md[em] for md in integral_metadatas if md[em] != "auto"}
 
     # Function that extracts a unique parameter value or raises an error
