5b94e8a18b22ebcc0cf94bee17173233baa72803
Jorgen <jsd55@cam.ac.uk>
2019-10-08 10:39:27 +0100

    Removing local hack for finding number of dofs for a component of the coordinate element, using the fiat interface

--- ffc/codegeneration/definitions.py
+++ ffc/codegeneration/definitions.py
@@ -16,34 +16,15 @@ logger = logging.getLogger(__name__)
 def num_coordinate_component_dofs(coordinate_element):
     """Get the number of dofs for a coordinate component for this degree.
 
-    This is a local hack that works for Lagrange 1-3, better
-    would be to get this passed by ffc from fiat through the ir.
-    The table data is to messy to figure out a clean design for that quickly.
     """
-    from ufl.cell import num_cell_entities
-    degree = coordinate_element.degree()
-    cell = coordinate_element.cell()
-    tdim = cell.topological_dimension()
-    cellname = cell.cellname()
+    from ffc.fiatinterface import create_element
+    # Extracting only first component degrees of freedom from FIAT
+    fiat_element = create_element(coordinate_element).elements()[0]
+    dof_dict = fiat_element.entity_dofs()
     d = 0
-    for entity_dim in range(tdim + 1):
-        # n = dofs per cell entity
-        if entity_dim == 0:
-            n = 1
-        elif entity_dim == 1:
-            n = degree - 1
-        elif entity_dim == 2:
-            if cellname == "quadrilateral":
-                n = (degree - 1) * (degree - 1)
-            else:
-                n = (degree - 2) * (degree - 1) // 2
-        elif entity_dim == 3:
-            n = (degree - 3) * (degree - 2) * (degree - 1) // 6
-        else:
-            raise RuntimeError("Entity dimension out of range")
-        # Accumulate
-        num_entities = num_cell_entities[cellname][entity_dim]
-        d += num_entities * n
+    for entity_dim in dof_dict.keys():
+        for enitity in dof_dict[entity_dim]:
+            d += len(dof_dict[entity_dim][enitity])
     return d
 
 
