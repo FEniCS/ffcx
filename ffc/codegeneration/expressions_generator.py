07c42f15db4adb6786b26e8eef528050129bf502
Michal Habera <michal.habera@gmail.com>
2019-10-27 14:37:29 +0100

    Fix flake

--- ffc/codegeneration/expressions_generator.py
+++ ffc/codegeneration/expressions_generator.py
@@ -5,7 +5,6 @@
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 
 import collections
-import itertools
 import logging
 
 import ufl
@@ -496,4 +495,3 @@ class ExpressionGenerator:
         parts = L.ArrayDecl("static const int", "value_shape", values=self.ir.expression_shape,
                             sizes=len(self.ir.expression_shape))
         return parts
-
