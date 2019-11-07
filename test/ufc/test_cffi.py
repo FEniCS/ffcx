dc2eabefe0ad6134d540cca44c0144266fbeef33
Jorgen <jsd55@cam.ac.uk>
2019-11-07 12:41:27 +0000

    Added compute_physical_coordinates test

--- test/ufc/test_cffi.py
+++ test/ufc/test_cffi.py
@@ -109,13 +109,15 @@ def test_cmap():
     coords = np.array([0.0, 0.0, 2.0, 0.0, 0.0, 4.0], dtype=np.float64)
     coords_ptr = module.ffi.cast("double *", module.ffi.from_buffer(coords))
     compiled_cmap[0].compute_reference_coordinates(X_ptr, X.shape[0], x_ptr, coords_ptr, 0)
+
     assert(np.isclose(X[0, 0], 0.25))
     assert(np.isclose(X[0, 1], 0.125))
 
 
-@pytest.mark.parametrize("degree,coords", [(1, np.array([[0, 0], [0, 2], [2, 0], [2, 2]], dtype=np.float64)),
-                                           (2, np.array([[0, 0], [0, 2], [0, 1], [2, 0],
-                                                         [2, 2], [2, 1], [1, 0], [1, 2], [1, 1]], dtype=np.float64))])
+@pytest.mark.parametrize("degree,coords", [(1, np.array([[0, 0], [0, 2], [3, 0], [3, 2]], dtype=np.float64)),
+                                           (2, np.array([[0, 0], [0, 2], [0, 1], [3, 0],
+                                                         [3, 2], [3, 1], [1.5, 0], [1.5, 2], [1.5, 1]],
+                                                        dtype=np.float64))])
 def test_cmap_quads(degree, coords):
     # Test for first and second order quadrilateral meshes,
     # assuming FIAT Tensor Product layout of cell.
@@ -124,15 +126,25 @@ def test_cmap_quads(degree, coords):
     e = ufl.VectorElement("Lagrange", cell, degree)
     mesh = ufl.Mesh(e)
     compiled_cmap, module = ffc.codegeneration.jit.compile_coordinate_maps([mesh])
-    x = np.array([[1, 0.5]], dtype=np.float64)
+    x = np.array([[0.5, 0.5]], dtype=np.float64)
     x_ptr = module.ffi.cast("double *", module.ffi.from_buffer(x))
     X = np.zeros_like(x)
     X_ptr = module.ffi.cast("double *", module.ffi.from_buffer(X))
 
     coords_ptr = module.ffi.cast("double *", module.ffi.from_buffer(coords))
     compiled_cmap[0].compute_reference_coordinates(X_ptr, X.shape[0], x_ptr, coords_ptr, 0)
-    assert(np.isclose(X[0, 0], x[0, 0] / 2))
+
+    assert(np.isclose(X[0, 0], x[0, 0] / 3))
     assert(np.isclose(X[0, 1], x[0, 1] / 2))
+
+    x = np.array([[1 / 3, 1 / 3]], dtype=np.float64)
+    x_ptr = module.ffi.cast("double *", module.ffi.from_buffer(x))
+    X_physical = np.zeros_like(x)
+    X_physical_ptr = module.ffi.cast("double *", module.ffi.from_buffer(X_physical))
+    compiled_cmap[0].compute_physical_coordinates(X_physical_ptr, X.shape[0], x_ptr, coords_ptr)
+
+    assert(np.isclose(X_physical[0, 0], 3 * x[0, 0]))
+    assert(np.isclose(X_physical[0, 1], 2 * x[0, 1]))
 
 
 @pytest.mark.parametrize("mode,expected_result", [
