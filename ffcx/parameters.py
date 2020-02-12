5f3f1e057a9f352d4bb0cd568ff2278f10ba4696
Michal Habera <michal.habera@gmail.com>
2020-02-12 11:04:17 -0800

    Fix docstring

--- ffcx/parameters.py
+++ ffcx/parameters.py
@@ -11,9 +11,9 @@ logger = logging.getLogger(__name__)
 
 FFCX_PARAMETERS = {
     "representation": "auto",  # form representation / code generation strategy
-    "quadrature_rule": "auto",  # quadrature rule used for integration of element tensors (None is auto)
-    "quadrature_degree": -1,  # quadrature degree used for computing integrals (None is auto)
-    "precision": -1,  # precision used when writing numbers (None for max precision)
+    "quadrature_rule": "auto",  # quadrature rule used for integration of element tensors
+    "quadrature_degree": -1,  # quadrature degree used for computing integrals (-1 means auto)
+    "precision": -1,  # precision used when writing numbers (-1 for max precision)
     "epsilon": 1e-14,  # machine precision, used for dropping zero terms in tables
     # Scalar type to be used in generated code (real or complex
     # C double precision floating-point types)
