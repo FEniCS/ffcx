910c3f6cc658308e6e60068517c42d99fe33c4c2
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-06-25 12:25:58 +0100

    Correct entity dofs for reordered vectorelement

--- ffcx/ir/representation.py
+++ ffcx/ir/representation.py
@@ -26,7 +26,7 @@ import numpy
 import FIAT
 import ufl
 from ffcx import naming
-from ffcx.fiatinterface import SpaceOfReals, create_element
+from ffcx.fiatinterface import SpaceOfReals, create_element, _compute_entity_dofs
 from ffcx.ir import dof_permutations
 from ffcx.ir.integral import compute_integral_ir
 from ffcx.ir.representationutils import (QuadratureRule,
@@ -211,7 +211,7 @@ def _compute_dofmap_ir(ufl_element, element_numbers, dofmap_names):
 
     # Precompute repeatedly used items
     num_dofs_per_entity = _num_dofs_per_entity(fiat_element)
-    entity_dofs = fiat_element.entity_dofs()
+    entity_dofs = _compute_entity_dofs(fiat_element, ufl_element)
 
     # Store id
     ir = {"id": element_numbers[ufl_element]}
@@ -566,6 +566,8 @@ def _compute_form_ir(form_data, form_id, prefix, element_numbers, finite_element
     ir["create_dofmap"] = [
         dofmap_names[e] for e in form_data.argument_elements + form_data.coefficient_elements
     ]
+
+    # from IPython import embed; embed()
 
     fs = {}
     for function in form_data.original_form.arguments() + tuple(form_data.reduced_coefficients):
