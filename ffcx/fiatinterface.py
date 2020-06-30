80643323f1344a05fb9e21dbdd314688435eec90
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-06-30 15:34:24 +0100

    array -> numpy.array

--- ffcx/fiatinterface.py
+++ ffcx/fiatinterface.py
@@ -143,8 +143,8 @@ def _create_vector_finiteelement(element: ufl.VectorElement) -> FIAT.MixedElemen
     def tabulate(self, order, points, entity=None):
         block_size = self.num_sub_elements()
         return {
-            i: array([item[j] for block in range(block_size)
-                for j in range(block, len(item), block_size)])
+            i: numpy.array([item[j] for block in range(block_size)
+                            for j in range(block, len(item), block_size)])
             for i, item in self.old_tabulate(order, points, entity=entity).items()
         }
 
