84aab51d848abef69e47f6f527a50dbf848e751c
Jack S. Hale <mail@jackhale.co.uk>
2021-10-18 13:44:16 +0200

    Editable installs, cleanup manifest.

--- setup.py
+++ setup.py
@@ -1,2 +1,9 @@
 import setuptools
+
+# Can be removed when pip editable user installs are fixed
+# https://github.com/pypa/pip/issues/7953
+import site
+import sys
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
 setuptools.setup()
