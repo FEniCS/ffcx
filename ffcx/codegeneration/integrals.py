bb76c6bfd311a713eccad46a4cb6be590d701d5f
IgorBaratta <igorbaratta@gmail.com>
2021-11-17 12:24:02 +0000

    fix flake8 again

--- ffcx/codegeneration/integrals.py
+++ ffcx/codegeneration/integrals.py
@@ -692,7 +692,7 @@ class IntegralGenerator(object):
     def fuse_loops(self, definitions):
         """
         Merge a sequence of loops with the same iteration space into a single loop.
-        
+
         Loop fusion improves data locality, cache reuse and decreases the loop control overhead.
         NOTE: Loop fusion might increase the pressure on register allocation.
         Ideally, we should define a cost function to determine how many loops should fuse at a time.
