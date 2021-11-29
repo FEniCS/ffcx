5b2edd6accf1ac36a9626cfd74691241bf1f5c6c
Garth N. Wells <gnw20@cam.ac.uk>
2021-11-29 09:57:11 +0000

    Remove ufc_blocked_element enum (#431)
    * Remmove ufc_blocked_element enum. Fixes #417.
    
    Also fixes some formatting.
    
    * Undo line breaks
    
    * return sub element type, not ufc_basix_element
    
    Co-authored-by: Matthew Scroggs <matthew.w.scroggs@gmail.com>

--- ffcx/codegeneration/form.py
+++ ffcx/codegeneration/form.py
@@ -6,6 +6,7 @@
 
 # Note: Most of the code in this file is a direct translation from the
 # old implementation in FFC
+
 import logging
 
 from ffcx.codegeneration import form_template
--- ffcx/codegeneration/ufc.h
+++ ffcx/codegeneration/ufc.h
@@ -63,9 +63,8 @@ extern "C"
   {
     ufc_basix_element = 0,
     ufc_mixed_element = 1,
-    ufc_blocked_element = 2,
-    ufc_quadrature_element = 3,
-    ufc_custom_element = 4
+    ufc_quadrature_element = 2,
+    ufc_custom_element = 3
   } ufc_element_type;
 
   /// Forward declarations
