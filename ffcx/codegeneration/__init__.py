d324271bafe8e9f1aac81c8740ad64fc4d74c56e
Garth N. Wells <gnw20@cam.ac.uk>
2022-01-12 12:34:53 +0000

    Revert CI branch

--- ffcx/codegeneration/__init__.py
+++ ffcx/codegeneration/__init__.py
@@ -4,20 +4,21 @@ import hashlib
 # Version of FFCx header files
 __author__ = "FEniCS Project"
 __license__ = "This code is released into the public domain"
-__version__ = "2018.2.0.dev0"
+__version__ = "0.3.1"
 
 # Get abspath on import, it can in some cases be a relative path w.r.t.
 # curdir on startup
 _include_path = os.path.dirname(os.path.abspath(__file__))
 
 
-def get_include_path():
-    """Return location of UFC header files."""
+@property
+def include_path():
+    """Location of UFC header files"""
     return _include_path
 
 
 def _compute_signature():
-    # Compute signature of ufc header files
+    # Compute signature of ufcx header file
     h = hashlib.sha1()
     with open(os.path.join(get_include_path(), "ufcx.h")) as f:
         h.update(f.read().encode("utf-8"))
@@ -27,8 +28,9 @@ def _compute_signature():
 _signature = _compute_signature()
 
 
-def get_signature():
-    """Return SHA-1 hash of the contents of ufcx.h.
+@property
+def ufcx_sha1():
+    """SHA-1 hash of the ufcx.h file
 
     In this implementation, the value is computed on import.
     """
