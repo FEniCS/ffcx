8083aa813bf1016b814053b25d3945b2ad7972cd
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-04-28 16:26:30 +0100

    update script

--- update_version.py
+++ update_version.py
@@ -21,8 +21,8 @@ import os
 import re
 
 
-ef replace_version(content, version, next_version):
-    content = re.sub(r"((?:VERSION)|(?:version))([\s=]+)([\"']).+\3",
+def replace_version(content, version, next_version):
+    content = re.sub(r"((?:VERSION)|(?:version))([\s=]+)([\"']).+?\3",
                      lambda matches: f"{matches[1]}{matches[2]}{matches[3]}{version}{matches[3]}", content)
     content = re.sub(r"(\s+)(\"?)fenics-((?:basix)|(?:ffcx)|(?:dolfinx))\>\=.+(\2|\n)",
                      lambda matches: f"{matches[1]}{matches[2]}fenics-{matches[3]}>={version},<{next_version}{matches[4]}",
