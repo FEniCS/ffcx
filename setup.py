5cd949957ad61bd144bbca2f9b7ed7210a8aeb71
Jørgen Dokken <dokken92@gmail.com>
2022-05-23 13:39:38 +0000

    Similar fix as: https://github.com/FEniCS/ufl/pull/104

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 import setuptools
-
-# Can be removed when pip editable user installs are fixed
-# https://github.com/pypa/pip/issues/7953
-import site
 import sys
-site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+if sys.version_info < (3, 10):
+    # Can be removed when pip editable user installs are fixed
+    # https://github.com/pypa/pip/issues/7953
+    import site
+    site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 setuptools.setup()
