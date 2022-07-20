f55992587e3f8f814cf977b3fcb00355bc1bbcd4
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-07-20 14:53:31 +0100

    make equispaced for all degrees

--- ffcx/analysis.py
+++ ffcx/analysis.py
@@ -154,7 +154,7 @@ def _analyze_form(form: ufl.form.Form, parameters: typing.Dict) -> ufl.algorithm
     for n, i in enumerate(form._integrals):
         element = i._ufl_domain._ufl_coordinate_element
         assert not isinstance(element, basix.ufl_wrapper._BasixElementBase)
-        if element._sub_element._variant is None and element.degree() > 2:
+        if element._sub_element._variant is None:
             sub_element = ufl.FiniteElement(
                 element.family(), element.cell(), element.degree(), element.quadrature_scheme(),
                 variant="equispaced")
