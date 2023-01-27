ec10ab0837774565222c2a4a65d72c70c0a97182
Michal Habera <michal.habera@gmail.com>
2023-01-27 16:15:41 +0100

    Raise on invalid function name (#539)
    * Raise on invalid function name
    
    * Fix typo
    
    * Add test for invalid name
    
    * Update invalid name test
    
    * Update invalid name test
    
    * Update invalid name test
    
    ---------
    
    Co-authored-by: Jørgen Schartum Dokken <jsd55@cam.ac.uk>
    Co-authored-by: Garth N. Wells <gnw20@cam.ac.uk>

--- ffcx/ir/representation.py
+++ ffcx/ir/representation.py
@@ -533,6 +533,8 @@ def _compute_form_ir(form_data, form_id, prefix, form_names, integral_names, ele
     fs = {}
     for function in form_data.original_form.arguments() + tuple(form_data.reduced_coefficients):
         name = object_names.get(id(function), str(function))
+        if not str(name).isidentifier():
+            raise ValueError(f"Function name \"{name}\" must be a valid object identifier.")
         el = convert_element(convert_element(function.ufl_function_space().ufl_element()))
         cmap = function.ufl_function_space().ufl_domain().ufl_coordinate_element()
         # Default point spacing for CoordinateElement is equispaced
@@ -642,6 +644,8 @@ def _compute_expression_ir(expression, index, prefix, analysis, options, visuali
     fs = {}
     for function in tuple(original_coefficients) + tuple(arguments):
         name = object_names.get(id(function), str(function))
+        if not str(name).isidentifier():
+            raise ValueError(f"Function name \"{name}\" must be a valid object identifier.")
         el = convert_element(function.ufl_function_space().ufl_element())
         cmap = convert_element(function.ufl_function_space().ufl_domain().ufl_coordinate_element())
         family = cmap.family()
