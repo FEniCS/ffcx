84aaacbe524435b884883248a83cd81e1bf23bd2
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-03-20 15:35:42 +0000

    update function name in tests

--- test/test_elements.py
+++ test/test_elements.py
@@ -23,7 +23,7 @@
 import numpy as np
 import pytest
 
-from basix.ufl import finite_element
+from basix.ufl import element
 
 
 def element_coords(cell):
@@ -49,28 +49,28 @@ def random_point(shape):
 @pytest.mark.parametrize("degree, expected_dim", [(1, 3), (2, 6), (3, 10)])
 def test_continuous_lagrange(degree, expected_dim):
     "Test space dimensions of continuous Lagrange elements."
-    P = finite_element("Lagrange", "triangle", degree)
+    P = element("Lagrange", "triangle", degree)
     assert P.dim == expected_dim
 
 
 @pytest.mark.parametrize("degree, expected_dim", [(1, 4), (2, 9), (3, 16)])
 def xtest_continuous_lagrange_quadrilateral(degree, expected_dim):
     "Test space dimensions of continuous TensorProduct elements (quadrilateral)."
-    P = finite_element("Lagrange", "quadrilateral", degree)
+    P = element("Lagrange", "quadrilateral", degree)
     assert P.dim == expected_dim
 
 
 @pytest.mark.parametrize("degree, expected_dim", [(1, 4), (2, 9), (3, 16)])
 def xtest_continuous_lagrange_quadrilateral_spectral(degree, expected_dim):
     "Test space dimensions of continuous TensorProduct elements (quadrilateral)."
-    P = finite_element("Lagrange", "quadrilateral", degree, variant="spectral")
+    P = element("Lagrange", "quadrilateral", degree, variant="spectral")
     assert P.dim == expected_dim
 
 
 @pytest.mark.parametrize("degree, expected_dim", [(0, 1), (1, 3), (2, 6), (3, 10)])
 def test_discontinuous_lagrange(degree, expected_dim):
     "Test space dimensions of discontinuous Lagrange elements."
-    P = finite_element("DG", "triangle", degree)
+    P = element("DG", "triangle", degree)
     assert P.dim == expected_dim
 
 
@@ -78,7 +78,7 @@ def test_discontinuous_lagrange(degree, expected_dim):
                          [(0, 3), (1, 9), (2, 18), (3, 30)])
 def test_regge(degree, expected_dim):
     "Test space dimensions of generalized Regge element."
-    P = finite_element("Regge", "triangle", degree)
+    P = element("Regge", "triangle", degree)
     assert P.dim == expected_dim
 
 
@@ -86,7 +86,7 @@ def test_regge(degree, expected_dim):
                          [(0, 3), (1, 9), (2, 18), (3, 30)])
 def xtest_hhj(degree, expected_dim):
     "Test space dimensions of Hellan-Herrmann-Johnson element."
-    P = finite_element("HHJ", "triangle", degree)
+    P = element("HHJ", "triangle", degree)
     assert P.dim == expected_dim
 
 
@@ -182,14 +182,14 @@ supported (non-mixed) for low degrees"""
     @pytest.mark.parametrize("family, cell, degree, reference", tests)
     def test_values(self, family, cell, degree, reference):
         # Create element
-        element = finite_element(family, cell, degree)
+        e = element(family, cell, degree)
 
         # Get some points and check basis function values at points
         points = [random_point(element_coords(cell)) for i in range(5)]
         for x in points:
-            table = element.tabulate(0, (x,))
+            table = e.tabulate(0, (x,))
             basis = table[0]
-            if sum(element.value_shape()) == 1:
+            if sum(e.value_shape()) == 1:
                 for i, value in enumerate(basis[0]):
                     assert np.isclose(value, reference[i](x))
             else:
