c57c403aba5ddb1f2f6eb248c1bc934bb523b1a9
Chris Richardson <chris@bpi.cam.ac.uk>
2023-08-23 10:37:44 +0100

    Tidy up

--- ffcx/codegeneration/definitions.py
+++ ffcx/codegeneration/definitions.py
@@ -98,9 +98,7 @@ class FFCXDefinitions(object):
 
             # If a map is necessary from stride 1 to bs, the code must be added before the quadrature loop.
             if dof_access_map:
-                pre_code += [
-                    L.ArrayDecl(dof_access.array, sizes=num_dofs)
-                ]
+                pre_code += [L.ArrayDecl(dof_access.array, sizes=num_dofs)]
                 pre_body = [L.Assign(dof_access, dof_access_map)]
                 pre_code += [L.ForRange(ic, 0, num_dofs, pre_body)]
         else:
