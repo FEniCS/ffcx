9fd04f27ce1d980c532e7a49c08b8d60dd1df6e4
IgorBaratta <igorbaratta@gmail.com>
2024-01-08 16:49:31 +0000

    fix flake8

--- ffcx/codegeneration/optimizer.py
+++ ffcx/codegeneration/optimizer.py
@@ -1,5 +1,6 @@
 from typing import List
 import ffcx.codegeneration.lnodes as L
+from collections import defaultdict
 
 
 def optimize(code: List[L.LNode]) -> List[L.LNode]:
@@ -19,11 +20,17 @@ def optimize(code: List[L.LNode]) -> List[L.LNode]:
 
     code = fuse_sections(code, "Jacobian")
 
+    for i, section in enumerate(code):
+        if isinstance(section, L.Section):
+            if section.name == "Jacobian":
+                section = fuse_loops(section)
+                code[i] = section
+
     return code
 
 
 def fuse_sections(code: List[L.LNode], name) -> List[L.LNode]:
-    """Fuse sections.
+    """Fuse sections with the same name and same annotations.
 
     Parameters
     ----------
@@ -69,8 +76,41 @@ def fuse_sections(code: List[L.LNode], name) -> List[L.LNode]:
     section = L.Section(name, declarations + loops, input, output)
 
     # Replace the first section with the fused section
+    code = code.copy()
     code[indices[0]] = section
     # Remove the other sections
     code = [c for i, c in enumerate(code) if i not in indices[1:]]
 
     return code
+
+
+def fuse_loops(code: L.Section) -> List[L.Section]:
+    """Fuse loops with the same range and same annotations.
+
+    Parameters
+    ----------
+    code : list of LNodes
+        List of LNodes to fuse.
+
+    Returns
+    -------
+    list of LNodes
+        Fused list of LNodes.
+
+    """
+
+    loops = defaultdict(list)
+    pre_loop = []
+    for statement in code.statements:
+        if isinstance(statement, L.ForRange):
+            id = (statement.index, statement.begin, statement.end)
+            loops[id].append(statement.body)
+        else:
+            pre_loop.append(statement)
+
+    for info, body in loops.items():
+        index, begin, end = info
+        loop = L.ForRange(index, begin, end, body)
+        pre_loop.append(loop)
+
+    return L.Section(code.name, pre_loop, code.input, code.output)
