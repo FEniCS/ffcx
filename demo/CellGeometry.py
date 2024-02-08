8d80ecacb5f6dc57022dc0deb16ca53b08796e8d
Garth N. Wells <gnw20@cam.ac.uk>
2024-02-08 07:52:58 +0000

    Tidy demo

--- demo/CellGeometry.py
+++ demo/CellGeometry.py
@@ -25,16 +25,15 @@ domain = Mesh(basix.ufl.element("P", "tetrahedron", 1, shape=(3,)))
 space = FunctionSpace(domain, V)
 u = Coefficient(space)
 
-# TODO: Add all geometry for all cell types to this and other demo files, need for regression test.
+# TODO: Add all geometry for all cell types to this and other demo
+# files, need for regression test.
 x = SpatialCoordinate(domain)
 n = FacetNormal(domain)
 vol = CellVolume(domain)
 rad = Circumradius(domain)
 area = FacetArea(domain)
 
-M = (
-    u * (x[0] * vol * rad) * dx + u * (x[0] * vol * rad * area) * ds
-)  # + u*area*avg(n[0]*x[0]*vol*rad)*dS
+M = u * (x[0] * vol * rad) * dx + u * (x[0] * vol * rad * area) * ds
 
 # Test some obscure functionality
 fev = FacetEdgeVectors(domain)
