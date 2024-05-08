56d3236eeb0280aa1b332ebedf9cdeec75d1a5fe
Jack S. Hale <mail@jackhale.co.uk>
2024-05-08 11:02:18 +0200

    Try running demos on Windows.

--- demo/test_demos.py
+++ demo/test_demos.py
@@ -17,6 +17,10 @@ for file in os.listdir(demo_dir):
 @pytest.mark.parametrize("scalar_type", ["float64", "float32", "complex128", "complex64"])
 def test_demo(file, scalar_type):
     """Test a demo."""
+    if sys.platform.startswith("win32") and "complex" in scalar_type:
+        # Skip complex demos on win32
+        pytest.skip()
+
     if file in [
         "MixedGradient",
         "TraceElement",  # HDiv Trace
@@ -38,16 +42,20 @@ def test_demo(file, scalar_type):
         # Skip demos that are only implemented for complex scalars
         pytest.skip()
 
-    opts = f"--scalar_type {scalar_type}"
-    extra_flags = "-Wunused-variable -Werror -fPIC "
-    assert os.system(f"cd {demo_dir} && ffcx {opts} {file}.py") == 0
-    assert (
-        os.system(
-            f"cd {demo_dir} && "
-            "CPATH=../ffcx/codegeneration/ "
-            f"gcc -I/usr/include/python{sys.version_info.major}.{sys.version_info.minor} "
-            f"{extra_flags} "
-            f"-shared {file}.c -o {file}.so"
+    if sys.platform.startswith("win32"):
+        opts = f"--scalar_type {scalar_type}"
+        assert os.system(f"cd {demo_dir} && ffcx {opts} {file}.py") == 0
+        assert os.system(f"cd {demo_dir} && " f'cl "../ffcx/codegeneration" /c {file}.c') == 0
+    else:
+        opts = f"--scalar_type {scalar_type}"
+        extra_flags = "-Wunused-variable -Werror -fPIC "
+        assert os.system(f"cd {demo_dir} && ffcx {opts} {file}.py") == 0
+        assert (
+            os.system(
+                f"cd {demo_dir} && "
+                f"gcc -I../ffcx/codegeneration "
+                f"{extra_flags} "
+                f"-c {file}.c"
+            )
+            == 0
         )
-        == 0
-    )
