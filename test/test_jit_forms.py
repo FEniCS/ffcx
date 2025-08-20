f5b49a4a090bf964324739610411db052909d420
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2025-08-20 10:03:46 +0200

    Chnage to atol - rtol not working with allclose(0, eps) situation

--- test/test_jit_forms.py
+++ test/test_jit_forms.py
@@ -1479,7 +1479,7 @@ def test_vertex_integral(compile_args, geometry, rank, dtype):
                 ffi.NULL,
             )
             idx = (rank > 0) * vertex + (rank > 1) * vertex * vertex_count
-            assert np.isclose(J[idx], coords[vertex * 3], rtol=1e2 * np.finfo(dtype).eps)
+            assert np.isclose(coords[vertex * 3], J[idx], atol=1e-6)
             # Check all other entries are not touched
             assert np.allclose(np.delete(J, [idx]), 0)
 
