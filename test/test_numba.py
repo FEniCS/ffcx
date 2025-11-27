9ed41a1b3d25c043da054175b3e4ae90d0e3b1d7
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2025-11-27 17:12:17 +0100

    Parametrize over scalar type

--- test/test_numba.py
+++ test/test_numba.py
@@ -10,76 +10,81 @@ from pathlib import Path
 
 import numba
 import numpy as np
+import numpy.typing as npt
+import pytest
 
 from ffcx.codegeneration.utils import dtype_to_scalar_dtype, numba_ufcx_kernel_signature
 
 
-def test_poisson():
-    subprocess.run(
-        ["ffcx", Path(__file__).parent / "laplace.py", "--language", "numba"], check=True
-    )
+def wrap_kernel(scalar_type, real_type):
+    c_signature = numba_ufcx_kernel_signature(scalar_type, real_type)
+    return numba.cfunc(c_signature, nopython=True)
+
+
+def as_C_array(np_array: npt.NDArray):
+    dtype_C = np.ctypeslib.as_ctypes_type(np_array.dtype)
+    return np_array.ctypes.data_as(ctypes.POINTER(dtype_C))
+
+
+@pytest.mark.parametrize("scalar_type", ["float32", "float64"])  # TODO: complex limited by ctypes
+def test_poisson(scalar_type):
+    opts = f"--language numba --scalar_type {scalar_type}"
+    subprocess.run(["ffcx", Path(__file__).parent / "laplace.py", *opts.split(" ")], check=True)
 
     laplace = importlib.import_module("laplace_numba")
 
-    print(dir(laplace))
+    dtype = np.dtype(scalar_type).type
+    dtype_r = dtype_to_scalar_dtype(dtype)
 
-    def wrapper(scalar_type, real_type):
-        c_signature = numba_ufcx_kernel_signature(scalar_type, real_type)
-        return numba.cfunc(c_signature, nopython=True)
-
-    dtype = np.float64
-    realtype = np.dtype(dtype(0).real)
-    kernel_a = wrapper(dtype, realtype)(laplace.form_laplace_a.form_integrals[0].tabulate_tensor)
+    kernel_a = wrap_kernel(dtype, dtype_r)(laplace.form_laplace_a.form_integrals[0].tabulate_tensor)
 
     A = np.zeros((3, 3), dtype=dtype)
     w = np.array([], dtype=dtype)
     kappa_value = np.array([[1.0, 2.0], [3.0, 4.0]])
     c = np.array(kappa_value.flatten(), dtype=dtype)
+    coords = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]], dtype=dtype_r)
+    empty = np.empty((0,), dtype=dtype_r)
 
-    xdtype = dtype_to_scalar_dtype(dtype)
-    coords = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]], dtype=xdtype)
-
-    empty = np.empty((0,), dtype=realtype)
-
-    # c_type = "double"
-    # c_xtype = "double"
     kernel_a(
-        A.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-        w.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-        c.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-        coords.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-        empty.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-        empty.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+        as_C_array(A),
+        as_C_array(w),
+        as_C_array(c),
+        as_C_array(coords),
+        as_C_array(empty),
+        as_C_array(empty),
         0,
     )
-
-    A_expected = np.array([[1.0, -0.5, -0.5], [-0.5, 0.5, 0.0], [-0.5, 0.0, 0.5]], dtype=np.float64)
+    A_expected = np.array(
+        [[1.0, -0.5, -0.5], [-0.5, 0.5, 0.0], [-0.5, 0.0, 0.5]], dtype=scalar_type
+    )
 
     assert np.allclose(A, np.trace(kappa_value) * A_expected)
 
-    kernel_L = wrapper(dtype, realtype)(laplace.form_laplace_L.form_integrals[0].tabulate_tensor)
+    kernel_L = wrap_kernel(dtype, dtype_r)(laplace.form_laplace_L.form_integrals[0].tabulate_tensor)
 
     b = np.zeros((3,), dtype=dtype)
     w = np.full((3,), 0.5, dtype=dtype)
     c = np.empty((0,), dtype=dtype)
+    coords = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]], dtype=dtype_r)
+    empty = np.empty((0,), dtype=dtype_r)
 
-    xdtype = dtype_to_scalar_dtype(dtype)
-    coords = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]], dtype=xdtype)
-
-    empty = np.empty((0,), dtype=realtype)
-
-    # c_type = "double"
-    # c_xtype = "double"
     kernel_L(
-        b.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-        w.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-        c.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-        coords.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-        empty.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-        empty.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+        as_C_array(b),
+        as_C_array(w),
+        as_C_array(c),
+        as_C_array(coords),
+        as_C_array(empty),
+        as_C_array(empty),
         0,
     )
 
     b_expected = np.full((3,), 1 / 6, dtype=np.float64)
-
     assert np.allclose(b, 0.5 * b_expected)
+
+    subprocess.run(
+        [
+            "rm",
+            Path(__file__).parent / "laplace_numba.py",
+        ],
+        check=True,
+    )
