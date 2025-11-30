2b2fab44142afeab760ebb4aaa089e7cd3e2f51c
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2025-11-30 18:24:25 +0100

    format

--- ffcx/codegeneration/C/integrals.py
+++ ffcx/codegeneration/C/integrals.py
@@ -84,8 +84,8 @@ def generator(
         enabled_coefficients_init=code["enabled_coefficients_init"],
         tabulate_tensor=code["tabulate_tensor"],
         needs_facet_permutations="true" if ir.expression.needs_facet_permutations else "false",
-        scalar_type=dtype_to_c_type(options["scalar_type"]), # type: ignore
-        geom_type=dtype_to_c_type(dtype_to_scalar_dtype(options["scalar_type"])), # type: ignore
+        scalar_type=dtype_to_c_type(options["scalar_type"]),  # type: ignore
+        geom_type=dtype_to_c_type(dtype_to_scalar_dtype(options["scalar_type"])),  # type: ignore
         coordinate_element_hash=f"UINT64_C({ir.expression.coordinate_element_hash})",
         tabulate_tensor_float32=code["tabulate_tensor_float32"],
         tabulate_tensor_float64=code["tabulate_tensor_float64"],
