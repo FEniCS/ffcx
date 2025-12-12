74f229612b2c5f069dba7176a77c0a63a2d582a1
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2025-12-12 21:42:07 +0100

    Simplify

--- ffcx/codegeneration/common.py
+++ ffcx/codegeneration/common.py
@@ -31,29 +31,17 @@ class IntegralData(NamedTuple):
 
 def integral_data(ir: FormIR) -> IntegralData:
     """Extracts sorted intergral data from form."""
-    names = []
-    ids = []
+    names, ids, domains = [], [], []
     offsets = [0]
-    domains = []
     # Note: the order of this list is defined by the enum ufcx_integral_type in ufcx.h
     for itg_type in ("cell", "exterior_facet", "interior_facet", "vertex", "ridge"):
-        unsorted_integrals = []
-        unsorted_ids = []
-        unsorted_domains = []
-        for name, _domains, id in zip(
-            ir.integral_names[itg_type],
-            ir.integral_domains[itg_type],
-            ir.subdomain_ids[itg_type],
-        ):
-            unsorted_integrals += [name]
-            unsorted_ids += [id]
-            unsorted_domains += [_domains]
+        _ids = ir.subdomain_ids[itg_type]
+        id_sort = np.argsort(_ids)
 
-        id_sort = np.argsort(unsorted_ids)
-        names += [unsorted_integrals[i] for i in id_sort]
-        ids += [unsorted_ids[i] for i in id_sort]
-        domains += [unsorted_domains[i] for i in id_sort]
+        ids += [_ids[i] for i in id_sort]
+        names += [ir.integral_names[itg_type][i] for i in id_sort]
+        domains += [ir.integral_domains[itg_type][i] for i in id_sort]
 
-        offsets.append(sum(len(d) for d in domains))
+        offsets.append(offsets[-1] + sum(len(d) for d in domains[offsets[-1] :]))
 
     return IntegralData(names, ids, offsets, domains)
