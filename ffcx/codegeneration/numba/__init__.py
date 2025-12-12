ba959e9558d1f90884b3d99b63bd3f1f7069072c
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2025-12-12 20:29:32 +0100

    Fix import

--- ffcx/codegeneration/numba/__init__.py
+++ ffcx/codegeneration/numba/__init__.py
@@ -1,6 +1,7 @@
 """Generation of numba code."""
 
 from ffcx.codegeneration.numba import expressions, file, form, integrals
-from ffcx.codegeneration.numba.formatter import Formatter
+
+from .formatter import Formatter
 
 __all__ = ["Formatter", "expressions", "file", "form", "integrals", "suffixes"]
