e4f9ebefd4c4e2bb42dc9051057b851489104923
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2026-02-09 17:41:27 +0100

    Add protocols

--- ffcx/codegeneration/interface.py
+++ ffcx/codegeneration/interface.py
@@ -6,20 +6,48 @@
 
 """Backend interface declarations."""
 
-import abc
+from typing import Protocol
 
+import basix
 from numpy import typing as npt
 
 import ffcx.codegeneration.lnodes as L
+from ffcx.ir.representation import FormIR, IntegralIR
 
 
-class Formatter(abc.ABC):
+class Formatter(Protocol):
     """Formatter interface."""
 
     def __init__(self, dtype: npt.DTypeLike) -> None:
         """Create."""
-        raise NotImplementedError
+        ...
 
     def __call__(self, obj: L.LNode) -> str:
         """Convert L-Node(s) to string representation."""
-        raise NotImplementedError
+        ...
+
+
+class file:
+    """File formatter."""
+
+    def generator(options: dict[str, int | float | npt.DTypeLike]) -> tuple[str, ...]:
+        """File to source string."""
+        ...
+
+
+class form:
+    """Form formatter."""
+
+    def generator(ir: FormIR, options: dict[str, int | float | npt.DTypeLike]) -> tuple[str]:
+        """Form to source string."""
+        ...
+
+
+class integral:
+    """Integral formatter."""
+
+    def generator(
+        ir: IntegralIR, domain: basix.CellType, options: dict[str, int | float | npt.DTypeLike]
+    ) -> tuple[str]:
+        """Integral to source string."""
+        ...
