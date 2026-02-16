38ab264dcddfca3a9f437d45771465f6b683a048
Paul T. Kühner <56360279+schnellerhase@users.noreply.github.com>
2026-02-16 18:56:53 +0100

    Define language backend interface as protocol (#818)
    * Add protocols
    
    * more
    
    * Docstring
    
    * static
    
    * Expression and actually use it
    
    * only for type checking
    
    * hack test
    
    * dtype
    
    * docstring + format
    
    * Can not type comapre module and class
    
    * base on callbables
    
    * format
    
    * trigger CI
    
    * Back alive?
    
    * Try function
    
    * Revert "Try function"
    
    This reverts commit 256dfba5ef6a59dea21f844e3f6d224865c00fc8.

--- ffcx/codegeneration/numba/__init__.py
+++ ffcx/codegeneration/numba/__init__.py
@@ -1,5 +1,8 @@
 """Generation of numba code."""
 
+from typing import TYPE_CHECKING
+
+from ffcx.codegeneration import interface
 from ffcx.codegeneration.numba import expression, file, form, integral
 
 from .formatter import Formatter
@@ -11,3 +14,13 @@ __all__ = [
     "form",
     "integral",
 ]
+
+if TYPE_CHECKING:
+    # ensure protocol compliance
+    import numpy as np
+
+    _formatter: interface.Formatter = Formatter(np.float64)
+    _expression: interface.expression_generator = expression.generator
+    _file: interface.file_generator = file.generator
+    _form: interface.form_generator = form.generator
+    _integral: interface.integral_generator = integral.generator
